69d5b9f97fd4dbfed69617795fccff42f1e6f7aa
Jan Cizmar <cizmar@chlupac.com>
2021-04-16 11:00:21 +0200

    fix: TG-114 Mode without authentication > API keys > Remove user menu

--- webapp/src/component/views/userSettings/BaseUserSettingsView.tsx
+++ webapp/src/component/views/userSettings/BaseUserSettingsView.tsx
@@ -6,32 +6,41 @@ import {Box, Grid, Typography} from "@material-ui/core";
 import {useUser} from "../../../hooks/useUser";
 import {UserSettingsMenu} from "./UserSettingsMenu";
 import UserOrganizationSettingsSubtitleLink from "../organizations/components/UserOrganizationSettingsSubtitleLink";
+import {useConfig} from "../../../hooks/useConfig";
 
 export const BaseUserSettingsView: FunctionComponent<BaseViewProps> = ({children, title, ...otherProps}: PropsWithChildren<BaseViewProps>) => {
 
     const user = useUser()
+    const config = useConfig()
+
+    const pageHeader = config.authentication ?
+        <>
+            <Typography variant="h5">{user?.name}</Typography>
+            <UserOrganizationSettingsSubtitleLink isUser={true}/>
+        </>
+        :
+        <>
+            <Typography variant="h5">{title}</Typography>
+        </>
 
     return (
         <DashboardPage>
             <BaseView {...otherProps} containerMaxWidth="md"
-                      customHeader={
-                          <>
-                              <Typography variant="h5">{user?.name}</Typography>
-                              <UserOrganizationSettingsSubtitleLink isUser={true}/>
-                          </>
-                      }
+                      customHeader={pageHeader}
             >
                 <Grid container>
-                    <Grid item lg={3} md={4}>
+                    {config.authentication &&
+                    <Grid item lg={3} md={4} sm={12} xs={12}>
                         <Box mr={4} mb={4}>
                             <UserSettingsMenu/>
                         </Box>
-                    </Grid>
-                    <Grid item lg={9} md={8} sm={12} xs={12}>
+                    </Grid>}
+                    <Grid item lg md sm xs>
                         <Box>
+                            {config.authentication &&
                             <Box mb={2}>
                                 <Typography variant="h6">{title}</Typography>
-                            </Box>
+                            </Box>}
                             {children}
                         </Box>
                     </Grid>
