99717b4f35d0220dbfb761e86faa757ad7c67663
Štěpán Granát <granat.stepan@gmail.com>
2021-06-22 09:50:14 +0200

    feat(LanguagesProvider): switch to react query

--- webapp/src/hooks/ProjectLanguagesProvider.tsx
+++ webapp/src/hooks/ProjectLanguagesProvider.tsx
@@ -1,54 +1,46 @@
-import { FunctionComponent, useEffect } from 'react';
+import { FunctionComponent, useEffect, createContext } from 'react';
 import { container } from 'tsyringe';
 import { useSelector } from 'react-redux';
 import { AppState } from '../store';
 import { GlobalError } from '../error/GlobalError';
-import { LanguageActions } from '../store/languages/LanguageActions';
 import { useProject } from './useProject';
 import { FullPageLoading } from '../component/common/FullPageLoading';
 import { ProjectPreferencesService } from '../service/ProjectPreferencesService';
 import { TranslationActions } from '../store/project/TranslationActions';
+import { useApiQuery } from '../service/http/useQueryApi';
+import { components } from '../service/apiSchema.generated';
 
-const languageActions = container.resolve(LanguageActions);
+export const ProjectLanguagesContext =
+  // @ts-ignore
+  createContext<components['schemas']['PagedModelLanguageModel']>(null);
+
 const translationActions = container.resolve(TranslationActions);
 const selectedLanguagesService = container.resolve(ProjectPreferencesService);
 
 export const ProjectLanguagesProvider: FunctionComponent = (props) => {
   const projectDTO = useProject();
 
-  const languagesLoadable = useSelector(
-    (state: AppState) => state.languages.loadables.globalList
-  );
   const selectedLanguages = useSelector(
     (state: AppState) => state.translations.selectedLanguages
   );
 
-  const isLoading = languagesLoadable.loading;
-  const init =
-    !languagesLoadable.data && !languagesLoadable.error && !isLoading;
-  const idChanged =
-    languagesLoadable.dispatchParams &&
-    languagesLoadable.dispatchParams[0].path.projectId !== projectDTO.id;
-
-  useEffect(() => {
-    if (init || idChanged) {
-      languageActions.loadableActions.globalList.dispatch({
-        path: { projectId: projectDTO.id },
-        query: {
-          pageable: {
-            page: 0,
-            size: 1000,
-            sort: ['tag'],
-          },
-        },
-      });
-    }
-  }, [init]);
+  const languagesLoadable = useApiQuery({
+    url: '/v2/projects/{projectId}/languages',
+    method: 'get',
+    path: { projectId: projectDTO.id },
+    query: {
+      pageable: {
+        page: 0,
+        size: 1000,
+        sort: ['tag'],
+      },
+    },
+  });
 
   useEffect(() => {
     // reset languages when unmount
     return () => {
-      languageActions.loadableReset.globalList.dispatch();
+      languagesLoadable.remove();
     };
   }, []);
 
@@ -69,12 +61,16 @@ export const ProjectLanguagesProvider: FunctionComponent = (props) => {
     }
   }, [languagesLoadable.data]);
 
-  if (init || idChanged || languagesLoadable.loading || !selectedLanguages) {
+  if (languagesLoadable.isFetching || !selectedLanguages) {
     return <FullPageLoading />;
   }
 
   if (languagesLoadable.data) {
-    return <>{props.children}</>;
+    return (
+      <ProjectLanguagesContext.Provider value={languagesLoadable.data}>
+        {props.children}
+      </ProjectLanguagesContext.Provider>
+    );
   }
 
   throw new GlobalError(
--- webapp/src/service/OrganizationService.ts
+++ webapp/src/service/OrganizationService.ts
@@ -1,23 +1,9 @@
 import { singleton } from 'tsyringe';
 import { ApiV2HttpService } from './http/ApiV2HttpService';
-import { components } from './apiSchema.generated';
 
 @singleton()
 export class OrganizationService {
   constructor(private v2http: ApiV2HttpService) {}
-
-  public listPermitted = async (
-    query?: components['schemas']['Pageable'] &
-      components['schemas']['OrganizationRequestParamsDto']
-  ): Promise<components['schemas']['PagedModelOrganizationModel']> =>
-    this.v2http.get(`organizations`, query);
-
-  public createOrganization = (
-    data: components['schemas']['OrganizationDto']
-  ) =>
-    this.v2http.post(`organizations`, data) as Promise<
-      components['schemas']['OrganizationModel']
-    >;
 
   public generateSlug = (name: string, oldSlug?: string) =>
     this.v2http.post(`address-part/generate-organization`, {
@@ -29,52 +15,4 @@ export class OrganizationService {
     this.v2http.get(
       `address-part/validate-organization/${slug}`
     ) as Promise<boolean>;
-
-  public getOrganization = (
-    slug: string
-  ): Promise<components['schemas']['OrganizationModel']> =>
-    this.v2http.get(`organizations/${slug}`);
-
-  public editOrganization = (
-    id,
-    data: components['schemas']['OrganizationDto']
-  ) =>
-    this.v2http.put(`organizations/${id}`, data) as Promise<
-      components['schemas']['OrganizationModel']
-    >;
-
-  public listUsers = async (
-    id: number,
-    query?: components['schemas']['Pageable']
-  ): Promise<
-    components['schemas']['PagedModelUserAccountWithOrganizationRoleModel']
-  > => this.v2http.get(`organizations/${id}/users`, query);
-
-  public setRole = async (
-    organizationId: number,
-    userId: number,
-    roleType: string
-  ) =>
-    this.v2http.put(
-      `organizations/${organizationId}/users/${userId}/set-role`,
-      { roleType }
-    );
-
-  public invite = async (
-    organizationId: number,
-    roleType: string
-  ): Promise<components['schemas']['OrganizationInvitationModel']> =>
-    this.v2http.put(`organizations/${organizationId}/invite`, { roleType });
-
-  public listInvitations = async (
-    organizationId: number
-  ): Promise<
-    components['schemas']['CollectionModelOrganizationInvitationModel']
-  > => this.v2http.get(`organizations/${organizationId}/invitations`);
-
-  public removeUser = async (organizationId: number, userId: number) =>
-    this.v2http.delete(`organizations/${organizationId}/users/${userId}`, {});
-
-  public deleteOrganization = async (organizationId: number) =>
-    this.v2http.delete(`organizations/${organizationId}`, {});
 }
