95ba141d70afdd3c4bdc2d7c7da9bfe11b2a10bc
Jan Cizmar <cizmar@chlupac.com>
2021-07-12 19:20:02 +0200

    feat(sockets): Enabled by default, add configuration property for frontend

--- src/main/kotlin/io/tolgee/configuration/tolgee/SocketIoProperties.kt
+++ src/main/kotlin/io/tolgee/configuration/tolgee/SocketIoProperties.kt
@@ -4,8 +4,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "tolgee.socket-io")
 class SocketIoProperties(
-  var enabled: Boolean = false,
+  var enabled: Boolean = true,
   var port: Int = 9090,
   var host: String? = null,
-  var useRedis: Boolean = false
+  var useRedis: Boolean = false,
+  var externalUrl: String?
 )
