2e18c811bae9d41a7c05a5f0858aea23955d6763
Jan Cizmar <cizmar@chlupac.com>
2021-07-24 11:00:44 +0200

    feat(projects): Projects list dashboard TG-185

--- webapp/src/hooks/redirect.ts
+++ webapp/src/hooks/redirect.ts
@@ -3,9 +3,6 @@ import { container } from 'tsyringe';
 import { Link, PARAMS } from '../constants/links';
 import { RedirectionActions } from '../store/global/RedirectionActions';
 
-export function useRedirect(
-  to: Link,
-  params?: Partial<{ [K in PARAMS]: any }>
-) {
+export function redirect(to: Link, params?: Partial<{ [K in PARAMS]: any }>) {
   container.resolve(RedirectionActions).redirect.dispatch(to.build(params));
 }
