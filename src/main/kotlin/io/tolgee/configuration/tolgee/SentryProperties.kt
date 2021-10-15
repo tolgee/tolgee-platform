663298731321221152e4a4df20335407da01e5ef
Jan Cizmar <cizmar@chlupac.com>
2021-10-15 10:34:12 +0200

    fix: Remove sentry enabled from properties TG-365
    Sentry.enabled is not needed since disabling of sentry may be achieved by omitting dsn in configuration

--- src/main/kotlin/io/tolgee/configuration/tolgee/SentryProperties.kt
+++ src/main/kotlin/io/tolgee/configuration/tolgee/SentryProperties.kt
@@ -4,7 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "tolgee.sentry")
 class SentryProperties {
-  var enabled = false
   var serverDsn: String? = null
   var clientDsn: String? = null
 }
