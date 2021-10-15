313d6cf308be13b29a6420c76944420b1542cce3
Štěpán Granát <granat.stepan@gmail.com>
2021-10-15 10:34:00 +0200

    feat: Upgrade tolgee-js and minor fixes

--- e2e/cypress/common/types.ts
+++ e2e/cypress/common/types.ts
@@ -1,4 +1,11 @@
-export type Scope = 'translations.edit' | 'translations.view' | 'keys.edit';
+export type Scope =
+  | 'translations.view'
+  | 'translations.edit'
+  | 'keys.edit'
+  | 'screenshots.view'
+  | 'screenshots.upload'
+  | 'screenshots.delete';
+
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type ArgumentTypes<F extends Function> = F extends (
   ...args: infer A
