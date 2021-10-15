23f9ae1894ab4a51b10536ee93b69e8b3144639e
Štěpán Granát <granat.stepan@gmail.com>
2021-10-15 13:29:47 +0200

    fix: Add parameter name to editor plural cases preview TG-295

--- webapp/src/views/projects/translations/TranslationVisual.tsx
+++ webapp/src/views/projects/translations/TranslationVisual.tsx
@@ -1,9 +1,8 @@
 import React, { useMemo } from 'react';
-import { makeStyles } from '@material-ui/core';
+import { Typography, makeStyles } from '@material-ui/core';
 
 import { icuVariants } from 'tg.component/editor/icuVariants';
 import { LimitedHeightText } from './LimitedHeightText';
-import clsx from 'clsx';
 
 const useStyles = makeStyles((theme) => ({
   variants: {
@@ -12,6 +11,13 @@ const useStyles = makeStyles((theme) => ({
     columnGap: 4,
   },
   wrapped: {
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+  },
+  parameter: {
+    display: 'flex',
+    margin: '1px 0px 3px 0px',
     overflow: 'hidden',
     whiteSpace: 'nowrap',
     textOverflow: 'ellipsis',
@@ -48,7 +54,10 @@ export const TranslationVisual: React.FC<Props> = ({
   width,
 }) => {
   const classes = useStyles();
-  const { variants } = useMemo(() => icuVariants(text || '', locale), [text]);
+  const { variants, parameters } = useMemo(
+    () => icuVariants(text || '', locale),
+    [text]
+  );
 
   if (!variants) {
     return (
@@ -60,39 +69,50 @@ export const TranslationVisual: React.FC<Props> = ({
         {text}
       </LimitedHeightText>
     );
-  } else if (variants.length === 1) {
-    return (
-      <LimitedHeightText
-        width={width}
-        maxLines={limitLines ? 3 : undefined}
-        lang={locale}
-      >
-        {variants[0].value}
-      </LimitedHeightText>
-    );
   } else {
+    const allParams = parameters
+      .filter((p) => p.function !== 'argument')
+      .map((p) => p.name)
+      .join(', ');
     return (
-      <LimitedHeightText
-        width={width}
-        maxLines={limitLines ? 6 : undefined}
-        lang={locale}
-        lineHeight="26px"
-      >
-        <div
-          className={clsx({
-            [classes.variants]: true,
-          })}
-        >
-          {variants.map(({ option, value }, i) => (
-            <React.Fragment key={i}>
-              <div className={classes.chip}>{option}</div>
-              <div className={limitLines ? classes.wrapped : undefined}>
-                {value}
-              </div>
-            </React.Fragment>
-          ))}
-        </div>
-      </LimitedHeightText>
+      <>
+        {allParams && (
+          <Typography
+            className={classes.parameter}
+            variant="caption"
+            color="textSecondary"
+          >
+            {allParams}
+          </Typography>
+        )}
+        {variants.length === 1 ? (
+          <LimitedHeightText
+            width={width}
+            maxLines={limitLines ? 3 : undefined}
+            lang={locale}
+          >
+            {variants[0].value}
+          </LimitedHeightText>
+        ) : (
+          <LimitedHeightText
+            width={width}
+            maxLines={limitLines ? 6 : undefined}
+            lang={locale}
+            lineHeight="26px"
+          >
+            <div className={classes.variants}>
+              {variants.map(({ option, value }, i) => (
+                <React.Fragment key={i}>
+                  <div className={classes.chip}>{option}</div>
+                  <div className={limitLines ? classes.wrapped : undefined}>
+                    {value}
+                  </div>
+                </React.Fragment>
+              ))}
+            </div>
+          </LimitedHeightText>
+        )}
+      </>
     );
   }
 };
