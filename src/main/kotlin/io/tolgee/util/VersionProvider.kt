d22dc6c5d8beb424afd84f77aa7efbeb1225b053
Jan Cizmar <cizmar@chlupac.com>
2021-10-20 13:58:28 +0200

    fix: Missing .VERSION in released .jar
    - "java.io.FileNotFoundException: class path resource [.VERSION] cannot be resolved to absolute file" was resolved

--- src/main/kotlin/io/tolgee/util/VersionProvider.kt
+++ src/main/kotlin/io/tolgee/util/VersionProvider.kt
@@ -1,12 +1,18 @@
 package io.tolgee.util
 
+import io.tolgee.Application
 import org.springframework.core.io.ClassPathResource
+import java.io.FileNotFoundException
 
 class VersionProvider {
   companion object {
     val version: String by lazy {
-      val resource = ClassPathResource(".VERSION", this::class.java.classLoader)
-      if (!resource.exists()) "??" else resource.file.readText()
+      try {
+        val resource = ClassPathResource(".VERSION", this::class.java.classLoader)
+        return@lazy if (!resource.exists()) "??" else resource.file.readText()
+      } catch (e: FileNotFoundException) {
+        return@lazy Application::class.java.getPackage().implementationVersion
+      }
     }
   }
 }
