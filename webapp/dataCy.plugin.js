409be1237985b47450f1fd810c5c5980a452e3cc
Jan Cizmar <cizmar@chlupac.com>
2022-02-09 13:17:09 +0100

    fix: Fix data-cy webpack plugin

--- webapp/dataCy.plugin.js
+++ webapp/dataCy.plugin.js
@@ -1,6 +1,10 @@
 const { resolve } = require('path');
 const { readdir, readFile, writeFile } = require('fs').promises;
+const { existsSync } = require('fs');
 const path = require('path');
+const watch = require('glob-watcher');
+
+const SRC_PATH = path.resolve('./src');
 
 /**
  * Plugin generating types for Cypress data-cy items
@@ -9,51 +13,83 @@ class DataCyPlugin {
   fileItems = {};
 
   apply(compiler) {
-    const srcPath = path.resolve('./src');
-    // Specify the event hook to attach to
     compiler.hooks.afterPlugins.tap('DataCyPlugin', async () => {
-      const files = await this.getFiles(srcPath);
+      const files = await this.getFiles(SRC_PATH);
       await this.generate(files);
     });
 
-    compiler.hooks.watchRun.tapPromise('DataCyPluginWatch', async (comp) => {
-      const changedTimes = comp.watchFileSystem.watcher.mtimes;
-      const changedFiles = Object.keys(changedTimes);
-      if (changedFiles.length) {
-        await this.generate(changedFiles);
-      }
-    });
+    if (process.env.NODE_ENV === 'development') {
+      compiler.hooks.afterPlugins.tap('RunDataCyWathc', () => {
+        const watcher = watch([`${SRC_PATH}/**/*.tsx`, `${SRC_PATH}/**/*.ts`]);
+
+        watcher.on('change', (path) => {
+          this.generate([path]);
+        });
+
+        watcher.on('add', (path) => {
+          this.generate([path]);
+        });
+
+        watcher.on('unlink', (path) => {
+          this.generate([path]);
+        });
+      });
+    }
   }
 
   async generate(files) {
-    for (let file of files) {
-      if (/.*\.tsx$/.test(file)) {
-        const content = (await readFile(file)).toString();
-        const matches = content.matchAll(
-          /["']?data-cy["']?\s*[=:]\s*{?["'`]([A-Za-z0-9-_\s]+)["'`]?}?/g
-        );
-        this.fileItems[file] = [];
-        for (let match of matches) {
-          this.fileItems[file] = [...this.fileItems[file], match[1]];
-        }
-      }
-    }
+    await this.processFiles(files);
+    const sortedItems = this.getSortedItems();
+    let fileContent = this.generateFileContent(sortedItems);
+    await this.writeFile(fileContent);
+  }
 
-    const items = Object.values(this.fileItems).reduce(
-      (acc, curr) => [...acc, ...curr],
-      []
+  async processFiles(files) {
+    for (let file of files) {
+      await this.processFile(file);
+    }
+  }
+
+  async writeFile(fileContent) {
+    await writeFile(
+      path.resolve(`../e2e/cypress/support/dataCyType.d.ts`),
+      fileContent
     );
-    const itemsSet = new Set(items);
-    const sortedItems = [...itemsSet].sort();
+  }
+
+  generateFileContent(sortedItems) {
     let fileContent = 'declare namespace DataCy {\n';
     fileContent +=
       '    export type Value = \n        ' +
       sortedItems.map((i) => `"${i}"`).join(' |\n        ') +
       '\n}';
-    await writeFile(
-      path.resolve(`../e2e/cypress/support/dataCyType.d.ts`),
-      fileContent
+    return fileContent;
+  }
+
+  getSortedItems() {
+    const items = Object.values(this.fileItems).reduce(
+      (acc, curr) => [...acc, ...curr],
+      []
     );
+    const itemsSet = new Set(items);
+    return [...itemsSet].sort();
+  }
+
+  async processFile(file) {
+    if (/.*\.tsx?$/.test(file)) {
+      if (!existsSync(file)) {
+        this.fileItems[file] = [];
+        return;
+      }
+      const content = (await readFile(file)).toString();
+      const matches = content.matchAll(
+        /["']?data-cy["']?\s*[=:]\s*{?["'`]([A-Za-z0-9-_\s]+)["'`]?}?/g
+      );
+      this.fileItems[file] = [];
+      for (let match of matches) {
+        this.fileItems[file].push(match[1]);
+      }
+    }
   }
 
   async getFiles(dir) {
