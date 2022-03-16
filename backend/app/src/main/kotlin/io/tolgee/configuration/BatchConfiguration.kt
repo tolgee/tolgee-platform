841e6b62971ffd07e74a6fa5d6712dc5ffd4b874
Jan Cizmar <cizmar@chlupac.com>
2022-03-16 13:19:08 +0100

    feat: Add project dashboard page #915 - add statistics to translations and populate them

---
+++ backend/app/src/main/kotlin/io/tolgee/configuration/BatchConfiguration.kt
@@ -0,0 +1,8 @@
+package io.tolgee.configuration
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.context.annotation.Configuration
+
+@EnableBatchProcessing
+@Configuration
+class BatchConfiguration
