962aa9da44c3420459ccf1b9990d172404dd90ca
Jan Cizmar <cizmar@chlupac.com>
2022-04-02 10:49:43 +0200

    fix: Fix complex update permission checking

--- backend/app/src/main/kotlin/io/tolgee/api/v2/controllers/V2KeyController.kt
+++ backend/app/src/main/kotlin/io/tolgee/api/v2/controllers/V2KeyController.kt
@@ -6,12 +6,11 @@ import io.tolgee.api.v2.hateoas.key.KeyModel
 import io.tolgee.api.v2.hateoas.key.KeyModelAssembler
 import io.tolgee.api.v2.hateoas.key.KeyWithDataModel
 import io.tolgee.api.v2.hateoas.key.KeyWithDataModelAssembler
-import io.tolgee.constants.Message
+import io.tolgee.component.KeyComplexEditHelper
 import io.tolgee.controllers.IController
 import io.tolgee.dtos.request.key.ComplexEditKeyDto
 import io.tolgee.dtos.request.key.CreateKeyDto
 import io.tolgee.dtos.request.key.EditKeyDto
-import io.tolgee.exceptions.BadRequestException
 import io.tolgee.exceptions.NotFoundException
 import io.tolgee.model.Permission
 import io.tolgee.model.Project
@@ -22,10 +21,8 @@ import io.tolgee.security.api_key_auth.AccessWithApiKey
 import io.tolgee.security.project_auth.AccessWithProjectPermission
 import io.tolgee.security.project_auth.ProjectHolder
 import io.tolgee.service.KeyService
-import io.tolgee.service.ScreenshotService
 import io.tolgee.service.SecurityService
-import io.tolgee.service.TagService
-import io.tolgee.service.TranslationService
+import org.springframework.context.ApplicationContext
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.transaction.annotation.Transactional
@@ -57,9 +54,7 @@ class V2KeyController(
   private val keyWithDataModelAssembler: KeyWithDataModelAssembler,
   private val securityService: SecurityService,
   private val authenticationFacade: AuthenticationFacade,
-  private val translationService: TranslationService,
-  private val tagService: TagService,
-  private val screenshotService: ScreenshotService
+  private val applicationContext: ApplicationContext
 ) : IController {
   @PostMapping(value = ["/create", ""])
   @AccessWithProjectPermission(Permission.ProjectPermissionType.EDIT)
@@ -81,25 +76,7 @@ class V2KeyController(
   @AccessWithApiKey([ApiScope.TRANSLATIONS_EDIT])
   @Transactional
   fun complexEdit(@PathVariable id: Long, @RequestBody @Valid dto: ComplexEditKeyDto): KeyWithDataModel {
-    val key = keyService.findOptional(id).orElseThrow { NotFoundException() }
-    key.checkInProject()
-
-    dto.translations?.let {
-      securityService.checkLanguageTagPermissions(it.keys, projectHolder.project.id)
-      translationService.setForKey(key, translations = it)
-    }
-
-    dto.tags?.let { dtoTags ->
-      updateTags(key, dtoTags)
-    }
-
-    if (key.name != dto.name) {
-      key.project.checkKeysEditPermission()
-    }
-
-    updateScreenshots(dto, key)
-
-    return keyWithDataModelAssembler.toModel(keyService.edit(key, dto.name))
+    return KeyComplexEditHelper(applicationContext, id, dto).doComplexEdit()
   }
 
   @PutMapping(value = ["/{id}"])
@@ -129,62 +106,7 @@ class V2KeyController(
   private val Key.model: KeyModel
     get() = keyModelAssembler.toModel(this)
 
-  private fun Project.checkKeysEditPermission() {
-    if (authenticationFacade.isApiKeyAuthentication) {
-      securityService.checkApiKeyScopes(setOf(ApiScope.KEYS_EDIT), authenticationFacade.apiKey)
-    }
-    securityService.checkProjectPermission(this.id, Permission.ProjectPermissionType.EDIT)
-  }
-
-  private fun Project.checkScreenshotsDeletePermission() {
-    if (authenticationFacade.isApiKeyAuthentication) {
-      securityService.checkApiKeyScopes(setOf(ApiScope.SCREENSHOTS_DELETE), authenticationFacade.apiKey)
-    }
-    securityService.checkProjectPermission(this.id, Permission.ProjectPermissionType.TRANSLATE)
-  }
-
   private fun Project.checkScreenshotsUploadPermission() {
-    if (authenticationFacade.isApiKeyAuthentication) {
-      securityService.checkApiKeyScopes(setOf(ApiScope.SCREENSHOTS_UPLOAD), authenticationFacade.apiKey)
-    }
-    securityService.checkProjectPermission(this.id, Permission.ProjectPermissionType.TRANSLATE)
-  }
-
-  private fun updateScreenshots(dto: ComplexEditKeyDto, key: Key) {
-    dto.screenshotIdsToDelete?.let { screenshotIds ->
-      deleteScreenshots(screenshotIds, key)
-    }
-
-    dto.screenshotUploadedImageIds?.let {
-      key.project.checkScreenshotsUploadPermission()
-      screenshotService.saveUploadedImages(it, key)
-    }
-  }
-
-  private fun deleteScreenshots(
-    screenshotIds: List<Long>,
-    key: Key
-  ) {
-    if (screenshotIds.isNotEmpty()) {
-      key.project.checkScreenshotsDeletePermission()
-    }
-    val screenshots = screenshotService.findByIdIn(screenshotIds).onEach {
-      if (it.key.id != key.id) {
-        throw BadRequestException(Message.SCREENSHOT_NOT_OF_KEY)
-      }
-    }
-    screenshotService.delete(screenshots)
-  }
-
-  private fun updateTags(key: Key, dtoTags: List<String>) {
-    val existingTagsContainAllNewTags = key.keyMeta?.tags?.map { it.name }?.containsAll(dtoTags) ?: false
-    val newTagsContainAllExistingTags = dtoTags.containsAll(key.keyMeta?.tags?.map { it.name } ?: listOf())
-
-    val areTagsModified = !existingTagsContainAllNewTags || !newTagsContainAllExistingTags
-
-    if (areTagsModified) {
-      key.project.checkKeysEditPermission()
-      tagService.updateTags(key, dtoTags)
-    }
+    securityService.checkScreenshotsUploadPermission(this.id)
   }
 }
---
+++ backend/app/src/main/kotlin/io/tolgee/component/KeyComplexEditHelper.kt
@@ -0,0 +1,160 @@
+package io.tolgee.component
+
+import io.tolgee.api.v2.hateoas.key.KeyWithDataModel
+import io.tolgee.api.v2.hateoas.key.KeyWithDataModelAssembler
+import io.tolgee.constants.Message
+import io.tolgee.dtos.request.key.ComplexEditKeyDto
+import io.tolgee.exceptions.BadRequestException
+import io.tolgee.model.Permission
+import io.tolgee.model.Project
+import io.tolgee.model.enums.ApiScope
+import io.tolgee.model.key.Key
+import io.tolgee.security.AuthenticationFacade
+import io.tolgee.security.project_auth.ProjectHolder
+import io.tolgee.service.KeyService
+import io.tolgee.service.LanguageService
+import io.tolgee.service.ScreenshotService
+import io.tolgee.service.SecurityService
+import io.tolgee.service.TagService
+import io.tolgee.service.TranslationService
+import org.springframework.context.ApplicationContext
+import kotlin.properties.Delegates
+
+class KeyComplexEditHelper(
+  applicationContext: ApplicationContext,
+  private val keyId: Long,
+  private val dto: ComplexEditKeyDto
+) {
+
+  private val keyWithDataModelAssembler: KeyWithDataModelAssembler =
+    applicationContext.getBean(KeyWithDataModelAssembler::class.java)
+  private val keyService: KeyService = applicationContext.getBean(KeyService::class.java)
+  private val securityService: SecurityService = applicationContext.getBean(SecurityService::class.java)
+  private val languageService: LanguageService = applicationContext.getBean(LanguageService::class.java)
+  private val projectHolder: ProjectHolder = applicationContext.getBean(ProjectHolder::class.java)
+  private val translationService: TranslationService = applicationContext.getBean(TranslationService::class.java)
+  private val tagService: TagService = applicationContext.getBean(TagService::class.java)
+  private val screenshotService: ScreenshotService = applicationContext.getBean(ScreenshotService::class.java)
+  private val authenticationFacade: AuthenticationFacade = applicationContext.getBean(AuthenticationFacade::class.java)
+
+  private lateinit var key: Key
+  private var modifiedTranslations: Map<String, String?>? = null
+  private val dtoTags = dto.tags
+
+  private var areTranslationsModified by Delegates.notNull<Boolean>()
+  private var areTagsModified by Delegates.notNull<Boolean>()
+  private var isKeyModified by Delegates.notNull<Boolean>()
+  private var isScreenshotDeleted by Delegates.notNull<Boolean>()
+  private var isScreenshotAdded by Delegates.notNull<Boolean>()
+
+  fun doComplexEdit(): KeyWithDataModel {
+    prepareData()
+    prepareConditions()
+
+    if (modifiedTranslations != null && areTranslationsModified) {
+      securityService.checkLanguageTagPermissions(modifiedTranslations!!.keys, projectHolder.project.id)
+      translationService.setForKey(key, translations = modifiedTranslations!!)
+    }
+
+    if (dtoTags !== null && areTagsModified) {
+      key.project.checkKeysEditPermission()
+      tagService.updateTags(key, dtoTags)
+    }
+
+    if (isKeyModified) {
+      key.project.checkKeysEditPermission()
+    }
+
+    if (isScreenshotAdded || isScreenshotDeleted) {
+      updateScreenshotsWithPermissionCheck(dto, key)
+    }
+
+    return keyWithDataModelAssembler.toModel(keyService.edit(key, dto.name))
+  }
+
+  private fun prepareData() {
+    key = keyService.get(keyId)
+    key.checkInProject()
+    modifiedTranslations = dto.translations?.let { dtoTranslations -> filterModifiedOnly(dtoTranslations, key) }
+  }
+
+  private fun prepareConditions() {
+    areTranslationsModified = !modifiedTranslations.isNullOrEmpty()
+    areTagsModified = dtoTags != null && areTagsModified(key, dtoTags)
+    isKeyModified = key.name != dto.name
+    isScreenshotDeleted = !dto.screenshotIdsToDelete.isNullOrEmpty()
+    isScreenshotAdded = !dto.screenshotUploadedImageIds.isNullOrEmpty()
+  }
+
+  private fun areTagsModified(
+    key: Key,
+    dtoTags: List<String>
+  ): Boolean {
+    val existingTagsContainAllNewTags = key.keyMeta?.tags?.map { it.name }?.containsAll(dtoTags) ?: false
+    val newTagsContainAllExistingTags = dtoTags.containsAll(key.keyMeta?.tags?.map { it.name } ?: listOf())
+
+    return !existingTagsContainAllNewTags || !newTagsContainAllExistingTags
+  }
+
+  private fun filterModifiedOnly(
+    dtoTranslations: Map<String, String?>,
+    key: Key?
+  ): Map<String, String?> {
+    val languages = languageService.findByTags(dtoTranslations.keys, projectHolder.project.id)
+    val existingTranslations = translationService.getKeyTranslations(
+      languages,
+      projectHolder.projectEntity,
+      key
+    ).associate { it.language.tag to it.text }
+
+    return dtoTranslations.filter { it.value != existingTranslations[it.key] }
+  }
+
+  private fun updateScreenshotsWithPermissionCheck(dto: ComplexEditKeyDto, key: Key) {
+    dto.screenshotIdsToDelete?.let { screenshotIds ->
+      deleteScreenshots(screenshotIds, key)
+    }
+
+    dto.screenshotUploadedImageIds?.let {
+      key.project.checkScreenshotsUploadPermission()
+      screenshotService.saveUploadedImages(it, key)
+    }
+  }
+
+  private fun deleteScreenshots(
+    screenshotIds: List<Long>,
+    key: Key
+  ) {
+    if (screenshotIds.isNotEmpty()) {
+      key.project.checkScreenshotsDeletePermission()
+    }
+    val screenshots = screenshotService.findByIdIn(screenshotIds).onEach {
+      if (it.key.id != key.id) {
+        throw BadRequestException(Message.SCREENSHOT_NOT_OF_KEY)
+      }
+    }
+    screenshotService.delete(screenshots)
+  }
+
+  private fun Project.checkScreenshotsDeletePermission() {
+    if (authenticationFacade.isApiKeyAuthentication) {
+      securityService.checkApiKeyScopes(setOf(ApiScope.SCREENSHOTS_DELETE), authenticationFacade.apiKey)
+    }
+    securityService.checkProjectPermission(this.id, Permission.ProjectPermissionType.TRANSLATE)
+  }
+
+  private fun Project.checkKeysEditPermission() {
+    if (authenticationFacade.isApiKeyAuthentication) {
+      securityService.checkApiKeyScopes(setOf(ApiScope.KEYS_EDIT), authenticationFacade.apiKey)
+    }
+    securityService.checkProjectPermission(this.id, Permission.ProjectPermissionType.EDIT)
+  }
+
+  private fun Key.checkInProject() {
+    keyService.checkInProject(this, projectHolder.project.id)
+  }
+
+  private fun Project.checkScreenshotsUploadPermission() {
+    securityService.checkScreenshotsUploadPermission(this.id)
+  }
+}
