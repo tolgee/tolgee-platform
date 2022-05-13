97cac44ff61b258c6171093ca222c5e0a69a2618
Jochen Munz <jochen.munz@ysura.com>
2022-05-13 11:06:12 +0200

    This adds the missing logo (thx!), and has `ktlintFormat` applied

--- backend/data/src/main/kotlin/io/tolgee/configuration/tolgee/machineTranslation/DeeplMachineTranslationProperties.kt
+++ backend/data/src/main/kotlin/io/tolgee/configuration/tolgee/machineTranslation/DeeplMachineTranslationProperties.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "tolgee.machine-translation.deepl")
 open class DeeplMachineTranslationProperties(
-    override var defaultEnabled: Boolean = true,
-    override var defaultPrimary: Boolean = false,
-    var authKey: String? = null,
-    var formality: String = "default"
+  override var defaultEnabled: Boolean = true,
+  override var defaultPrimary: Boolean = false,
+  var authKey: String? = null,
+  var formality: String = "default"
 ) : MachineTranslationServiceProperties
