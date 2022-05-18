dd2f956c65e85ae9eafe00696e776f605dcb6681
Štěpán Granát <granat.stepan@gmail.com>
2022-05-18 16:46:20 +0200

    fix: Dashboard faster loading, members link and user name layout

--- webapp/src/views/projects/dashboard/ActivityList.tsx
+++ webapp/src/views/projects/dashboard/ActivityList.tsx
@@ -35,7 +35,7 @@ const StyledScroller = styled('div')`
 
 const StyledList = styled('div')`
   display: grid;
-  grid-template-columns: minmax(auto, 30%) 1fr auto;
+  grid-template-columns: fit-content(30%) 1fr auto;
   gap: 3px 0px;
   padding-bottom: 12px;
 `;
