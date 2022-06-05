fae44490a757563450b2135f224da9a7b657e523
Johannes Waigel <waigel@klexhub.com>
2022-06-05 19:12:35 +0200

    feat: Implement OAuth2 login

--- webapp/src/component/security/LoginView.tsx
+++ webapp/src/component/security/LoginView.tsx
@@ -20,6 +20,7 @@ import { GlobalActions } from 'tg.store/global/GlobalActions';
 import {
   gitHubService,
   googleService,
+  oauth2Service,
   OAuthService,
 } from 'tg.component/security/OAuthService';
 
@@ -39,11 +40,26 @@ export const LoginView: FunctionComponent<LoginProps> = (props) => {
   const oAuthServices: OAuthService[] = [];
   const githubConfig = remoteConfig.authMethods?.github;
   const googleConfig = remoteConfig.authMethods?.google;
+  const oauth2Config = remoteConfig.authMethods?.oauth2;
   if (githubConfig?.enabled && githubConfig.clientId) {
     oAuthServices.push(gitHubService(githubConfig.clientId));
   }
   if (googleConfig?.enabled && googleConfig.clientId) {
     oAuthServices.push(googleService(googleConfig.clientId));
+  }
+  if (
+    oauth2Config?.enabled &&
+    oauth2Config?.clientId &&
+    oauth2Config.scopes &&
+    oauth2Config?.authorizationUrl
+  ) {
+    oAuthServices.push(
+      oauth2Service(
+        oauth2Config.clientId,
+        oauth2Config.authorizationUrl,
+        oauth2Config.scopes
+      )
+    );
   }
 
   const history = useHistory();
