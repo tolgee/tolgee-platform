bff4be2a6155043d668787aea8717374e73de13f
Štěpán Granát <granat.stepan@gmail.com>
2022-06-10 15:49:59 +0200

    feat: Add chatwoot to tolgee

--- webapp/src/service/apiSchema.generated.ts
+++ webapp/src/service/apiSchema.generated.ts
@@ -1210,7 +1210,6 @@ export interface components {
       page?: components["schemas"]["PageMetadata"];
     };
     EntityModelImportFileIssueView: {
-      params: components["schemas"]["ImportFileIssueParamView"][];
       id: number;
       type:
         | "KEY_IS_NOT_STRING"
@@ -1222,6 +1221,7 @@ export interface components {
         | "ID_ATTRIBUTE_NOT_PROVIDED"
         | "TARGET_NOT_PROVIDED"
         | "TRANSLATION_TOO_LONG";
+      params: components["schemas"]["ImportFileIssueParamView"][];
     };
     ImportFileIssueParamView: {
       value?: string;
@@ -1482,6 +1482,7 @@ export interface components {
       maxTranslationTextLength: number;
       recaptchaSiteKey?: string;
       openReplayApiKey?: string;
+      chatwootToken?: string;
     };
     SocketIo: {
       enabled: boolean;
