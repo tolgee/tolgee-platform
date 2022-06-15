33975eded8d538b1aa8dd68f64c2b4668678f7f7
Johannes Waigel <waigel@klexhub.com>
2022-06-15 07:51:13 +0200

    fix: Set content-type header and extend error logger

--- backend/app/src/main/kotlin/io/tolgee/security/third_party/OAuth2Delegate.kt
+++ backend/app/src/main/kotlin/io/tolgee/security/third_party/OAuth2Delegate.kt
@@ -11,11 +11,11 @@ import io.tolgee.security.JwtTokenProviderImpl
 import io.tolgee.security.payload.JwtAuthenticationResponse
 import io.tolgee.service.InvitationService
 import io.tolgee.service.UserAccountService
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
-import org.springframework.http.HttpStatus
+import org.slf4j.LoggerFactory
+import org.springframework.http.*
 import org.springframework.stereotype.Component
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
 import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.RestTemplate
 
@@ -28,14 +28,15 @@ class OAuth2Delegate(
   private val invitationService: InvitationService
 ) {
   private val oauth2ConfigurationProperties: OAuth2AuthenticationProperties = properties.authentication.oauth2
-
+  private val logger = LoggerFactory.getLogger(this::class.java)
   fun getTokenResponse(
     receivedCode: String?,
     @Nullable invitationCode: String?,
     redirectUri: String?
   ): JwtAuthenticationResponse {
     try {
-      val body = HashMap<String, String?>()
+
+      val body: MultiValueMap<String, String> = LinkedMultiValueMap<String, String>()
       body["client_id"] = oauth2ConfigurationProperties.clientId
       body["client_secret"] = oauth2ConfigurationProperties.clientSecret
       body["code"] = receivedCode
@@ -49,21 +50,44 @@ class OAuth2Delegate(
         throw AuthenticationException(Message.OAUTH2_USER_URL_NOT_SET)
       }
 
-      val response: MutableMap<*, *>? = restTemplate
-        .postForObject(oauth2ConfigurationProperties.tokenUrl!!, body, MutableMap::class.java)
+      val requestHeaders = HttpHeaders()
+      requestHeaders.contentType = MediaType.APPLICATION_FORM_URLENCODED
+
+      val response: MutableMap<*, *>? =
+        restTemplate.postForObject(
+          oauth2ConfigurationProperties.tokenUrl!!,
+          HttpEntity(body, requestHeaders),
+          MutableMap::class.java
+        )
 
       if (response != null && response.containsKey("access_token")) {
         val headers = HttpHeaders()
         headers["Authorization"] = "Bearer ${response["access_token"]}"
         val entity = HttpEntity<String?>(null, headers)
 
-        val exchange = restTemplate
-          .exchange(oauth2ConfigurationProperties.userUrl!!, HttpMethod.GET, entity, GenericUserResponse::class.java)
+        val exchange = restTemplate.exchange(
+          oauth2ConfigurationProperties.userUrl!!,
+          HttpMethod.GET,
+          entity,
+          GenericUserResponse::class.java
+        )
         if (exchange.statusCode != HttpStatus.OK || exchange.body == null) {
+          logger.error("Failed to get user info from OAuth2 provider")
           throw AuthenticationException(Message.THIRD_PARTY_UNAUTHORIZED)
         }
         val userResponse = exchange.body
-        val userAccountOptional = userAccountService.findByThirdParty("oauth2", userResponse!!.sub)
+
+        if (userResponse?.sub == null) {
+          logger.info("Third party 'sub' field is null.")
+          throw AuthenticationException(Message.THIRD_PARTY_AUTH_NO_SUB)
+        }
+
+        if (userResponse.email == null) {
+          logger.info("Third party user email is null. Missing scope email?")
+          throw AuthenticationException(Message.THIRD_PARTY_AUTH_NO_EMAIL)
+        }
+
+        val userAccountOptional = userAccountService.findByThirdParty("oauth2", userResponse.sub)
         val user = userAccountOptional.orElseGet {
           userAccountService.findOptional(userResponse.email).ifPresent {
             throw AuthenticationException(Message.USERNAME_ALREADY_EXISTS)
@@ -79,9 +103,17 @@ class OAuth2Delegate(
           }
 
           val newUserAccount = UserAccount()
-          newUserAccount.username = userResponse.email
-            ?: throw AuthenticationException(Message.THIRD_PARTY_AUTH_NO_EMAIL)
-          newUserAccount.name = userResponse.name ?: (userResponse.given_name + " " + userResponse.family_name)
+          newUserAccount.username =
+            userResponse.email ?: throw AuthenticationException(Message.THIRD_PARTY_AUTH_NO_EMAIL)
+
+          // build name for userAccount based on available fields by third party
+          var name = userResponse.email!!.split("@")[0]
+          if (userResponse.name != null) {
+            name = userResponse.name!!
+          } else if (userResponse.given_name != null && userResponse.family_name != null) {
+            name = "${userResponse.given_name} ${userResponse.family_name}"
+          }
+          newUserAccount.name = name
           newUserAccount.thirdPartyAuthId = userResponse.sub
           newUserAccount.thirdPartyAuthType = "oauth2"
           userAccountService.createUser(newUserAccount)
@@ -94,13 +126,16 @@ class OAuth2Delegate(
         return JwtAuthenticationResponse(jwt)
       }
       if (response == null) {
+        logger.error("Error getting token from third party server. Response is null.")
         throw AuthenticationException(Message.THIRD_PARTY_AUTH_UNKNOWN_ERROR)
       }
       if (response.containsKey("error")) {
+        logger.error("Error while getting token from third party: ${response["error"]}")
         throw AuthenticationException(Message.THIRD_PARTY_AUTH_ERROR_MESSAGE)
       }
       throw AuthenticationException(Message.THIRD_PARTY_AUTH_UNKNOWN_ERROR)
     } catch (e: HttpClientErrorException) {
+      logger.error("Error while getting token from third party: {}", e.message)
       throw AuthenticationException(Message.THIRD_PARTY_AUTH_UNKNOWN_ERROR)
     }
   }
