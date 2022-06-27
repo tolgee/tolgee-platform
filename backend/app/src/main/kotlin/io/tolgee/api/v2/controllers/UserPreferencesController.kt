50f04f8e8df41da649b0680b1568a42e49ac02af
Jan Cizmar <cizmar@chlupac.com>
2022-06-27 16:18:57 +0200

    chore: fix tests

--- backend/app/src/main/kotlin/io/tolgee/api/v2/controllers/UserPreferencesController.kt
+++ backend/app/src/main/kotlin/io/tolgee/api/v2/controllers/UserPreferencesController.kt
@@ -7,6 +7,7 @@ package io.tolgee.api.v2.controllers
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import io.tolgee.api.v2.hateoas.userPreferences.UserPreferencesModel
+import io.tolgee.security.AuthenticationFacade
 import io.tolgee.service.UserPreferencesService
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
@@ -20,12 +21,13 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping(value = ["/v2/user-preferences"])
 @Tag(name = "User preferences")
 class UserPreferencesController(
-  private val userPreferencesService: UserPreferencesService
+  private val userPreferencesService: UserPreferencesService,
+  private val authenticationFacade: AuthenticationFacade
 ) {
   @GetMapping("")
   @Operation(summary = "")
   fun get(): UserPreferencesModel {
-    return userPreferencesService.findOrCreate().let {
+    return userPreferencesService.findOrCreate(authenticationFacade.userAccount.id).let {
       UserPreferencesModel(language = it.language, preferredOrganizationId = it.preferredOrganization?.id)
     }
   }
@@ -35,6 +37,6 @@ class UserPreferencesController(
   fun setLanguage(
     @PathVariable languageTag: String
   ) {
-    userPreferencesService.setLanguage(languageTag)
+    userPreferencesService.setLanguage(languageTag, authenticationFacade.userAccountEntity)
   }
 }
