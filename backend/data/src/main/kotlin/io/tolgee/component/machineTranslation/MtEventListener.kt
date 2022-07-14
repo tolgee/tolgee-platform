b93fe641192ef826aba8adec6b47b77c249601e1
Jan Cizmar <cizmar@chlupac.com>
2022-07-14 13:50:58 +0200

    fix: Credit consumption when freeCreditAmount = -1

--- backend/data/src/main/kotlin/io/tolgee/component/machineTranslation/MtEventListener.kt
+++ backend/data/src/main/kotlin/io/tolgee/component/machineTranslation/MtEventListener.kt
@@ -1,5 +1,7 @@
 package io.tolgee.component.machineTranslation
 
+import io.tolgee.component.publicBillingConfProvider.PublicBillingConfProvider
+import io.tolgee.configuration.tolgee.machineTranslation.MachineTranslationProperties
 import io.tolgee.events.OnAfterMachineTranslationEvent
 import io.tolgee.events.OnBeforeMachineTranslationEvent
 import io.tolgee.service.machineTranslation.MtCreditBucketService
@@ -8,15 +10,25 @@ import org.springframework.stereotype.Component
 
 @Component
 class MtEventListener(
-  private val mtCreditBucketService: MtCreditBucketService
+  private val mtCreditBucketService: MtCreditBucketService,
+  private val machineTranslationProperties: MachineTranslationProperties,
+  private val publicBillingConfProvider: PublicBillingConfProvider
 ) {
   @EventListener(OnBeforeMachineTranslationEvent::class)
   fun onBeforeMtEvent(event: OnBeforeMachineTranslationEvent) {
-    mtCreditBucketService.consumeCredits(event.project, event.expectedSumPrice)
+    if (shouldConsumeCredits()) {
+      mtCreditBucketService.consumeCredits(event.project, event.expectedSumPrice)
+    }
   }
 
   @EventListener(OnAfterMachineTranslationEvent::class)
   fun onAfterMtEvent(event: OnAfterMachineTranslationEvent) {
-    mtCreditBucketService.addCredits(event.project, event.expectedSumPrice - event.actualSumPrice)
+    if (shouldConsumeCredits()) {
+      mtCreditBucketService.addCredits(event.project, event.expectedSumPrice - event.actualSumPrice)
+    }
+  }
+
+  fun shouldConsumeCredits(): Boolean {
+    return machineTranslationProperties.freeCreditsAmount > -1 || publicBillingConfProvider().enabled
   }
 }
