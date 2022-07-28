694370cbf634f54c3259f917b704ef480938ba02
Jan Cizmar <cizmar@chlupac.com>
2022-07-28 16:07:34 +0200

    feat: Search in organizations

--- backend/data/src/main/kotlin/io/tolgee/dtos/request/organization/OrganizationRequestParamsDto.kt
+++ backend/data/src/main/kotlin/io/tolgee/dtos/request/organization/OrganizationRequestParamsDto.kt
@@ -1,5 +1,6 @@
 package io.tolgee.dtos.request.organization
 
 data class OrganizationRequestParamsDto(
-  val filterCurrentUserOwner: Boolean = false
+  val filterCurrentUserOwner: Boolean = false,
+  val search: String? = ""
 )
