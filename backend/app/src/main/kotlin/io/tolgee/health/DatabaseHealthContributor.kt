546a05a49765b026263a0ecb897fe8c635f41af6
Jan Cizmar <cizmar@chlupac.com>
2022-10-26 13:19:57 +0200

    fix: Healthcheck endpoint checking DB connection (#1408)

---
+++ backend/app/src/main/kotlin/io/tolgee/health/DatabaseHealthContributor.kt
@@ -0,0 +1,28 @@
+package io.tolgee.health
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.HealthContributor
+import org.springframework.boot.actuate.health.HealthIndicator
+import org.springframework.stereotype.Component
+import java.sql.SQLException
+import java.sql.Statement
+import javax.sql.DataSource
+
+@Component("Database")
+class DatabaseHealthContributor : HealthIndicator, HealthContributor {
+  @Autowired
+  private lateinit var ds: DataSource
+
+  override fun health(): Health {
+    try {
+      ds.connection.use { conn ->
+        val stmt: Statement = conn.createStatement()
+        stmt.execute("select name from user_account limit 1")
+      }
+    } catch (ex: SQLException) {
+      return Health.outOfService().withException(ex).build()
+    }
+    return Health.up().build()
+  }
+}
