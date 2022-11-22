8c2d35e4c0166ceffe64ba7bc2a0d78de9e7d8fe
Jan Cizmar <cizmar@chlupac.com>
2022-11-22 07:25:08 +0100

    feat: Endpoint to get all keys (#1438)

---
+++ backend/app/src/test/kotlin/io/tolgee/api/v2/controllers/V2AllKeysControllerTest.kt
@@ -0,0 +1,43 @@
+package io.tolgee.api.v2.controllers
+
+import io.tolgee.controllers.ProjectAuthControllerTest
+import io.tolgee.development.testDataBuilder.data.KeysTestData
+import io.tolgee.fixtures.andAssertThatJson
+import io.tolgee.fixtures.andIsOk
+import io.tolgee.fixtures.andPrettyPrint
+import io.tolgee.fixtures.isValidId
+import io.tolgee.fixtures.node
+import io.tolgee.testing.annotations.ProjectJWTAuthTestMethod
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class V2AllKeysControllerTest : ProjectAuthControllerTest("/v2/projects/") {
+  lateinit var testData: KeysTestData
+
+  @BeforeEach
+  fun setup() {
+    testData = KeysTestData()
+    testDataService.saveTestData(testData.root)
+    userAccount = testData.user
+    this.projectSupplier = { testData.project }
+  }
+
+  @Test
+  @ProjectJWTAuthTestMethod
+  fun `returns all keys sorted`() {
+    performProjectAuthGet("all-keys").andPrettyPrint.andIsOk.andAssertThatJson {
+      node("_embedded.keys") {
+        isArray.hasSize(3)
+        node("[0]") {
+          node("id").isValidId
+          node("namespace").isNull()
+          node("name").isEqualTo("first_key")
+        }
+      }
+    }
+  }
+}
