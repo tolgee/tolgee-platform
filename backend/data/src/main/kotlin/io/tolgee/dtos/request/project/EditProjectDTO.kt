eeb46ed371c20e9803b095b8e36b062d8893bbf4
Štěpán Granát <granat.stepan@gmail.com>
2023-01-02 13:55:58 +0100

    feat: add project description - fix ktlint

--- backend/data/src/main/kotlin/io/tolgee/dtos/request/project/EditProjectDTO.kt
+++ backend/data/src/main/kotlin/io/tolgee/dtos/request/project/EditProjectDTO.kt
@@ -1,6 +1,5 @@
 package io.tolgee.dtos.request.project
 
-import io.tolgee.activity.annotation.ActivityLoggedProp
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Pattern
 import javax.validation.constraints.Size
