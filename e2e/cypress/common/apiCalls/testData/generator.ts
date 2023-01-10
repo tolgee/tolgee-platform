31f44e8b72dba34ca5fdcbeda21fc5260c0213ef
Štěpán Granát <granat.stepan@gmail.com>
2023-01-10 10:11:07 +0100

    fix: fixes after merge

--- e2e/cypress/common/apiCalls/testData/generator.ts
+++ e2e/cypress/common/apiCalls/testData/generator.ts
@@ -5,7 +5,10 @@ export const cleanTestData = (
   resource: string,
   options?: ArgumentTypes<typeof cy.request>[0]
 ) => {
-  return internalFetch(`e2e-data/${resource}/clean`, {timeout: 20000, ...options});
+  return internalFetch(`e2e-data/${resource}/clean`, {
+    timeout: 20000,
+    ...options,
+  });
 };
 
 export const generateTestData = (resource: string) => {
