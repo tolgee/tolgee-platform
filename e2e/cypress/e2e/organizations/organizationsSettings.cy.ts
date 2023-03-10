724fed0df91a19c76d163a85c1ee8276721f13b5
Štěpán Granát <granat.stepan@gmail.com>
2023-03-10 17:42:46 +0100

    chore: organization tests more organized

--- e2e/cypress/e2e/organizations/organizationsSettings.cy.ts
+++ e2e/cypress/e2e/organizations/organizationsSettings.cy.ts
@@ -9,10 +9,6 @@ import {
 } from '../../common/shared';
 import { login } from '../../common/apiCalls/common';
 import { organizationTestData } from '../../common/apiCalls/testData/testData';
-import {
-  permissionsMenuSelectAdvanced,
-  permissionsMenuSelectRole,
-} from '../../common/permissionsMenu';
 
 describe('Organization Settings', () => {
   beforeEach(() => {
@@ -47,38 +43,6 @@ describe('Organization Settings', () => {
     gcy('organization-description-field').within(() =>
       cy.get('input').should('have.value', newValues.description)
     );
-  });
-
-  it('changes member privileges', () => {
-    gcy('organization-side-menu').contains('Member permissions').click();
-    gcy('permissions-menu-button').click();
-    permissionsMenuSelectRole('Translate', { confirm: true });
-    visitMemberPrivileges();
-    gcy('permissions-menu-button').contains('Translate');
-  });
-
-  it("member privileges change doesn't affect profile", () => {
-    gcy('organization-side-menu').contains('Member permissions').click();
-    gcy('permissions-menu-button').click();
-    permissionsMenuSelectRole('Translate', { confirm: true });
-    visitProfile();
-    gcy('organization-name-field').within(() =>
-      cy.get('input').should('have.value', 'Tolgee')
-    );
-    gcy('organization-address-part-field').within(() =>
-      cy.get('input').should('have.value', 'tolgee')
-    );
-    gcy('organization-description-field').within(() =>
-      cy.get('input').should('have.value', 'This is us')
-    );
-  });
-
-  it('changes advanced permissions', () => {
-    gcy('organization-side-menu').contains('Member permissions').click();
-    gcy('permissions-menu-button').click();
-    permissionsMenuSelectAdvanced(['keys.create', 'keys.edit'], {
-      confirm: true,
-    });
   });
 
   it('Gates cannot change Tolgee settings', () => {
