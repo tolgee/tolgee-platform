623282b552a5125b382d43bcdf276f764321b6da
Jan Cizmar <cizmar@chlupac.com>
2023-04-26 18:38:29 +0200

    chore: Fix backend tests

--- backend/data/src/main/kotlin/io/tolgee/security/InternalController.kt
+++ backend/data/src/main/kotlin/io/tolgee/security/InternalController.kt
@@ -1,6 +1,3 @@
 package io.tolgee.security
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-
-@ConditionalOnProperty("tolgee.internal.controller-enabled")
 annotation class InternalController
