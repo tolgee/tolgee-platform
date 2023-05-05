3946c21374232ab9016678dfe2e110b26de9c7ab
Jan Cizmar <cizmar@chlupac.com>
2023-05-05 14:56:07 +0200

    fix: Bug fixes

--- backend/app/src/test/kotlin/io/tolgee/api/v2/controllers/organizationController/OrganizationProjectsControllerTest.kt
+++ backend/app/src/test/kotlin/io/tolgee/api/v2/controllers/organizationController/OrganizationProjectsControllerTest.kt
@@ -1,9 +1,12 @@
 package io.tolgee.api.v2.controllers.organizationController
 
+import io.tolgee.development.testDataBuilder.data.PermissionsTestData
 import io.tolgee.fixtures.andAssertThatJson
 import io.tolgee.fixtures.andIsOk
 import io.tolgee.fixtures.andPrettyPrint
 import io.tolgee.fixtures.node
+import io.tolgee.model.enums.OrganizationRoleType
+import io.tolgee.model.enums.ProjectPermissionType
 import io.tolgee.testing.AuthorizedControllerTest
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -75,5 +78,39 @@ class OrganizationProjectsControllerTest : AuthorizedControllerTest() {
           }
         }
     }
+  }
+
+  @Test
+  fun `user wint none permissions cannot see the the project`() {
+    val testData = PermissionsTestData()
+    val user = testData.addUserWithPermissions(type = ProjectPermissionType.NONE)
+    testDataService.saveTestData(testData.root)
+    userAccount = user
+    performAuthGet("/v2/organizations/${testData.organizationBuilder.self.id}/projects-with-stats")
+      .andAssertThatJson {
+        node("_embedded").isAbsent()
+      }
+  }
+
+  @Test
+  fun `user with no direct permission cannot see the the project in organization with none base permissions`() {
+    val testData = PermissionsTestData()
+    val user = testData.root.addUserAccount {
+      username = "pavek@stb.cz"
+    }.self
+    testData.organizationBuilder.self.basePermission.type = ProjectPermissionType.NONE
+    testData.organizationBuilder.build {
+      addRole {
+        this.user = user
+        this.type = OrganizationRoleType.MEMBER
+      }
+    }
+
+    testDataService.saveTestData(testData.root)
+    userAccount = user
+    performAuthGet("/v2/organizations/${testData.organizationBuilder.self.id}/projects-with-stats")
+      .andAssertThatJson {
+        node("_embedded").isAbsent()
+      }
   }
 }
