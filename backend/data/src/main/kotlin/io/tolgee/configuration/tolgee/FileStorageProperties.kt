3705a4620452e2ea822e482c005acf9e72b7d0be
Jan Cizmar <cizmar@chlupac.com>
2023-05-12 12:26:36 +0200

    feat: Configuration docs endpoint

--- backend/data/src/main/kotlin/io/tolgee/configuration/tolgee/FileStorageProperties.kt
+++ backend/data/src/main/kotlin/io/tolgee/configuration/tolgee/FileStorageProperties.kt
@@ -4,10 +4,12 @@
 
 package io.tolgee.configuration.tolgee
 
+import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "tolgee.file-storage")
 class FileStorageProperties(
   var s3: S3Settings = S3Settings(),
+  @Schema(description = "Path to directory where Tolgee will store its files.", defaultValue = "`~/.tolgee/`, with docker `/data/`")
   var fsDataPath: String = """${System.getProperty("user.home")}/.tolgee""",
 )
--- backend/data/src/main/kotlin/io/tolgee/configuration/tolgee/TolgeeProperties.kt
+++ backend/data/src/main/kotlin/io/tolgee/configuration/tolgee/TolgeeProperties.kt
@@ -4,12 +4,14 @@
 
 package io.tolgee.configuration.tolgee
 
+import io.swagger.v3.oas.annotations.media.Schema
 import io.tolgee.configuration.tolgee.machineTranslation.MachineTranslationProperties
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConfigurationProperties(prefix = "tolgee")
 @ConstructorBinding
+@Schema(description = "Configuration specific to Tolgee")
 open class TolgeeProperties(
   var authentication: AuthenticationProperties = AuthenticationProperties(),
   var smtp: SmtpProperties = SmtpProperties(),
@@ -18,13 +20,18 @@ open class TolgeeProperties(
   var capterraTracker: String? = null,
   var ga4Tag: String? = null,
   var internal: InternalProperties = InternalProperties(),
+  @Schema(description = "Public base path where files are accessible. Used by the user interface. (default: same origin, at the root).")
   var fileStorageUrl: String = "",
+  @Schema(description = "Maximum size of uploaded files (in kilobytes).", defaultValue = "`2048` ≈ 2MB")
   var maxUploadFileSize: Int = 51200,
+  @Schema(description = "Maximum amount of screenshots which can be uploaded per API key.")
   val maxScreenshotsPerKey: Int = 20,
   var fileStorage: FileStorageProperties = FileStorageProperties(),
+  @Schema(description = "Public URL where Tolgee is accessible. Used to generate links to Tolgee (e.g. email confirmation link).")
   var frontEndUrl: String? = null,
   var websocket: WebsocketProperties = WebsocketProperties(),
   var appName: String = "Tolgee",
+  @Schema(description = "Maximum length of translations.")
   open var maxTranslationTextLength: Long = 10000,
   var cache: CacheProperties = CacheProperties(),
   var recaptcha: ReCaptchaProperties = ReCaptchaProperties(),
