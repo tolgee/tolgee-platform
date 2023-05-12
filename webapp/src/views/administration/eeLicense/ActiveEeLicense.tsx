c50ff34f8b4269aec5f1d9cefb19e57cf7634c54
Štěpán Granát <granat.stepan@gmail.com>
2023-05-12 12:47:56 +0200

    feat: improve active license FE

--- webapp/src/views/administration/eeLicense/ActiveEeLicense.tsx
+++ webapp/src/views/administration/eeLicense/ActiveEeLicense.tsx
@@ -25,6 +25,7 @@ export const ActiveEeLicense = ({ info }: Props) => {
     <Plan>
       <PlanContent>
         <ActivePlanTitle name={info.name} status={info.status} />
+        <RefreshButton />
         <PlanInfoArea>
           {info.status === 'ACTIVE' ? (
             <IncludedFeatures features={info.enabledFeatures} />
@@ -32,15 +33,11 @@ export const ActiveEeLicense = ({ info }: Props) => {
             t('billing_subscriptions_ee_license_inactive')
           )}
         </PlanInfoArea>
-        <Box
-          display="flex"
-          gap={1}
-          gridArea="price / price / span 1 / span 2"
-          justifyContent="end"
-        >
-          <PlanLicenseKey licenseKey={info.licenseKey} defaultOpen={false} />
-          <RefreshButton />
+        <Box gridArea="price">
           <ReleaseKeyButton />
+        </Box>
+        <Box gridArea="action" display="flex" justifyContent="flex-end">
+          <PlanLicenseKey licenseKey={info.licenseKey} defaultOpen={false} />
         </Box>
       </PlanContent>
     </Plan>
