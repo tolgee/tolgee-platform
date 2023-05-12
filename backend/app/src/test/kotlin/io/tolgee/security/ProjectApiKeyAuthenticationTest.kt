a632574baba561e1f6960f7919653b58d64e37e4
Jan Cizmar <cizmar@chlupac.com>
2023-05-12 07:56:04 +0200

    chore: Fix backend tests

--- backend/app/src/test/kotlin/io/tolgee/security/ProjectApiKeyAuthenticationTest.kt
+++ backend/app/src/test/kotlin/io/tolgee/security/ProjectApiKeyAuthenticationTest.kt
@@ -1,27 +1,36 @@
 package io.tolgee.security
 
 import io.tolgee.API_KEY_HEADER_NAME
+import io.tolgee.component.CurrentDateProvider
 import io.tolgee.controllers.AbstractApiKeyTest
 import io.tolgee.development.testDataBuilder.data.ApiKeysTestData
 import io.tolgee.fixtures.andAssertThatJson
 import io.tolgee.fixtures.andIsForbidden
 import io.tolgee.fixtures.andIsOk
 import io.tolgee.fixtures.generateUniqueString
-import io.tolgee.fixtures.retry
 import io.tolgee.model.enums.Scope
 import io.tolgee.testing.assert
 import io.tolgee.testing.assertions.Assertions
 import io.tolgee.testing.assertions.UserApiAppAction
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
-import java.util.*
 
 @AutoConfigureMockMvc
-class ProjectApiKeyAuthenticationTest : AbstractApiKeyTest() {
+class ProjectApiKeyAuthenticationTest() : AbstractApiKeyTest() {
+
+  @Autowired
+  private lateinit var currentDateProvider: CurrentDateProvider
+
+  @Test
+  fun after() {
+    currentDateProvider.forcedDate = null
+  }
+
   @Test
   fun accessWithApiKey_failure() {
     val mvcResult = mvc.perform(MockMvcRequestBuilders.get("/uaa/en"))
@@ -59,21 +68,21 @@ class ProjectApiKeyAuthenticationTest : AbstractApiKeyTest() {
 
   @Test
   fun `works with tgpak_ prefix`() {
-    retry {
-      val testData = ApiKeysTestData()
-      testDataService.saveTestData(testData.root)
+    val testData = ApiKeysTestData()
+    testDataService.saveTestData(testData.root)
 
-      performGet(
-        "/v2/api-keys/current",
-        HttpHeaders().apply {
-          add(API_KEY_HEADER_NAME, "tgpak_" + testData.frantasKey.encodedKey)
-        }
-      ).andIsOk.andAssertThatJson {
-        node("description").isNotNull
+    currentDateProvider.forcedDate = currentDateProvider.date
+
+    performGet(
+      "/v2/api-keys/current",
+      HttpHeaders().apply {
+        add(API_KEY_HEADER_NAME, "tgpak_" + testData.frantasKey.encodedKey)
       }
-
-      apiKeyService.get(testData.frantasKey.id).lastUsedAt.assert.isNotNull.isBefore(Date())
+    ).andIsOk.andAssertThatJson {
+      node("description").isNotNull
     }
+
+    apiKeyService.get(testData.frantasKey.id).lastUsedAt!!.time.assert.isEqualTo(currentDateProvider.forcedDate!!.time)
   }
 
   @Test
