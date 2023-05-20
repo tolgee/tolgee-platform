b9c0b267daa1fad68a4a99ba4b0933c7aca7b55a
Jan Cizmar <cizmar@chlupac.com>
2023-05-20 10:31:03 +0200

    chore: More logs

--- backend/app/src/test/kotlin/io/tolgee/controllers/internal/SqlControllerTest.kt
+++ backend/app/src/test/kotlin/io/tolgee/controllers/internal/SqlControllerTest.kt
@@ -13,6 +13,9 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.web.method.HandlerMethod
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping
 
 @AutoConfigureMockMvc
 @ContextRecreatingTest
@@ -21,6 +24,21 @@ class SqlControllerTest : AbstractControllerTest(), Logging {
   @Suppress("RedundantModalityModifier")
   final inline fun <reified T> MvcResult.parseResponseTo(): T {
     return jacksonObjectMapper().readValue(this.response.contentAsString)
+  }
+
+  @Test
+  fun listEndpoints() {
+    val requestMappingHandlerMapping: RequestMappingHandlerMapping = applicationContext
+      .getBean("requestMappingHandlerMapping", RequestMappingHandlerMapping::class.java)
+    val map: Map<RequestMappingInfo, HandlerMethod> = requestMappingHandlerMapping
+      .handlerMethods
+    map.forEach { (key: RequestMappingInfo?, value: HandlerMethod?) ->
+      logger.info(
+        "{} {}",
+        key,
+        value
+      )
+    }
   }
 
   @Test
