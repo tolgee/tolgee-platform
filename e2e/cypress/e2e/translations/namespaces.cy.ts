caad1238d1ab6cdfa1a0f0b37d3b892af9b11b20
Štěpán Granát <granat.stepan@gmail.com>
2023-06-08 12:56:57 +0200

    chore: fix e2e tests

--- e2e/cypress/e2e/translations/namespaces.cy.ts
+++ e2e/cypress/e2e/translations/namespaces.cy.ts
@@ -31,6 +31,7 @@ describe('namespaces in translations', () => {
 
   afterEach(() => {
     namespaces.clean({ failOnStatusCode: false, timeout: 60000 });
+    cy.wait(10000);
   });
 
   it('displays keys with namespaces correctly', () => {
