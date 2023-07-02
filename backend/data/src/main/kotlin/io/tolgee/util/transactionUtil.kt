ce8fa39fc65717e96abaef1d3268572921cf8f1c
Jan Cizmar <cizmar@chlupac.com>
2023-07-02 07:08:30 +0200

    fix: Stats job

--- backend/data/src/main/kotlin/io/tolgee/util/transactionUtil.kt
+++ backend/data/src/main/kotlin/io/tolgee/util/transactionUtil.kt
@@ -1,8 +1,10 @@
 package io.tolgee.util
 
+import org.springframework.dao.CannotAcquireLockException
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.TransactionDefinition
 import org.springframework.transaction.support.TransactionTemplate
+import javax.persistence.OptimisticLockException
 
 fun <T> executeInNewTransaction(
   transactionManager: PlatformTransactionManager,
@@ -19,6 +21,31 @@ fun <T> executeInNewTransaction(
   } as T
 }
 
+fun <T> executeInNewRepeatableTransaction(
+  transactionManager: PlatformTransactionManager,
+  propagationBehavior: Int = TransactionDefinition.PROPAGATION_REQUIRES_NEW,
+  fn: () -> T
+): T {
+  var exception: Exception? = null
+  var repeats = 0
+  for (it in 1..100) {
+    try {
+      return executeInNewTransaction(transactionManager, propagationBehavior = propagationBehavior) {
+        fn()
+      }
+    } catch (e: Exception) {
+      when (e) {
+        is OptimisticLockException, is CannotAcquireLockException -> {
+          exception = e
+          repeats++
+        }
+        else -> throw e
+      }
+    }
+  }
+  throw RepeatedlyCannotSerializeTransactionException(exception!!, repeats)
+}
+
 fun <T> executeInNewTransaction(
   transactionManager: PlatformTransactionManager,
   fn: () -> T
@@ -29,3 +56,6 @@ fun <T> executeInNewTransaction(
     propagationBehavior = TransactionDefinition.PROPAGATION_REQUIRES_NEW
   )
 }
+
+class RepeatedlyCannotSerializeTransactionException(cause: Throwable, repeats: Int) :
+  RuntimeException("Retry failed $repeats times.", cause)
