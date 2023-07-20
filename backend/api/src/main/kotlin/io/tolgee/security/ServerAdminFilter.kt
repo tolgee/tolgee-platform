37201a203708b8944ec71a5b044e3895a9258a19
Štěpán Granát <granat.stepan@gmail.com>
2023-07-20 13:04:31 +0200

    chore: fix static checks

--- backend/api/src/main/kotlin/io/tolgee/security/ServerAdminFilter.kt
+++ backend/api/src/main/kotlin/io/tolgee/security/ServerAdminFilter.kt
@@ -13,7 +13,6 @@ import javax.servlet.ServletException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-
 @Component
 class ServerAdminFilter @Autowired constructor(
   private val securityService: SecurityService,
