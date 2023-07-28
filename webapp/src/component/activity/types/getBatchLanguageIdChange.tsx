6f967dd4a9d6208a7979b3e19f4ba342fcbb709e
Štěpán Granát <granat.stepan@gmail.com>
2023-07-28 17:13:10 +0200

    feat: batch operations activity

--- webapp/src/component/activity/types/getBatchLanguageIdChange.tsx
+++ webapp/src/component/activity/types/getBatchLanguageIdChange.tsx
@@ -3,19 +3,24 @@ import { styled } from '@mui/material';
 import { useProjectLanguages } from 'tg.hooks/useProjectLanguages';
 import { CircledLanguageIcon } from 'tg.component/languages/CircledLanguageIcon';
 
-const StyledContainer = styled('div')`
-  display: flex;
-  gap: 8px;
+const StyledContainer = styled('span')`
+  display: div;
 `;
 
-const StyledLanguage = styled('div')`
-  display: flex;
-  align-items: center;
+const StyledLanguage = styled('span')`
   gap: 4px;
   background: ${({ theme }) => theme.palette.emphasis[200]};
   border-radius: 4px;
   border: 1px solid ${({ theme }) => theme.palette.emphasis[300]};
   padding: 0px 4px;
+  & + & {
+    margin-left: 4px;
+  }
+`;
+
+const StyledName = styled('span')`
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 type Props = {
@@ -26,14 +31,25 @@ const LanguageIdsComponent: React.FC<Props> = ({ input }) => {
   const allLangs = useProjectLanguages();
   const langId = input.new;
   const language = allLangs.find((lang) => lang.id === langId);
-  return (
-    <StyledContainer>
-      <StyledLanguage key={langId}>
-        {language?.name && <span>{language.name}</span>}
-        <CircledLanguageIcon size={14} flag={language?.flagEmoji} />
-      </StyledLanguage>
-    </StyledContainer>
-  );
+  if (langId) {
+    return (
+      <StyledContainer>
+        <StyledLanguage key={langId}>
+          {language?.name && <StyledName>{language.name}</StyledName>}
+          <CircledLanguageIcon
+            size={14}
+            flag={language?.flagEmoji}
+            display="inline-block"
+            position="relative"
+            top="3px"
+            marginLeft="2px"
+          />
+        </StyledLanguage>
+      </StyledContainer>
+    );
+  } else {
+    return null;
+  }
 };
 
 export const getBatchLanguageIdChange = (input: DiffValue<number>) => {
