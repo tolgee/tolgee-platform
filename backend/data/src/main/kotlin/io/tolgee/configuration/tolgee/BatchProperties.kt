7e27c171f1c9a1742253e324d603c70d5ed5d1d0
Martin Chrástek <chrastek12@gmail.com>
2023-08-01 11:55:55 +0200

    feat: Documentation & translation improvements

--- backend/data/src/main/kotlin/io/tolgee/configuration/tolgee/BatchProperties.kt
+++ backend/data/src/main/kotlin/io/tolgee/configuration/tolgee/BatchProperties.kt
@@ -4,6 +4,7 @@ import io.tolgee.configuration.annotations.DocProperty
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "tolgee.batch")
+@DocProperty(description = "Configuration of batch operations.", displayName = "Batch operations")
 class BatchProperties {
   @DocProperty(description = "How many parallel jobs can be run at once on single Tolgee instance")
   var concurrency: Int = 10
