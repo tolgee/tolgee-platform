61787a86ae95715f340f40e87dc3c5625c4d0613
Štěpán Granát <granat.stepan@gmail.com>
2023-08-07 17:31:11 +0200

    feat: auto translate batch operation

--- webapp/src/translationTools/useBatchOperationTypeTranslation.ts
+++ webapp/src/translationTools/useBatchOperationTypeTranslation.ts
@@ -26,6 +26,8 @@ export function useBatchOperationTypeTranslate() {
         return t('batch_operation_type_tag_keys');
       case 'UNTAG_KEYS':
         return t('batch_operation_type_untag_keys');
+      case 'AUTO_TRANSLATE':
+        return t('batch_operation_type_auto_translate');
       default:
         return type;
     }
