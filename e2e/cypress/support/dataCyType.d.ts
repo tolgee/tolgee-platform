d1b75ee2b30d54f9903658087bc7799eeb8d339f
Jan Cizmar <cizmar@chlupac.com>
2023-08-22 16:50:16 +0200

    fix: Clean job queue using scheduled task

--- e2e/cypress/support/dataCyType.d.ts
+++ e2e/cypress/support/dataCyType.d.ts
@@ -443,7 +443,4 @@ declare namespace DataCy {
         "user-menu-server-administration" |
         "user-menu-user-settings" |
         "user-profile"
-}ile"
-}r-settings" |
-        "user-profile"
 }
