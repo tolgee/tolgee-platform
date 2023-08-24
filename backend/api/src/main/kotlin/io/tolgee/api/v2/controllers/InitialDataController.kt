70ea704020c7a2b5429216af2c546f4e250b8559
Štěpán Granát <granat.stepan@gmail.com>
2023-08-24 15:57:55 +0200

    feat: announcement banner

--- backend/api/src/main/kotlin/io/tolgee/api/v2/controllers/InitialDataController.kt
+++ backend/api/src/main/kotlin/io/tolgee/api/v2/controllers/InitialDataController.kt
@@ -31,7 +31,7 @@ class InitialDataController(
   private val preferredOrganizationFacade: PreferredOrganizationFacade,
   private val eeSubscriptionModelAssembler: EeSubscriptionModelAssembler,
   private val eeSubscriptionService: EeSubscriptionService,
-  private val announcementService: AnnouncementService
+  private val announcementController: AnnouncementController,
 ) : IController {
   @GetMapping(value = [""])
   @Operation(description = "Returns initial data always required by frontend")
@@ -47,7 +47,7 @@ class InitialDataController(
       data.userInfo = userController.getInfo()
       data.preferredOrganization = preferredOrganizationFacade.getPreferred()
       data.languageTag = userPreferencesService.find(userAccount.id)?.language
-      data.announcement = announcementService.getAnnouncement()?.let { AnnouncementDto.fromEntity(it) }
+      data.announcement = announcementController.getLatest()
     }
 
     return data
