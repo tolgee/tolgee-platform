ade0c6c98c70f208f62f580422d9ee554d95ac79
Jan Cizmar <cizmar@chlupac.com>
2023-08-28 10:18:26 +0200

    chore: Refactor Announcement.kt

--- backend/data/src/main/kotlin/io/tolgee/model/enums/Announcement.kt
+++ backend/data/src/main/kotlin/io/tolgee/model/enums/Announcement.kt
@@ -3,11 +3,12 @@ package io.tolgee.model.enums
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
-fun parseTime(datetime: String): Long {
-  val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm z")
-  return ZonedDateTime.parse(datetime, formatter).toInstant().toEpochMilli()
-}
-
+@Suppress("unused")
 enum class Announcement(val until: Long) {
   FEATURE_BATCH_OPERATIONS(parseTime("2023-09-10 00:00 UTC")),
+}
+
+private fun parseTime(datetime: String): Long {
+  val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm z")
+  return ZonedDateTime.parse(datetime, formatter).toInstant().toEpochMilli()
 }
