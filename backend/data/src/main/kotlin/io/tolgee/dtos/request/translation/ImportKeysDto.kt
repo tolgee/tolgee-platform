c52eddabb87199f845b0812c0cb56d006490998b
Jan Cizmar <cizmar@chlupac.com>
2023-08-31 15:58:35 +0200

    fix: StackOverflowError in bigmeta & validate keys import (#1882)
    - fix: StackOverflowError when too many related keys for big meta
    provided
    - fix: Validate keys import data properly

--- backend/data/src/main/kotlin/io/tolgee/dtos/request/translation/ImportKeysDto.kt
+++ backend/data/src/main/kotlin/io/tolgee/dtos/request/translation/ImportKeysDto.kt
@@ -1,8 +1,10 @@
 package io.tolgee.dtos.request.translation
 
 import org.springframework.validation.annotation.Validated
+import javax.validation.Valid
 
 @Validated
 data class ImportKeysDto(
+  @field:Valid
   val keys: List<ImportKeysItemDto> = listOf()
 )
