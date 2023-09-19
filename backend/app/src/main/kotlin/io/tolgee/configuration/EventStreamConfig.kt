bb8fcfa40bac47afd2cac4d6eb5ad3b09d7048a7
Jan Cizmar <cizmar@chlupac.com>
2023-09-19 06:52:49 +0200

    fix: Error message conversion issues

--- backend/app/src/main/kotlin/io/tolgee/configuration/EventStreamConfig.kt
+++ backend/app/src/main/kotlin/io/tolgee/configuration/EventStreamConfig.kt
@@ -5,26 +5,22 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
 import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter
-import org.springframework.stereotype.Component
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class EventStreamConfig(
-  private val eventStreamHttpMessageConverter: EventStreamHttpMessageConverter,
-  private val javascriptHttpMessageConverter: JavascriptHttpMessageConverter
+  private val objectMapper: ObjectMapper
 ) : WebMvcConfigurer {
   override fun configureMessageConverters(converters: MutableList<HttpMessageConverter<*>?>) {
-    converters.add(eventStreamHttpMessageConverter)
-    converters.add(javascriptHttpMessageConverter)
+    converters.add(EventStreamHttpMessageConverter(objectMapper))
+    converters.add(JavascriptHttpMessageConverter(objectMapper))
   }
 }
 
-@Component
 class EventStreamHttpMessageConverter(
   objectMapper: ObjectMapper
 ) : AbstractJackson2HttpMessageConverter(objectMapper, MediaType.TEXT_EVENT_STREAM)
 
-@Component
 class JavascriptHttpMessageConverter(
   objectMapper: ObjectMapper
 ) : AbstractJackson2HttpMessageConverter(objectMapper, MediaType("application", "javascript"))
