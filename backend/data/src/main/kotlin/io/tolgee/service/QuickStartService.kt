d818df902498fbac7429e946cf74850e9cb6af72
Jan Cizmar <cizmar@chlupac.com>
2023-09-25 15:07:19 +0200

    feat: Demo project

--- backend/data/src/main/kotlin/io/tolgee/service/QuickStartService.kt
+++ backend/data/src/main/kotlin/io/tolgee/service/QuickStartService.kt
@@ -1,17 +1,27 @@
 package io.tolgee.service
 
+import io.tolgee.component.demoProject.DemoProjectCreator
 import io.tolgee.dtos.cacheable.UserAccountDto
 import io.tolgee.model.Organization
 import io.tolgee.model.QuickStart
 import io.tolgee.model.UserAccount
 import io.tolgee.repository.QuickStartRepository
+import org.springframework.context.ApplicationContext
 import org.springframework.data.crossstore.ChangeSetPersister
 import org.springframework.stereotype.Component
 
 @Component
 class QuickStartService(
   private val quickStartRepository: QuickStartRepository,
+  private val applicationContext: ApplicationContext
 ) {
+  fun create(userAccount: UserAccount, organization: Organization) {
+    val quickStart = QuickStart(userAccount)
+    quickStart.organization = organization
+    quickStartRepository.save(quickStart)
+    DemoProjectCreator(organization, applicationContext).createDemoProject()
+  }
+
   fun completeStep(userAccount: UserAccountDto, step: String): QuickStart? {
     val quickStart = quickStartRepository.findByUserAccountId(userAccount.id)
     if (quickStart?.completedSteps?.let { !it.contains(step) } == true) {
@@ -35,12 +45,6 @@ class QuickStartService(
     quickStart.open = open
     quickStartRepository.save(quickStart)
     return quickStart
-  }
-
-  fun create(userAccount: UserAccount, organization: Organization) {
-    val quickStart = QuickStart(userAccount)
-    quickStart.organization = organization
-    quickStartRepository.save(quickStart)
   }
 
   fun find(userAccountId: Long, organizationId: Long?): QuickStart? {
