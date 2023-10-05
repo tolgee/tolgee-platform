af71989708226f78b13bdf45cb213b0d2749d460
Cynthia <cynthia@cynthia.dev>
2023-10-05 16:26:05 +0200

    fix: handle RedisException gracefully when retrieving cached data

---
+++ backend/api/src/main/kotlin/io/tolgee/configuration/CacheConfiguration.kt
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) 2023 Tolgee s.r.o. and contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.tolgee.configuration
+
+import io.tolgee.component.TolgeeCacheErrorHandler
+import org.springframework.cache.annotation.CachingConfigurerSupport
+import org.springframework.cache.interceptor.CacheErrorHandler
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class CacheConfiguration : CachingConfigurerSupport() {
+  override fun errorHandler(): CacheErrorHandler {
+    return TolgeeCacheErrorHandler()
+  }
+}
