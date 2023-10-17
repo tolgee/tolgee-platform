ede4be381575860d0390197daa72662cd4d53ae6
Jan Cizmar <cizmar@chlupac.com>
2023-10-17 16:12:39 +0200

    fix: Activity interceptors

--- backend/data/src/main/kotlin/io/tolgee/configuration/tolgee/HibernateConfiguration.kt
+++ backend/data/src/main/kotlin/io/tolgee/configuration/tolgee/HibernateConfiguration.kt
@@ -3,7 +3,11 @@ package io.tolgee.configuration.tolgee
 import io.tolgee.activity.iterceptor.ActivityDatabaseInterceptor
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean
+import javax.sql.DataSource
+
 
 @Configuration
 class HibernateConfiguration : HibernatePropertiesCustomizer {
@@ -12,6 +16,7 @@ class HibernateConfiguration : HibernatePropertiesCustomizer {
   lateinit var activityInterceptor: ActivityDatabaseInterceptor
 
   override fun customize(vendorProperties: MutableMap<String, Any?>) {
-    vendorProperties["hibernate.ejb.interceptor"] = activityInterceptor
+    vendorProperties["hibernate.session_factory.interceptor"] = activityInterceptor
   }
+
 }
