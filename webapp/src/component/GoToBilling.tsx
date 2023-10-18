efc2cf02444be65536a3b80191c6436dc6aab042
Štěpán Granát <granat.stepan@gmail.com>
2023-10-18 16:36:33 +0200

    fix: improve out of credit message in machine translations

---
+++ webapp/src/component/GoToBilling.tsx
@@ -0,0 +1,25 @@
+import { Button } from '@mui/material';
+import { LINKS } from 'tg.constants/links';
+import { useGlobalContext } from 'tg.globalContext/GlobalContext';
+
+type Props = {
+  render: (props: {
+    href: string;
+    rel: string;
+    target: string;
+  }) => React.ReactElement;
+};
+
+export const GoToBilling = ({ render }: Props) => {
+  const billingEnabled = useGlobalContext(
+    (c) => c.serverConfiguration.billing.enabled
+  );
+  if (!billingEnabled) {
+    return null;
+  }
+  return render({
+    href: LINKS.GO_TO_CLOUD_BILLING.build(),
+    rel: 'noopener noreferrer',
+    target: '_blank',
+  });
+};
