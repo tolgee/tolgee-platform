57909b5b715300f88caa157fe90616280108b46f
Jan Cizmar <cizmar@chlupac.com>
2023-10-20 15:07:36 +0200

    fix: Null precedence problems

--- backend/data/src/main/kotlin/io/tolgee/service/query_builders/translationViewBuilder/TranslationsViewQueryBuilder.kt
+++ backend/data/src/main/kotlin/io/tolgee/service/query_builders/translationViewBuilder/TranslationsViewQueryBuilder.kt
@@ -4,6 +4,8 @@ import io.tolgee.dtos.request.translation.TranslationFilters
 import io.tolgee.dtos.response.CursorValue
 import io.tolgee.model.*
 import jakarta.persistence.criteria.*
+import org.hibernate.query.NullPrecedence
+import org.hibernate.query.sqm.tree.select.SqmSortSpecification
 import org.springframework.data.domain.*
 import java.util.*
 
@@ -32,17 +34,7 @@ class TranslationsViewQueryBuilder(
       val queryBase = getBaseQuery(query)
       val paths = queryBase.querySelection.values.toTypedArray()
       query.multiselect(*paths)
-      val orderList = sort.asSequence().filter { queryBase.querySelection[it.property] != null }.map {
-        val expression = queryBase.querySelection[it.property] as Expression<*>
-        when (it.direction) {
-          Sort.Direction.DESC -> cb.desc(expression)
-          else -> cb.asc(expression)
-        }
-      }.toMutableList()
-
-      if (orderList.isEmpty()) {
-        orderList.add(cb.asc(queryBase.keyNameExpression))
-      }
+      val orderList = getOrderList(queryBase)
       val where = queryBase.whereConditions.toMutableList()
 
       val cursorPredicateProvider = CursorPredicateProvider(cb, cursor, queryBase.querySelection)
@@ -55,6 +47,31 @@ class TranslationsViewQueryBuilder(
       query.orderBy(orderList)
       return query
     }
+
+  private fun getOrderList(queryBase: QueryBase<Array<Any?>>): MutableList<Order> {
+    val orderList = sort.asSequence().filter { queryBase.querySelection[it.property] != null }.map {
+      val expression = queryBase.querySelection[it.property] as Expression<*>
+      when (it.direction) {
+        Sort.Direction.DESC -> cb.desc(expression)
+        else -> cb.asc(expression)
+      }
+    }.toMutableList()
+
+    if (orderList.isEmpty()) {
+      orderList.add(cb.asc(queryBase.keyNameExpression))
+    }
+
+    orderList.forEach {
+      (it as? SqmSortSpecification)?.let { sortSpec ->
+        when {
+          sortSpec.isAscending -> sortSpec.nullPrecedence(NullPrecedence.FIRST)
+          else -> sortSpec.nullPrecedence(NullPrecedence.LAST)
+        }
+      }
+    }
+
+    return orderList
+  }
 
   val countQuery: CriteriaQuery<Long>
     get() {
