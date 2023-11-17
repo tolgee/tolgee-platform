6ac7d4aeac400ca4b9666e2b86775f85493e5aa0
Jan Cizmar <cizmar@chlupac.com>
2023-11-17 14:32:35 +0100

    fix: Handle storage errors

--- backend/data/src/main/kotlin/io/tolgee/component/automations/processors/CdnPublishProcessor.kt
+++ backend/data/src/main/kotlin/io/tolgee/component/automations/processors/CdnPublishProcessor.kt
@@ -1,7 +1,10 @@
 package io.tolgee.component.automations.processors
 
+import io.tolgee.batch.RequeueWithDelayException
 import io.tolgee.component.automations.AutomationProcessor
 import io.tolgee.component.cdn.CdnUploader
+import io.tolgee.constants.Message
+import io.tolgee.exceptions.FileStoreException
 import io.tolgee.model.automations.AutomationAction
 import io.tolgee.security.ProjectHolder
 import io.tolgee.service.security.SecurityService
@@ -14,7 +17,17 @@ class CdnPublishProcessor(
   val projectHolder: ProjectHolder,
 ) : AutomationProcessor {
   override fun process(action: AutomationAction, activityRevisionId: Long?) {
-    val exporter = action.cdn ?: throw IllegalStateException("Wrong params passed to cdn publish processor")
-    cdnUploader.upload(cdnId = exporter.id)
+    try {
+      val exporter = action.cdn ?: throw IllegalStateException("Wrong params passed to cdn publish processor")
+      cdnUploader.upload(cdnId = exporter.id)
+    } catch (e: Throwable) {
+      when (e) {
+        is FileStoreException -> throw RequeueWithDelayException(
+          Message.CANNOT_STORE_FILE_TO_CONTENT_STORAGE,
+          cause = e,
+        )
+        else -> throw RequeueWithDelayException(Message.UNEXPECTED_ERROR_WHILE_PUBLISHING_TO_CDN, cause = e)
+      }
+    }
   }
 }
