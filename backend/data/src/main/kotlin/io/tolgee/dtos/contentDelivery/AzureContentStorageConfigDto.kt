14e7bd741efc036aa58fc4fd08dcd2dc7c8318e8
Jan Cizmar <cizmar@chlupac.com>
2023-11-23 10:38:31 +0100

    fix: Add validation constraints

--- backend/data/src/main/kotlin/io/tolgee/dtos/contentDelivery/AzureContentStorageConfigDto.kt
+++ backend/data/src/main/kotlin/io/tolgee/dtos/contentDelivery/AzureContentStorageConfigDto.kt
@@ -2,10 +2,14 @@ package io.tolgee.dtos.contentDelivery
 
 import io.tolgee.model.contentDelivery.AzureBlobConfig
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
 
 class AzureContentStorageConfigDto : AzureBlobConfig {
+  @field:NotBlank
+  @field:Size(max = 255)
   override var connectionString: String? = ""
 
   @field:NotBlank
+  @field:Size(max = 255)
   override var containerName: String? = ""
 }
--- backend/data/src/main/kotlin/io/tolgee/dtos/contentDelivery/ContentStorageRequest.kt
+++ backend/data/src/main/kotlin/io/tolgee/dtos/contentDelivery/ContentStorageRequest.kt
@@ -2,13 +2,16 @@ package io.tolgee.dtos.contentDelivery
 
 import javax.validation.Valid
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
 
 data class ContentStorageRequest(
-  @NotBlank
+  @field:NotBlank
+  @field:Size(max = 100)
   val name: String,
   @field:Valid
   val azureContentStorageConfig: AzureContentStorageConfigDto?,
   @field:Valid
   val s3ContentStorageConfig: S3ContentStorageConfigDto?,
+  @field:Size(max = 255)
   val publicUrlPrefix: String? = null
 )
