2518743270eb4fd2ffc973591a8724806186fafe
Jan Cizmar <cizmar@chlupac.com>
2023-11-24 18:28:12 +0100

    fix: Wrong content type in responses

--- backend/app/src/main/kotlin/io/tolgee/configuration/WebMvcConfiguration.kt
+++ backend/app/src/main/kotlin/io/tolgee/configuration/WebMvcConfiguration.kt
@@ -5,7 +5,6 @@ import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-
 @Configuration
 class WebMvcConfiguration : WebMvcConfigurer {
   override fun configureMessageConverters(converters: MutableList<HttpMessageConverter<*>?>) {
