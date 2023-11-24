1add367587d85b31a1e9484f29ad38bf95ed8858
Jan Cizmar <cizmar@chlupac.com>
2023-11-24 12:35:43 +0100

    fix: Add jacksonDataFormatXml

--- backend/data/src/main/kotlin/io/tolgee/configuration/tolgee/ContentStorageAzureProperties.kt
+++ backend/data/src/main/kotlin/io/tolgee/configuration/tolgee/ContentStorageAzureProperties.kt
@@ -3,7 +3,7 @@ package io.tolgee.configuration.tolgee
 import io.tolgee.model.contentDelivery.AzureBlobConfig
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties(prefix = "tolgee.content-delivery.azure")
+@ConfigurationProperties(prefix = "tolgee.content-delivery.storage.azure")
 class ContentStorageAzureProperties : AzureBlobConfig {
   override var connectionString: String? = null
   override var containerName: String? = null
