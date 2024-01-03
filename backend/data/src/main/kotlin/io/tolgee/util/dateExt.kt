82c8807ea16ddc4f1707db6855e1c34eada2fb0f
Jan Cizmar <cizmar@chlupac.com>
2024-01-03 17:34:59 +0100

    fix: Invoicing

--- backend/data/src/main/kotlin/io/tolgee/util/dateExt.kt
+++ backend/data/src/main/kotlin/io/tolgee/util/dateExt.kt
@@ -1,5 +1,7 @@
 package io.tolgee.util
 
+import org.apache.commons.lang3.ObjectUtils.max
+import org.apache.commons.lang3.ObjectUtils.min
 import java.util.*
 
 fun Date.addMonths(months: Int): Date {
@@ -30,6 +32,18 @@ private fun Date.toUtcCalendar(): Calendar {
   val calendar = getUtcCalendar()
   calendar.time = this
   return calendar
+}
+
+fun Date.fullMonthsDiff(date: Date): Int {
+  val first = min(this, date)
+  val second = max(this, date)
+
+  var fullMonths = 0
+  while (first.addMonths(fullMonths) < second) {
+    fullMonths++
+  }
+
+  return fullMonths
 }
 
 private fun getUtcCalendar(): Calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
