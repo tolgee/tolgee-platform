0f5708e52f0a717f5b4306f173e76de158064595
Štěpán Granát <granat.stepan@gmail.com>
2024-01-03 10:00:58 +0100

    chore: fix e2e and typescript

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -32,6 +32,6 @@ export default defineConfig({
     // this ensures that the browser opens upon server start
     open: true,
     // this sets a default port to 3000
-    port: 3000,
+    port: Number(process.env.PORT) || 3000,
   },
 });
