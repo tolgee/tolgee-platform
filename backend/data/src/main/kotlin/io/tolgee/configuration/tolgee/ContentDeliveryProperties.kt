bb0b2f0620bc5f3ce51034f50113a1da1a38866a
Jan Cizmar <cizmar@chlupac.com>
2024-01-04 16:59:36 +0100

    chore: Documentation typo

--- backend/data/src/main/kotlin/io/tolgee/configuration/tolgee/ContentDeliveryProperties.kt
+++ backend/data/src/main/kotlin/io/tolgee/configuration/tolgee/ContentDeliveryProperties.kt
@@ -13,12 +13,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 class ContentDeliveryProperties {
   @DocProperty(
     description =
-      "The root path where the content is accessible by the public. If this property is not null. " +
-        "Content delivery will be enabled on your server.",
+      "The root path where the content is accessible by the public. If this property is not null, " +
+        "content delivery will be enabled on your server.",
   )
   var publicUrlPrefix: String? = null
 
-  @DocProperty(description = "Configuration of the storage. You have to configure exactly one storage")
+  @DocProperty(description = "Configuration of the storage. You have to configure exactly one storage.")
   var storage: ContentStorageProperties = ContentStorageProperties()
 
   @DocProperty(hidden = true)
