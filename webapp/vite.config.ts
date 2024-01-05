a9b3b8b2dffdd8c219fb4f4e9030fde0eaae6de7
Jan Cizmar <cizmar@chlupac.com>
2024-01-05 15:42:53 +0100

    feat: Enable Tolgee AI translator on self-hosted -> Basic tests

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -13,7 +13,6 @@ import { loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
   return {
     // depending on your application, base can also be "/"
     base: '/',
