f875f15496c678783064978fb271b5acb74474c0
Jan Cizmar <cizmar@chlupac.com>
2024-01-15 19:11:30 +0100

    fix: Cache all Tolgee translator EE

--- backend/data/src/main/kotlin/io/tolgee/component/cacheWithExpiration/CacheWithExpiration.kt
+++ backend/data/src/main/kotlin/io/tolgee/component/cacheWithExpiration/CacheWithExpiration.kt
@@ -2,6 +2,7 @@ package io.tolgee.component.cacheWithExpiration
 
 import io.tolgee.component.CurrentDateProvider
 import org.springframework.cache.Cache
+import org.springframework.cache.Cache.ValueWrapper
 import java.time.Duration
 
 class CacheWithExpiration(
@@ -10,17 +11,34 @@ class CacheWithExpiration(
 ) {
   fun <T : Any?> get(key: Any): T? {
     this.cache.get(key, CachedWithExpiration::class.java)?.let {
-      if (it.expiresAt > currentDateProvider.date.time) {
-        try {
-          @Suppress("UNCHECKED_CAST")
-          return it.data as? T
-        } catch (e: ClassCastException) {
-          this.cache.evict(key)
-          return null
-        }
-      }
-      this.cache.evict(key)
+      return getNotExpiredValue(key, it)
     }
+    return null
+  }
+
+  fun getWrapper(key: Any): ValueWrapper? {
+    this.cache.get(key)?.let { valueWrapper ->
+      val it = valueWrapper.get() as CachedWithExpiration? ?: return ValueWrapper { null }
+      val value = getNotExpiredValue<Any>(key, it)
+      return ValueWrapper { value }
+    }
+    return null
+  }
+
+  private fun <T> getNotExpiredValue(
+    key: Any,
+    it: CachedWithExpiration,
+  ): T? {
+    if (it.expiresAt > currentDateProvider.date.time) {
+      try {
+        @Suppress("UNCHECKED_CAST")
+        return it.data as? T
+      } catch (e: ClassCastException) {
+        this.cache.evict(key)
+        return null
+      }
+    }
+    this.cache.evict(key)
     return null
   }
 
