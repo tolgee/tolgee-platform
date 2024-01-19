ae6bcae04f03afe6546cec8f3fa3ab8688da021b
Jan Cizmar <cizmar@chlupac.com>
2024-01-19 10:44:02 +0100

    chore: Set front-end port via .env (#2086)

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
