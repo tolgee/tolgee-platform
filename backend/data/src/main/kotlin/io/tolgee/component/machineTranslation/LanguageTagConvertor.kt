62719bbaca7dd243039006e27fb87b781a871c59
Jan Cizmar <cizmar@chlupac.com>
2024-01-22 14:32:26 +0100

    fix: Find suitable tag

--- backend/data/src/main/kotlin/io/tolgee/component/machineTranslation/LanguageTagConvertor.kt
+++ backend/data/src/main/kotlin/io/tolgee/component/machineTranslation/LanguageTagConvertor.kt
@@ -1,6 +1,8 @@
 package io.tolgee.component.machineTranslation
 
 object LanguageTagConvertor {
+  private const val MAX_ITERATIONS = 20
+
   fun findSuitableTag(
     suitableTags: Array<String>,
     desiredLanguage: String,
@@ -15,11 +17,16 @@ object LanguageTagConvertor {
     }
 
     var desired = desiredLanguage
+    var iterations = 0
 
     while (desired != "") {
       desired = desired.replace(Regex("[-_]?[a-zA-Z0-9]+$"), "")
       if (suitableTags.contains(desired)) {
         return desired
+      }
+
+      if (iterations++ >= MAX_ITERATIONS) {
+        break
       }
     }
 
