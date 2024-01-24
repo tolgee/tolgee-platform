1696603437b334dd7536a22ae2139a717942e288
Jan Cizmar <cizmar@chlupac.com>
2024-01-24 10:10:44 +0100

    fix: Sentry storing user information

--- backend/app/src/main/kotlin/io/tolgee/component/TolgeeSentryUserProvider.kt
+++ backend/app/src/main/kotlin/io/tolgee/component/TolgeeSentryUserProvider.kt
@@ -40,18 +40,20 @@ class TolgeeSentryUserProvider(
     return request.remoteAddr
   }
 
-  private val IP_HEADER_CANDIDATES =
-    arrayOf(
-      "X-Forwarded-For",
-      "Proxy-Client-IP",
-      "WL-Proxy-Client-IP",
-      "HTTP_X_FORWARDED_FOR",
-      "HTTP_X_FORWARDED",
-      "HTTP_X_CLUSTER_CLIENT_IP",
-      "HTTP_CLIENT_IP",
-      "HTTP_FORWARDED_FOR",
-      "HTTP_FORWARDED",
-      "HTTP_VIA",
-      "REMOTE_ADDR",
-    )
+  companion object {
+    private val IP_HEADER_CANDIDATES =
+      arrayOf(
+        "X-Forwarded-For",
+        "Proxy-Client-IP",
+        "WL-Proxy-Client-IP",
+        "HTTP_X_FORWARDED_FOR",
+        "HTTP_X_FORWARDED",
+        "HTTP_X_CLUSTER_CLIENT_IP",
+        "HTTP_CLIENT_IP",
+        "HTTP_FORWARDED_FOR",
+        "HTTP_FORWARDED",
+        "HTTP_VIA",
+        "REMOTE_ADDR",
+      )
+  }
 }
