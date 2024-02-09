99b476da454cf8b1e072ed9a1c448b8fe5caa4ac
Jan Cizmar <cizmar@chlupac.com>
2024-02-09 08:35:52 +0100

    fix: Add the test for apple xliff

--- backend/data/src/test/kotlin/io/tolgee/unit/formats/apple/out/AppleXliffFileExporterTest.kt
+++ backend/data/src/test/kotlin/io/tolgee/unit/formats/apple/out/AppleXliffFileExporterTest.kt
@@ -18,7 +18,130 @@ class AppleXliffFileExporterTest {
 
     // generate this with:
     // data.map { "data.assertFile(\"${it.key}\", \"\"\"\n    |${it.value.replace("\$", "\${'$'}").replace("\n", "\n    |")}\n    \"\"\".trimMargin())" }.joinToString("\n")
-    data
+    data.assertFile(
+      "cs.xlf",
+      """
+    |<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+    |<xliff xmlns="urn:oasis:names:tc:xliff:document:1.2" version="1.2">
+    |  <file datatype="plaintext" original="Localizable.strings" source-language="tag" target-language="cs">
+    |    <body>
+    |      <trans-unit id="key1">
+    |        <source xml:space="preserve">Hello! I%lld, %@, %e, %f</source>
+    |        <target xml:space="preserve">Ahoj! I%lld, %@, %e, %f</target>
+    |      </trans-unit>
+    |      <trans-unit id="No base!">
+    |        <source xml:space="preserve"/>
+    |        <target xml:space="preserve">I have no base</target>
+    |      </trans-unit>
+    |      <trans-unit id="No target!">
+    |        <source xml:space="preserve">I have no target</source>
+    |      </trans-unit>
+    |      <trans-unit id="key4">
+    |        <source xml:space="preserve">Namespaced</source>
+    |        <target xml:space="preserve">Namespaced</target>
+    |      </trans-unit>
+    |    </body>
+    |  </file>
+    |  <file datatype="plaintext" original="homepage.strings" source-language="tag" target-language="cs">
+    |    <body>
+    |      <trans-unit id="key2">
+    |        <source xml:space="preserve">Namespaced</source>
+    |        <target xml:space="preserve">Namespaced</target>
+    |      </trans-unit>
+    |    </body>
+    |  </file>
+    |  <file datatype="plaintext" original="homepage.stringsdict" source-language="tag" target-language="cs">
+    |    <body>
+    |      <trans-unit id="/key3:dict/NSStringLocalizedFormatKey:dict/:string">
+    |        <source xml:space="preserve">%#@property@</source>
+    |        <target xml:space="preserve">%#@property@</target>
+    |      </trans-unit>
+    |      <trans-unit id="/key3:dict/property:dict/one:dict/:string">
+    |        <source xml:space="preserve">%lld day</source>
+    |        <target xml:space="preserve">%lld den</target>
+    |      </trans-unit>
+    |      <trans-unit id="/key3:dict/property:dict/few:dict/:string">
+    |        <source xml:space="preserve">%lld days</source>
+    |        <target xml:space="preserve">%lld dny</target>
+    |      </trans-unit>
+    |      <trans-unit id="/key3:dict/property:dict/many:dict/:string">
+    |        <source xml:space="preserve">%lld days</source>
+    |        <target xml:space="preserve">%lld dní</target>
+    |      </trans-unit>
+    |      <trans-unit id="/key3:dict/property:dict/other:dict/:string">
+    |        <source xml:space="preserve">%lld days</source>
+    |        <target xml:space="preserve">%lld dní</target>
+    |      </trans-unit>
+    |    </body>
+    |  </file>
+    |  <file datatype="plaintext" original="Localizable.stringsdict" source-language="tag" target-language="cs">
+    |    <body>
+    |      <trans-unit id="/key5:dict/NSStringLocalizedFormatKey:dict/:string">
+    |        <source xml:space="preserve">%#@property@</source>
+    |        <target xml:space="preserve">%#@property@</target>
+    |      </trans-unit>
+    |      <trans-unit id="/key5:dict/property:dict/one:dict/:string">
+    |        <source xml:space="preserve">%lld day</source>
+    |        <target xml:space="preserve">%lld den</target>
+    |      </trans-unit>
+    |      <trans-unit id="/key5:dict/property:dict/few:dict/:string">
+    |        <source xml:space="preserve">%lld days</source>
+    |        <target xml:space="preserve">%lld dnů</target>
+    |      </trans-unit>
+    |      <trans-unit id="/key5:dict/property:dict/many:dict/:string">
+    |        <source xml:space="preserve">%lld days</source>
+    |        <target xml:space="preserve">%lld dnů</target>
+    |      </trans-unit>
+    |      <trans-unit id="/key5:dict/property:dict/other:dict/:string">
+    |        <source xml:space="preserve">%lld days</source>
+    |        <target xml:space="preserve">%lld dnů</target>
+    |      </trans-unit>
+    |      <trans-unit id="/key6:dict/NSStringLocalizedFormatKey:dict/:string">
+    |        <source xml:space="preserve">%#@property@</source>
+    |        <target xml:space="preserve">%#@property@</target>
+    |      </trans-unit>
+    |      <trans-unit id="/key6:dict/property:dict/one:dict/:string">
+    |        <source xml:space="preserve">%lld day</source>
+    |        <target xml:space="preserve">%lld den</target>
+    |      </trans-unit>
+    |      <trans-unit id="/key6:dict/property:dict/few:dict/:string">
+    |        <source xml:space="preserve">%lld days</source>
+    |        <target xml:space="preserve">dny</target>
+    |      </trans-unit>
+    |      <trans-unit id="/key6:dict/property:dict/many:dict/:string">
+    |        <source xml:space="preserve">%lld days</source>
+    |        <target xml:space="preserve">%lld dnů</target>
+    |      </trans-unit>
+    |      <trans-unit id="/key6:dict/property:dict/other:dict/:string">
+    |        <source xml:space="preserve">%lld days</source>
+    |        <target xml:space="preserve">%lld dnů</target>
+    |      </trans-unit>
+    |    </body>
+    |  </file>
+    |  <file datatype="plaintext" original="Localizable.xcstrings" source-language="tag" target-language="cs">
+    |    <body>
+    |      <trans-unit id="key &lt;omg&gt;|==|plural.one">
+    |        <source xml:space="preserve">%lld day</source>
+    |        <target xml:space="preserve">%lld day</target>
+    |      </trans-unit>
+    |      <trans-unit id="key &lt;omg&gt;|==|plural.few">
+    |        <source xml:space="preserve">%lld days</source>
+    |        <target xml:space="preserve">%lld days</target>
+    |      </trans-unit>
+    |      <trans-unit id="key &lt;omg&gt;|==|plural.many">
+    |        <source xml:space="preserve">%lld days</source>
+    |        <target xml:space="preserve">%lld days</target>
+    |      </trans-unit>
+    |      <trans-unit id="key &lt;omg&gt;|==|plural.other">
+    |        <source xml:space="preserve">%lld days</source>
+    |        <target xml:space="preserve">%lld days</target>
+    |      </trans-unit>
+    |    </body>
+    |  </file>
+    |</xliff>
+    |
+      """.trimMargin(),
+    )
   }
 
   private fun Map<String, String>.assertFile(
@@ -69,6 +192,7 @@ class AppleXliffFileExporterTest {
           baseText = "{count, plural, one {# day} other {# days}}",
         ) {
           key.namespace = "homepage"
+          key.description = "This is a description\n With some  spaces \n\n to preserve."
         }
         add(
           languageTag = "cs",
