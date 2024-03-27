29bf0b1741d218c55dd37ee68b74e4ad121a0078
Jan Cizmar <cizmar@chlupac.com>
2024-03-27 17:13:09 +0100

    build: Retry E2e when releasing

--- e2e/cypress/common/constants.ts
+++ e2e/cypress/common/constants.ts
@@ -4,5 +4,5 @@ export const USERNAME = Cypress.env('DEFAULT_USERNAME') || 'admin';
 export const API_URL = Cypress.env('API_URL') || 'http://localhost:8201';
 
 // get CI_RELEASE env variable
-const CI_RELEASE = Cypress.env('CI_RELEASE') === 'true';
+const CI_RELEASE = process.env.CI_RELEASE === 'true';
 export const GLOBAL_RETRIES = CI_RELEASE ? 10 : 0;
