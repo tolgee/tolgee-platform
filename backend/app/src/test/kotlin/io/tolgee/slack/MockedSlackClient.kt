35494e41f45011cfe8074a3099841226e63cdc6a
Ivan Manzhosov <manzhiva@fit.cvut.cz>
2024-04-16 23:29:13 +0200

    fix: redesign slack integration tests

---
+++ backend/app/src/test/kotlin/io/tolgee/slack/MockedSlackClient.kt
@@ -0,0 +1,22 @@
+package io.tolgee.slack
+
+import com.slack.api.RequestConfigurator
+import com.slack.api.methods.MethodsClient
+import com.slack.api.methods.request.chat.ChatPostMessageRequest
+import org.mockito.Mockito
+
+class MockedSlackClient(val methodsClientMock: MethodsClient) {
+  val chatPostMessageRequests: List<ChatPostMessageRequest>
+    get() =
+      Mockito.mockingDetails(methodsClientMock).invocations.flatMap { invocation ->
+        invocation.arguments.mapNotNull { argument ->
+          try {
+            @Suppress("UNCHECKED_CAST")
+            val configurator = argument as RequestConfigurator<ChatPostMessageRequest.ChatPostMessageRequestBuilder>
+            configurator.configure(ChatPostMessageRequest.builder()).build()
+          } catch (e: Exception) {
+            null
+          }
+        }
+      }
+}
--- backend/app/src/test/kotlin/io/tolgee/slack/SlackIntegrationTest.kt
+++ backend/app/src/test/kotlin/io/tolgee/slack/SlackIntegrationTest.kt
@@ -1,183 +1,40 @@
 package io.tolgee.slack
 
-import com.slack.api.RequestConfigurator
 import com.slack.api.Slack
 import com.slack.api.methods.MethodsClient
-import com.slack.api.methods.request.chat.ChatPostMessageRequest
-import com.slack.api.methods.response.chat.ChatPostMessageResponse
 import io.tolgee.AbstractSpringTest
-import io.tolgee.api.IModifiedEntityModel
-import io.tolgee.api.IProjectActivityModel
-import io.tolgee.component.automations.processors.slackIntegration.SavedMessageDto
-import io.tolgee.component.automations.processors.slackIntegration.SlackExecutor
-import io.tolgee.component.automations.processors.slackIntegration.SlackExecutorHelper
-import io.tolgee.component.automations.processors.slackIntegration.SlackRequest
-import io.tolgee.configuration.tolgee.TolgeeProperties
-import io.tolgee.development.testDataBuilder.data.SlackTestData
-import io.tolgee.model.slackIntegration.SavedSlackMessage
-import io.tolgee.service.slackIntegration.SavedSlackMessageService
+import io.tolgee.testing.assert
 import io.tolgee.util.Logging
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.*
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.mock.mockito.MockBean
 
 class SlackIntegrationTest : AbstractSpringTest(), Logging {
-  @MockBean
   @Autowired
+  @MockBean
   lateinit var slackClient: Slack
 
-  @Autowired
-  lateinit var slackExecutor: SlackExecutor
-
-  @Autowired
-  override lateinit var tolgeeProperties: TolgeeProperties
-
-  @Autowired
-  @MockBean
-  lateinit var savedSlackMessageService: SavedSlackMessageService
-
-  @BeforeAll
-  fun init() {
-    tolgeeProperties.slack.token = "token"
-  }
-
-  @AfterEach
-  fun clean() {
-    reset(savedSlackMessageService)
-  }
-
   @Test
-  fun `message sent to correct channel`() {
+  fun `it mocks slack client`() {
+    val mockedSlackClient = mockSlackClient()
+
+    slackClient.methods("ahhs").chatPostMessage {
+      it.channel("channel")
+      it.text("text")
+    }
+
+    mockedSlackClient.chatPostMessageRequests.assert.hasSize(1)
+    val request = mockedSlackClient.chatPostMessageRequests.single()
+    request.channel.assert.isEqualTo("channel")
+    request.text.assert.isEqualTo("text")
+  }
+
+  fun mockSlackClient(): MockedSlackClient {
     val methodsClientMock = mock<MethodsClient>()
     whenever(slackClient.methods(any())).thenReturn(methodsClientMock)
-
-    val testData = SlackTestData()
-    testDataService.saveTestData(testData.root)
-    val messageDto = mock<SavedMessageDto>()
-
-    whenever(
-      methodsClientMock.chatPostMessage(
-        any<RequestConfigurator<ChatPostMessageRequest.ChatPostMessageRequestBuilder>>(),
-      ),
-    ).thenAnswer {
-      val mockResponse = mock<ChatPostMessageResponse>()
-      whenever(mockResponse.isOk).thenReturn(true)
-      whenever(mockResponse.ts).thenReturn("123")
-      mockResponse
-    }
-
-    slackExecutor.sendRegularMessageWithSaving(messageDto, testData.slackConfig)
-
-    val builderSpy = spy(ChatPostMessageRequest.builder())
-    val lambdaCaptor = argumentCaptor<RequestConfigurator<ChatPostMessageRequest.ChatPostMessageRequestBuilder>>()
-
-    verify(methodsClientMock).chatPostMessage(lambdaCaptor.capture())
-    lambdaCaptor.firstValue.configure(builderSpy)
-
-    verify(builderSpy).channel(testData.slackConfig.channelId)
-
-    testDataService.cleanTestData(testData.root)
-  }
-
-  @Test
-  fun `message sent and saved`() {
-    val methodsClientMock = setUpSlackClientMocks()
-
-    val testData = SlackTestData()
-    testDataService.saveTestData(testData.root)
-    val messageDto = mock<SavedMessageDto>()
-
-    whenever(
-      methodsClientMock.chatPostMessage(
-        any<RequestConfigurator<ChatPostMessageRequest.ChatPostMessageRequestBuilder>>(),
-      ),
-    ).thenAnswer {
-      val mockResponse = mock<ChatPostMessageResponse>()
-      whenever(mockResponse.isOk).thenReturn(true)
-      whenever(mockResponse.ts).thenReturn("123")
-      mockResponse
-    }
-    val message = mock<SavedSlackMessage>()
-    whenever(savedSlackMessageService.create(any())).thenReturn(message)
-
-    slackExecutor.sendRegularMessageWithSaving(messageDto, testData.slackConfig)
-    verify(savedSlackMessageService, times(1)).create(any())
-
-    testDataService.cleanTestData(testData.root)
-  }
-
-  @Test
-  fun `sending the message failed and the message was not saved`() {
-    val methodsClientMock = setUpSlackClientMocks()
-
-    val testData = SlackTestData()
-    testDataService.saveTestData(testData.root)
-    val messageDto = mock<SavedMessageDto>()
-
-    whenever(
-      methodsClientMock.chatPostMessage(
-        any<RequestConfigurator<ChatPostMessageRequest.ChatPostMessageRequestBuilder>>(),
-      ),
-    ).thenAnswer {
-      val mockResponse = mock<ChatPostMessageResponse>()
-      whenever(mockResponse.isOk).thenReturn(false)
-      whenever(mockResponse.ts).thenReturn("123")
-      mockResponse
-    }
-    val message = mock<SavedSlackMessage>()
-    whenever(savedSlackMessageService.create(any())).thenReturn(message)
-
-    slackExecutor.sendRegularMessageWithSaving(messageDto, testData.slackConfig)
-    verify(savedSlackMessageService, times(0)).create(any())
-
-    testDataService.cleanTestData(testData.root)
-  }
-
-  private fun setUpSlackClientMocks(): MethodsClient {
-    val methodsClientMock = mock<MethodsClient>()
-    whenever(slackClient.methods(any())).thenReturn(methodsClientMock)
-
-    return methodsClientMock
-  }
-
-  @Test
-  fun `should send one message per translation change`() {
-    val testData = SlackTestData()
-    testDataService.saveTestData(testData.root)
-    val slackRequest = mock<SlackRequest>()
-    val slackExecutorHelper = mock<SlackExecutorHelper>()
-
-    doReturn(
-      mutableListOf(
-        mock<SavedSlackMessage>(),
-        mock<SavedSlackMessage>(),
-      ),
-    ).`when`(slackExecutorHelper).createTranslationChangeMessage()
-
-    whenever(slackExecutor.getHelper(any(), any())).thenReturn(slackExecutorHelper)
-    doReturn(testData.slackConfig).`when`(slackExecutorHelper).slackConfig
-    // doReturn(slackExecutorHelper).`when`(slackExecutor).getHelper(any(), any())
-
-    val modifiedEntities = mapOf("Translation" to listOf(mock<IModifiedEntityModel>(), mock<IModifiedEntityModel>()))
-    val activityData =
-      mock<IProjectActivityModel>().apply {
-        whenever(counts).thenReturn(mutableMapOf("Translation" to 2L)) // Ensure counts < 10 to avoid early return
-      }
-
-    whenever(slackRequest.activityData).thenReturn(activityData)
-    whenever(slackExecutor.findSavedMessageOrNull(any(), any(), any())).thenReturn(emptyList())
-    doNothing().whenever(slackExecutor).sendRegularMessageWithSaving(any(), any())
-
-    slackExecutor.sendMessageOnTranslationSet(testData.slackConfig, slackRequest)
-
-    // Verify that getHelper was indeed called
-    verify(slackExecutor).getHelper(any(), any())
-    // Verify that messages are sent as many times as there are translation changes
-    verify(slackExecutor, times(2)).sendRegularMessageWithSaving(any(), any())
-
-    testDataService.cleanTestData(testData.root)
+    return MockedSlackClient(methodsClientMock)
   }
 }
