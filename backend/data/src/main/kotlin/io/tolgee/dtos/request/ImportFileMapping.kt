2871c0593df537bde2e19b4d22cd351257ba07c9
Jan Cizmar <cizmar@chlupac.com>
2024-05-02 19:15:13 +0200

    docs: Add the docs for the schema

--- backend/data/src/main/kotlin/io/tolgee/dtos/request/ImportFileMapping.kt
+++ backend/data/src/main/kotlin/io/tolgee/dtos/request/ImportFileMapping.kt
@@ -1,10 +1,31 @@
 package io.tolgee.dtos.request
 
+import io.swagger.v3.oas.annotations.media.Schema
 import io.tolgee.formats.importCommon.ImportFormat
 
 class ImportFileMapping(
+  @Schema(
+    description =
+      "Name of the file to import. " +
+        "This is the name of the file provided in `files` request part or in uploaded archive.",
+  )
   val fileName: String,
+  @Schema(
+    description =
+      "This field maps the languages from imported files to languages existing in the Tolgee platform. " +
+        "If this field is null, Tolgee will try to guess the language from the file name or file contents.\n\n" +
+        "It is recommended to provide these values, to prevent any issues with language detection.",
+  )
   val languageMappings: List<LanguageMapping>? = null,
+  @Schema(
+    description = "Namespace to import the file to. If not provided, the key will be imported without namespace.",
+  )
   val namespace: String? = null,
+  @Schema(
+    description =
+      "Format of the file. If not provided, " +
+        "Tolgee will try to guess the format from the file name or file contents.\n\n" +
+        "It is recommended to provide these values, to prevent any issues with format detection.",
+  )
   val format: ImportFormat? = null,
 )
--- backend/data/src/main/kotlin/io/tolgee/dtos/request/LanguageMapping.kt
+++ backend/data/src/main/kotlin/io/tolgee/dtos/request/LanguageMapping.kt
@@ -1,6 +1,21 @@
 package io.tolgee.dtos.request
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 class LanguageMapping(
+  @Schema(
+    description =
+      "The language from the imported file. This is only case of files supporting multiple languages " +
+        "like XLIFF, where you specify the source and target languages. " +
+        "For files containing single language values, keep this `null`.",
+    example = "en-US",
+  )
   val importFileLanguage: String? = null,
+  @Schema(
+    description =
+      "The tag of language existing in the Tolgee platform to which the imported " +
+        "language should be mapped.",
+    example = "en-US",
+  )
   val existingLanguageTag: String,
 )
