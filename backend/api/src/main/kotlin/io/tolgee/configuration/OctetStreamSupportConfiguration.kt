ad0787ab0d48ba1030ae577a60d85457bff5babd
Jan Cizmar <cizmar@chlupac.com>
2024-05-02 15:20:08 +0200

    fix: Fix all

--- backend/api/src/main/kotlin/io/tolgee/configuration/OctetStreamSupportConfiguration.kt
+++ backend/api/src/main/kotlin/io/tolgee/configuration/OctetStreamSupportConfiguration.kt
@@ -1,16 +1,23 @@
 package io.tolgee.configuration
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Lazy
 import org.springframework.http.MediaType
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 
 @Configuration
-class OctetStreamSupportConfiguration {
-  @Bean
-  fun octetStreamJsonConverter(): MappingJackson2HttpMessageConverter {
-    val converter = MappingJackson2HttpMessageConverter()
-    converter.supportedMediaTypes = listOf(MediaType("application", "octet-stream"))
-    return converter
+@Lazy(false)
+class OctetStreamSupportConfiguration(converter: MappingJackson2HttpMessageConverter) {
+  init {
+    val supportedMediaTypes = converter.supportedMediaTypes.toMutableList()
+    supportedMediaTypes.add(MediaType("application", "octet-stream"))
+    converter.supportedMediaTypes = supportedMediaTypes
   }
+
+//  @Bean
+//  fun octetStreamJsonConverter(): MappingJackson2HttpMessageConverter {
+//    val converter = MappingJackson2HttpMessageConverter()
+//    converter.supportedMediaTypes = listOf(MediaType("application", "octet-stream"))
+//    return converter
+//  }
 }
