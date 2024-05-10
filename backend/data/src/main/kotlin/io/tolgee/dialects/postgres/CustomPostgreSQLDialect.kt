ee070ba84a9fb4f3849f8ed4099760b5df4defda
Jan Cizmar <cizmar@chlupac.com>
2024-05-10 11:43:45 +0200

    feat: Try to revert the dialect change

---
+++ backend/data/src/main/kotlin/io/tolgee/dialects/postgres/CustomPostgreSQLDialect.kt
@@ -0,0 +1,18 @@
+package io.tolgee.dialects.postgres
+
+import org.hibernate.boot.model.FunctionContributions
+import org.hibernate.dialect.DatabaseVersion
+import org.hibernate.dialect.PostgreSQLDialect
+import org.hibernate.type.StandardBasicTypes
+
+@Suppress("unused")
+class CustomPostgreSQLDialect : PostgreSQLDialect(DatabaseVersion.make(13)) {
+  override fun contributeFunctions(functionContributions: FunctionContributions) {
+    super.contributeFunctions(functionContributions)
+    functionContributions.functionRegistry.registerPattern(
+      "similarity",
+      "similarity(?1, ?2)",
+      functionContributions.typeConfiguration.basicTypeRegistry.resolve(StandardBasicTypes.FLOAT),
+    )
+  }
+}
