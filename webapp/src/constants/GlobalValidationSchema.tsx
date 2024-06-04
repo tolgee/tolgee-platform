fed1f92ad47837a6674754886ea6057897271ff7
Štěpán Granát <granat.stepan@gmail.com>
2024-06-04 14:02:47 +0200

    feat: tolgee cli 2 announcement (#2340)

--- webapp/src/constants/GlobalValidationSchema.tsx
+++ webapp/src/constants/GlobalValidationSchema.tsx
@@ -158,6 +158,7 @@ export class Validation {
       .test({
         name: 'language-tag-exists',
         test: (value) => !existingTags?.includes(value!),
+        // @tolgee-key validation_language_tag_exists
         message: t('validation_language_tag_exists'),
       })
       .matches(/^[^,]*$/, {
