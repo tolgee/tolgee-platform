aa419bb6d700fe06297adf2dd1b471d1e5021224
Jan Cizmar <cizmar@chlupac.com>
2024-06-07 18:39:07 +0200

    fix: Make batch MT translation less concurrent

--- backend/data/src/main/kotlin/io/tolgee/batch/processors/PreTranslationByTmChunkProcessor.kt
+++ backend/data/src/main/kotlin/io/tolgee/batch/processors/PreTranslationByTmChunkProcessor.kt
@@ -52,6 +52,10 @@ class PreTranslationByTmChunkProcessor(
     return PreTranslationByTmJobParams::class.java
   }
 
+  override fun getMaxPerJobConcurrency(): Int {
+    return 1
+  }
+
   override fun getChunkSize(
     request: PreTranslationByTmRequest,
     projectId: Long,
