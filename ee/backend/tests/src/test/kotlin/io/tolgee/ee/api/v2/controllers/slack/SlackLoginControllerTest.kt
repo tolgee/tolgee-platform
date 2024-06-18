508acd42058fa271a3fdfbea0824f0ee966bde9f
Ivan Manzhosov <manzhiva@fit.cvut.cz>
2024-06-18 12:41:14 +0200

    chore: test edge cases of creating user-slack connection

--- ee/backend/tests/src/test/kotlin/io/tolgee/ee/api/v2/controllers/slack/SlackLoginControllerTest.kt
+++ ee/backend/tests/src/test/kotlin/io/tolgee/ee/api/v2/controllers/slack/SlackLoginControllerTest.kt
@@ -2,12 +2,15 @@ package io.tolgee.ee.api.v2.controllers.slack
 
 import io.tolgee.constants.Feature
 import io.tolgee.development.testDataBuilder.data.SlackNoUserConnectionTestData
+import io.tolgee.development.testDataBuilder.data.SlackTestData
 import io.tolgee.ee.component.PublicEnabledFeaturesProvider
 import io.tolgee.ee.component.slackIntegration.SlackUserLoginUrlProvider
 import io.tolgee.ee.service.slackIntegration.SlackUserConnectionService
+import io.tolgee.fixtures.andIsBadRequest
 import io.tolgee.fixtures.andIsOk
 import io.tolgee.testing.AuthorizedControllerTest
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThatCode
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -38,5 +41,47 @@ class SlackLoginControllerTest : AuthorizedControllerTest() {
     }
 
     Assertions.assertThat(slackUserConnectionService.findBySlackId("TEST1")).isNotNull()
+  }
+
+  @Test
+  fun `should not allow duplicate SlackUserConnection for same Tolgee account and workspace`() {
+    val testData = SlackTestData()
+    testDataService.saveTestData(testData.root)
+
+    slackUserLoginUrlProvider.encryptData("ChannelTest", "TEST1", testData.slackWorkspace.id).let {
+      performAuthPost("/v2/slack/user-login?data=$it", null).andIsBadRequest
+    }
+
+    assertThatCode {
+      slackUserConnectionService.findBySlackId("TEST1")
+    }.doesNotThrowAnyException()
+  }
+
+  @Test
+  fun `logs in for same Tolgee acc and different workspace`() {
+    val testData = SlackTestData()
+    testDataService.saveTestData(testData.root)
+
+    slackUserLoginUrlProvider.encryptData("ChannelTest", "slackUserId", testData.slackWorkspace2.id).let {
+      performAuthPost("/v2/slack/user-login?data=$it", null).andIsOk
+    }
+
+    assertThatCode {
+      slackUserConnectionService.findBySlackId("slackUserId")
+    }.isNotNull()
+  }
+
+  @Test
+  fun `should not allow duplicate SlackUserConnection for same Tolgee account and workspace and different Slack acc`() {
+    val testData = SlackTestData()
+    testDataService.saveTestData(testData.root)
+
+    slackUserLoginUrlProvider.encryptData("ChannelTest", "slackUserId322", testData.slackWorkspace.id).let {
+      performAuthPost("/v2/slack/user-login?data=$it", null).andIsBadRequest
+    }
+
+    assertThatCode {
+      slackUserConnectionService.findBySlackId(testData.slackUserConnection.slackTeamId!!)
+    }.doesNotThrowAnyException()
   }
 }
