b6ab220d05465f847ff11a688f244d80a0ef6d37
Štěpán Granát <granat.stepan@gmail.com>
2024-06-18 13:50:47 +0200

    fix: word breaking for long words

--- webapp/src/component/billing/PlanFeature.tsx
+++ webapp/src/component/billing/PlanFeature.tsx
@@ -33,7 +33,9 @@ export function PlanFeature({ name, link, bold, title, sx, className }: Props) {
   return (
     <Box display="flex" gap={0.5} alignItems="center" {...{ sx, className }}>
       <Check style={{ fontSize: 16 }} />
-      <Typography fontSize={14}>{item}</Typography>
+      <Typography sx={{ wordBreak: 'break-word' }} fontSize={14}>
+        {item}
+      </Typography>
     </Box>
   );
 }
