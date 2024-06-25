2bd8e24c44102c947a5f5b668932ec71275cd9bd
Jan Cizmar <cizmar@chlupac.com>
2024-06-25 12:57:54 +0200

    fix: .po parser ignores escaped quotes (#2386)

--- backend/data/src/test/kotlin/io/tolgee/unit/formats/po/in/PoFileProcessorTest.kt
+++ backend/data/src/test/kotlin/io/tolgee/unit/formats/po/in/PoFileProcessorTest.kt
@@ -29,7 +29,7 @@ class PoFileProcessorTest {
     mockImportFile("example.po")
     PoFileProcessor(mockUtil.fileProcessorContext).process()
     assertThat(mockUtil.fileProcessorContext.languages).hasSize(1)
-    assertThat(mockUtil.fileProcessorContext.translations).hasSize(8)
+    assertThat(mockUtil.fileProcessorContext.translations).hasSize(9)
     val text = mockUtil.fileProcessorContext.translations["%d page read."]?.get(0)?.text
     assertThat(text)
       .isEqualTo(
