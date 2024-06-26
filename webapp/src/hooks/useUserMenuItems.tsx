475ddeff48f45bef21e3ef323009b43a6ffb26bf
Ivan Manzhosov <manzhiva@fit.cvut.cz>
2024-06-26 18:39:33 +0200

    feat: add a menu when the user has not confirmed the email to FE

--- webapp/src/hooks/useUserMenuItems.tsx
+++ webapp/src/hooks/useUserMenuItems.tsx
@@ -1,8 +1,8 @@
 import {useTranslate} from '@tolgee/react';
 import {useLocation} from 'react-router-dom';
 
-import {LINKS} from '../constants/links';
-import {useConfig, useIsEmailVerified, useUser,} from 'tg.globalContext/helpers';
+import { LINKS } from '../constants/links';
+import { useConfig, useUser } from 'tg.globalContext/helpers';
 
 export class UserMenuItem {
   constructor(
