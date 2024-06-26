281d6db261f5e95d574e9a9af0412985cd50227b
Ivan Manzhosov <manzhiva@fit.cvut.cz>
2024-06-26 18:39:33 +0200

    feat: add a menu when the user has not confirmed the email to FE

--- webapp/src/component/security/UserMenu/UserMenu.tsx
+++ webapp/src/component/security/UserMenu/UserMenu.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
-import { useConfig, useUser } from 'tg.globalContext/helpers';
-import { UserMissingMenu } from './UserMissingMenu';
-import { UserPresentMenu } from './UserPresentMenu';
+import {useConfig, useUser} from 'tg.globalContext/helpers';
+import {UserMissingMenu} from './UserMissingMenu';
+import {UserPresentMenu} from './UserPresentMenu';
+import {UserUnverifiedEmailMenu} from './UserUnverifiedEmailMenu';
+
 
 export const UserMenu: React.FC = () => {
   const config = useConfig();
   const user = useUser();
+  const isEmailVerified = user !== undefined && user.emailAwaitingVerification === null || !config.needsEmailVerification;
+
+  if(!isEmailVerified) {
+    return <UserUnverifiedEmailMenu />;
+  }
 
   if (!config.authentication || !user) {
     return <UserMissingMenu />;
