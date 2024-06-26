0ae032f4f0de3926339bcb02daf2ff2d224578a4
Ivan Manzhosov <manzhiva@fit.cvut.cz>
2024-06-26 17:53:03 +0200

    feat: add emailVerification view to FE

--- webapp/src/views/projects/ProjectListView.tsx
+++ webapp/src/views/projects/ProjectListView.tsx
@@ -1,20 +1,22 @@
-import { useState } from 'react';
-import { T, useTranslate } from '@tolgee/react';
+import {useState} from 'react';
+import {T, useTranslate} from '@tolgee/react';
 
-import { EmptyListMessage } from 'tg.component/common/EmptyListMessage';
-import { PaginatedHateoasList } from 'tg.component/common/list/PaginatedHateoasList';
-import { BaseView } from 'tg.component/layout/BaseView';
-import { BaseViewAddButton } from 'tg.component/layout/BaseViewAddButton';
-import { DashboardPage } from 'tg.component/layout/DashboardPage';
-import { LINKS } from 'tg.constants/links';
-import { useApiQuery } from 'tg.service/http/useQueryApi';
+import {EmptyListMessage} from 'tg.component/common/EmptyListMessage';
+import {PaginatedHateoasList} from 'tg.component/common/list/PaginatedHateoasList';
+import {BaseView} from 'tg.component/layout/BaseView';
+import {BaseViewAddButton} from 'tg.component/layout/BaseViewAddButton';
+import {DashboardPage} from 'tg.component/layout/DashboardPage';
+import {LINKS} from 'tg.constants/links';
+import {useApiQuery} from 'tg.service/http/useQueryApi';
 import DashboardProjectListItem from 'tg.views/projects/DashboardProjectListItem';
-import { Button, styled } from '@mui/material';
-import { Link } from 'react-router-dom';
-import { useIsAdmin, usePreferredOrganization } from 'tg.globalContext/helpers';
-import { OrganizationSwitch } from 'tg.component/organizationSwitch/OrganizationSwitch';
-import { Usage } from 'tg.component/billing/Usage';
-import { QuickStartHighlight } from 'tg.component/layout/QuickStartGuide/QuickStartHighlight';
+import {Button, styled} from '@mui/material';
+import {Link} from 'react-router-dom';
+import {useIsAdmin, usePreferredOrganization} from 'tg.globalContext/helpers';
+import {OrganizationSwitch} from 'tg.component/organizationSwitch/OrganizationSwitch';
+import {Usage} from 'tg.component/billing/Usage';
+import {QuickStartHighlight} from 'tg.component/layout/QuickStartGuide/QuickStartHighlight';
+import {useGlobalContext} from 'tg.globalContext/GlobalContext';
+import {EmailVerificationPage} from 'tg.component/EmailVerificationPage';
 
 const StyledWrapper = styled('div')`
   display: flex;
@@ -30,6 +32,7 @@ export const ProjectListView = () => {
   const [page, setPage] = useState(0);
   const [search, setSearch] = useState('');
   const { preferredOrganization } = usePreferredOrganization();
+  const isEmailVerified = useGlobalContext((c) => c.initialData.userInfo?.emailAwaitingVerification === null);
 
   const listPermitted = useApiQuery({
     url: '/v2/organizations/{slug}/projects-with-stats',
@@ -54,6 +57,8 @@ export const ProjectListView = () => {
 
   const isAdmin = useIsAdmin();
 
+
+
   const isAdminAccess = !preferredOrganization?.currentUserRole && isAdmin;
 
   const addAllowed = isOrganizationOwner || isAdminAccess;
@@ -71,7 +76,7 @@ export const ProjectListView = () => {
           maxWidth={1000}
           allCentered
           addComponent={
-            addAllowed && (
+            (addAllowed && isEmailVerified) && (
               <QuickStartHighlight itemKey="add_project">
                 <BaseViewAddButton
                   addLinkTo={LINKS.PROJECT_ADD.build()}
@@ -82,37 +87,41 @@ export const ProjectListView = () => {
           }
           addLabel={t('projects_add_button')}
           hideChildrenOnLoading={false}
-          navigation={[
+          navigation={ isEmailVerified ? [
             [<OrganizationSwitch key={0} />],
             [t('projects_title'), LINKS.PROJECTS.build()],
-          ]}
+          ] : undefined}
           navigationRight={<Usage />}
           loading={listPermitted.isFetching}
         >
-          <PaginatedHateoasList
-            wrapperComponentProps={{ className: 'listWrapper' }}
-            onPageChange={setPage}
-            loadable={listPermitted}
-            renderItem={(r) => <DashboardProjectListItem key={r.id} {...r} />}
-            emptyPlaceholder={
-              <EmptyListMessage
-                loading={listPermitted.isFetching}
-                hint={
-                  isOrganizationOwner ? (
-                    <Button
-                      component={Link}
-                      to={LINKS.PROJECT_ADD.build()}
-                      color="primary"
-                    >
-                      <T keyName="projects_empty_action" />
-                    </Button>
-                  ) : undefined
-                }
-              >
-                <T keyName="projects_empty" />
-              </EmptyListMessage>
-            }
-          />
+            {!isEmailVerified ? (
+                <EmailVerificationPage email={`userEmail` || ''} />
+            ) : (
+                <PaginatedHateoasList
+                    wrapperComponentProps={{ className: 'listWrapper' }}
+                    onPageChange={setPage}
+                    loadable={listPermitted}
+                    renderItem={(r) => <DashboardProjectListItem key={r.id} {...r} />}
+                    emptyPlaceholder={
+                        <EmptyListMessage
+                            loading={listPermitted.isFetching}
+                            hint={
+                                isOrganizationOwner ? (
+                                    <Button
+                                        component={Link}
+                                        to={LINKS.PROJECT_ADD.build()}
+                                        color="primary"
+                                    >
+                                        <T keyName="projects_empty_action" />
+                                    </Button>
+                                ) : undefined
+                            }
+                        >
+                            <T keyName="projects_empty" />
+                        </EmptyListMessage>
+                    }
+                />
+            )}
         </BaseView>
       </DashboardPage>
     </StyledWrapper>
