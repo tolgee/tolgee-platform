861cde4cd7d2a48948162c77b80cc145767ffb86
Ivan Manzhosov <manzhiva@fit.cvut.cz>
2024-07-09 11:13:34 +0300

    fix: don't call start guide hook, when email isn't verified

--- webapp/src/globalContext/useQuickStartGuideService.tsx
+++ webapp/src/globalContext/useQuickStartGuideService.tsx
@@ -1,13 +1,10 @@
-import { useMediaQuery } from '@mui/material';
-import { useState } from 'react';
-import { useRouteMatch } from 'react-router-dom';
-import {
-  HighlightItem,
-  ItemStep,
-} from 'tg.component/layout/QuickStartGuide/enums';
-import { LINKS, PARAMS } from 'tg.constants/links';
-import { useApiQuery } from 'tg.service/http/useQueryApi';
-import type { useInitialDataService } from './useInitialDataService';
+import {useMediaQuery} from '@mui/material';
+import {useState} from 'react';
+import {useRouteMatch} from 'react-router-dom';
+import {HighlightItem, ItemStep,} from 'tg.component/layout/QuickStartGuide/enums';
+import {LINKS, PARAMS} from 'tg.constants/links';
+import {useApiQuery} from 'tg.service/http/useQueryApi';
+import type {useInitialDataService} from './useInitialDataService';
 
 export const useQuickStartGuideService = (
   initialData: ReturnType<typeof useInitialDataService>
@@ -21,7 +18,8 @@ export const useQuickStartGuideService = (
   const projectIdParam = match?.params[PARAMS.PROJECT_ID];
   const projectId = isNaN(projectIdParam) ? undefined : projectIdParam;
   const [floatingOpen, setFloatingOpen] = useState(false);
-
+  const isVerified = initialData.state?.userInfo?.emailAwaitingVerification === null ||
+      !initialData.state?.serverConfiguration.needsEmailVerification
   const organizationSlug = initialData.state?.preferredOrganization?.slug;
   const isOwner =
     initialData.state?.preferredOrganization?.currentUserRole === 'OWNER';
@@ -32,7 +30,7 @@ export const useQuickStartGuideService = (
     path: { slug: organizationSlug! },
     query: { size: 1, sort: ['id,desc'] },
     options: {
-      enabled: projectId === undefined && Boolean(organizationSlug),
+      enabled: projectId === undefined && Boolean(organizationSlug) && isVerified,
     },
   });
 
--- webapp/src/service/http/ApiHttpService.tsx
+++ webapp/src/service/http/ApiHttpService.tsx
@@ -1,12 +1,12 @@
-import { LINKS } from 'tg.constants/links';
-import { GlobalError } from 'tg.error/GlobalError';
+import {LINKS} from 'tg.constants/links';
+import {GlobalError} from 'tg.error/GlobalError';
 
-import { tokenService } from '../TokenService';
-import { getUtmCookie } from 'tg.fixtures/utmCookie';
-import { handleApiError } from './handleApiError';
-import { ApiError } from './ApiError';
-import { errorAction } from './errorAction';
-import { globalContext } from 'tg.globalContext/globalActions';
+import {tokenService} from '../TokenService';
+import {getUtmCookie} from 'tg.fixtures/utmCookie';
+import {handleApiError} from './handleApiError';
+import {ApiError} from './ApiError';
+import {errorAction} from './errorAction';
+import {globalContext} from 'tg.globalContext/globalActions';
 
 let requests: { [address: string]: number } = {};
 const detectLoop = (url) => {
@@ -59,9 +59,8 @@ export class ApiHttpService {
             if (r.status >= 400) {
               const responseData = await ApiHttpService.getResObject(r);
               const resultError = new ApiError('Api error', responseData);
-              const currentPath = location.pathname;
               resultError.setErrorHandler(() =>
-                handleApiError(r, responseData, init, options, currentPath)
+                handleApiError(r, responseData, init, options)
               );
               if (r.status === 400) {
                 errorAction(responseData.code);
