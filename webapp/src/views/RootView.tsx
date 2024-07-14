b0c680bce72a3da2143584301f7da0f5eefb7114
Ivan Manzhosov <manzhiva@fit.cvut.cz>
2024-07-14 21:56:14 +0300

    fix: FE prettier

--- webapp/src/views/RootView.tsx
+++ webapp/src/views/RootView.tsx
@@ -1,9 +1,9 @@
-import {EmailNotVerifiedView} from "tg.component/EmailNotVerifiedView";
-import {useIsEmailVerified} from "tg.globalContext/helpers";
-import {ProjectListView} from "tg.views/projects/ProjectListView";
+import { EmailNotVerifiedView } from 'tg.component/EmailNotVerifiedView';
+import { useIsEmailVerified } from 'tg.globalContext/helpers';
+import { ProjectListView } from 'tg.views/projects/ProjectListView';
 
 export const RootView = () => {
-    const isEmailVerified = useIsEmailVerified()
+  const isEmailVerified = useIsEmailVerified();
 
-    return isEmailVerified ? <ProjectListView/> : <EmailNotVerifiedView/>
-}
+  return isEmailVerified ? <ProjectListView /> : <EmailNotVerifiedView />;
+};
