1143485ccca7bb5d35a403f1ec36abe359f36c8b
Ivan Manzhosov <manzhiva@fit.cvut.cz>
2024-09-06 19:34:46 +0200

    fix: change url

--- ee/backend/app/src/main/kotlin/io/tolgee/ee/api/v2/controllers/SsoProviderController.kt
+++ ee/backend/app/src/main/kotlin/io/tolgee/ee/api/v2/controllers/SsoProviderController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/providers")
+@RequestMapping("/v2/sso/providers")
 class SsoProviderController(private val tenantService: TenantService) {
   @PostMapping("")
   fun addProvider(
