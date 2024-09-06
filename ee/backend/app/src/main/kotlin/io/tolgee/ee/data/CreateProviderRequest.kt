d8dfdb86cb98e65482b7b6a127674fd29a9ebe89
Ivan Manzhosov <manzhiva@fit.cvut.cz>
2024-09-06 19:35:11 +0200

    fix: add more properties CreateProviderRequest

--- ee/backend/app/src/main/kotlin/io/tolgee/ee/data/CreateProviderRequest.kt
+++ ee/backend/app/src/main/kotlin/io/tolgee/ee/data/CreateProviderRequest.kt
@@ -1,13 +1,10 @@
 package io.tolgee.ee.data
 
 data class CreateProviderRequest(
-  val name: String,
-  val ssoProvider: String,
+  val name: String?,
   val clientId: String,
   val clientSecret: String,
   val authorizationUri: String,
-  val tokenUri: String,
-  val jwkSetUri: String,
-  val domain: String,
   val redirectUri: String,
+  val tokenUri: String,
 )
