ac048ac6c85239009a506b12beebf2a46956d66d
Jan Cizmar <cizmar@chlupac.com>
2024-09-23 09:30:39 +0200

    feat: Bunny.net cache purging, Origins specification for Cloudflare (#2471)

--- backend/data/src/test/kotlin/io/tolgee/unit/cachePurging/BunnyContentStorageConfigCachePurgingTest.kt
+++ backend/data/src/test/kotlin/io/tolgee/unit/cachePurging/BunnyContentStorageConfigCachePurgingTest.kt
@@ -1,12 +1,9 @@
 package io.tolgee.unit.cachePurging
 
-import io.tolgee.component.contentDelivery.cachePurging.cloudflare.CloudflareContentDeliveryCachePurging
-import io.tolgee.configuration.tolgee.ContentDeliveryCloudflareProperties
-import io.tolgee.fixtures.node
+import io.tolgee.component.contentDelivery.cachePurging.bunny.BunnyContentDeliveryCachePurging
+import io.tolgee.configuration.tolgee.ContentDeliveryBunnyProperties
 import io.tolgee.model.contentDelivery.ContentDeliveryConfig
 import io.tolgee.testing.assert
-import net.javacrumbs.jsonunit.assertj.JsonAssert
-import net.javacrumbs.jsonunit.assertj.assertThatJson
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.mockito.invocation.Invocation
@@ -21,19 +18,16 @@ import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.client.RestTemplate
 
-class CloudflareContentStorageConfigCachePurgingTest() {
+class BunnyContentStorageConfigCachePurgingTest() {
   @Test
   fun `correctly purges`() {
     val config =
-      ContentDeliveryCloudflareProperties(
-        zoneId = "fake-zone-id",
+      ContentDeliveryBunnyProperties(
         urlPrefix = "fake-url-prefix",
-        maxFilesPerRequest = 10,
         apiKey = "token",
-        origins = "fake-origin,fake-origin2",
       )
     val restTemplateMock: RestTemplate = mock()
-    val purging = CloudflareContentDeliveryCachePurging(config, restTemplateMock)
+    val purging = BunnyContentDeliveryCachePurging(config, restTemplateMock)
     val responseMock: ResponseEntity<*> = Mockito.mock(ResponseEntity::class.java)
     whenever(restTemplateMock.exchange(any<String>(), any<HttpMethod>(), any(), eq(String::class.java))).doAnswer {
       responseMock as ResponseEntity<String>
@@ -48,55 +42,15 @@ class CloudflareContentStorageConfigCachePurgingTest() {
     )
 
     val invocations = Mockito.mockingDetails(restTemplateMock).invocations
-    val firstInvocation = invocations.first()
-    assertFiles(firstInvocation) {
-      isArray
-        .hasSize(10)
-      node("[0]") {
-        node("headers").isEqualTo(
-          mapOf(
-            "Origin" to "fake-origin",
-          ),
-        )
-        node("url").isEqualTo("fake-url-prefix/fake-slug/fake-path-1")
-      }
-      node("[1]") {
-        node("headers").isEqualTo(
-          mapOf(
-            "Origin" to "fake-origin2",
-          ),
-        )
-      }
-    }
-    assertUrl(firstInvocation)
-    assertAuthorizationHeader(firstInvocation)
-
-    val invocationList = invocations.toList()
-    assertFiles(invocationList[1]) {
-      isArray.hasSize(10)
-    }
-    assertFiles(invocationList[2]) {
-      isArray.hasSize(10)
-    }
-    invocationList.assert.hasSize(3)
-  }
-
-  private fun assertFiles(
-    invocation: Invocation,
-    fn: JsonAssert.() -> Unit,
-  ) {
-    val httpEntity = getHttpEntity(invocation)
-    assertThatJson(httpEntity.body) {
-      node("files") {
-        fn()
-      }
-    }
+    val invocation = invocations.single()
+    assertUrl(invocation)
+    assertAuthorizationHeader(invocation)
   }
 
   private fun assertAuthorizationHeader(invocation: Invocation) {
     val httpEntity = getHttpEntity(invocation)
     val headers = httpEntity.headers
-    headers["Authorization"].assert.isEqualTo(listOf("Bearer token"))
+    headers["AccessKey"].assert.isEqualTo(listOf("token"))
   }
 
   private fun getHttpEntity(invocation: Invocation) = invocation.arguments[2] as HttpEntity<*>
@@ -104,7 +58,7 @@ class CloudflareContentStorageConfigCachePurgingTest() {
   private fun assertUrl(invocation: Invocation) {
     val url = invocation.arguments[0]
     url.assert.isEqualTo(
-      "https://api.cloudflare.com/client/v4/zones/fake-zone-id/purge_cache",
+      "https://api.bunny.net/purge?url=fake-url-prefix%2Ffake-slug%2F*",
     )
   }
 }
