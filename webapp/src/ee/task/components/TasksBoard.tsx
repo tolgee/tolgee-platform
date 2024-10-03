85c3ca10b8840fc614fc68cb07181d4a554f9858
Štěpán Granát <granat.stepan@gmail.com>
2024-10-03 11:09:38 +0200

    fix: board view fetch more

--- webapp/src/ee/task/components/TasksBoard.tsx
+++ webapp/src/ee/task/components/TasksBoard.tsx
@@ -58,9 +58,9 @@ export const TasksBoard = ({
     doneTasks.hasNextPage;
 
   function handleFetchMore() {
-    newTasks.fetchNextPage();
-    inProgressTasks.fetchNextPage();
-    doneTasks.fetchNextPage();
+    newTasks.hasNextPage && newTasks.fetchNextPage();
+    inProgressTasks.hasNextPage && inProgressTasks.fetchNextPage();
+    doneTasks.hasNextPage && doneTasks.fetchNextPage();
   }
 
   const isLoading =
