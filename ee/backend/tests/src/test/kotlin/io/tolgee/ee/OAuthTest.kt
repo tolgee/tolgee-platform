e494b5c919b5182c9bd9fcadf0f27bfadc8a38a3
Ivan Manzhosov <manzhiva@fit.cvut.cz>
2024-10-06 13:40:47 +0200

    chore: test

---
+++ ee/backend/tests/src/test/kotlin/io/tolgee/ee/OAuthTest.kt
@@ -0,0 +1,56 @@
+package io.tolgee.ee
+
+import io.tolgee.ee.model.Tenant
+import io.tolgee.ee.repository.DynamicOAuth2ClientRegistrationRepository
+import io.tolgee.ee.service.OAuthService
+import io.tolgee.ee.service.TenantService
+import io.tolgee.ee.utils.OAuthMultiTenantsMocks
+import io.tolgee.testing.AbstractControllerTest
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.web.client.RestTemplate
+
+class OAuthTest : AbstractControllerTest() {
+  @MockBean
+  @Autowired
+  private val restTemplate: RestTemplate? = null
+
+  @Autowired
+  private var authMvc: MockMvc? = null
+
+  @Autowired
+  private lateinit var dynamicOAuth2ClientRegistrationRepository: DynamicOAuth2ClientRegistrationRepository
+
+  @Autowired
+  private lateinit var oAuthService: OAuthService
+
+  @Autowired
+  private lateinit var tenantService: TenantService
+
+  private val oAuthMultiTenantsMocks: OAuthMultiTenantsMocks by lazy {
+    OAuthMultiTenantsMocks(authMvc, restTemplate, dynamicOAuth2ClientRegistrationRepository)
+  }
+
+  @Test
+  fun authorize() {
+    tenantService.save(
+      Tenant().apply {
+        name = "tenant1"
+        domain = "registrationId"
+        clientId = "clientId"
+        clientSecret = "clientSecret"
+        authorizationUri = "authorizationUri"
+        jwkSetUri = "jwkSetUri"
+        tokenUri = "tokenUri"
+        redirectUriBase = "redirectUriBase"
+        organizationId = 0L
+      },
+    )
+    val clientRegistraion = dynamicOAuth2ClientRegistrationRepository.findByRegistrationId("registrationId").clientRegistration
+    oAuthMultiTenantsMocks.authorize(clientRegistraion.registrationId)
+    val response = oAuthService.exchangeCodeForToken(clientRegistraion, "code", "redirectUrl")
+    response
+  }
+}
---
+++ ee/backend/tests/src/test/kotlin/io/tolgee/ee/utils/OAuthMultiTenantsMocks.kt
@@ -0,0 +1,51 @@
+package io.tolgee.ee.utils
+
+import io.tolgee.ee.repository.DynamicOAuth2ClientRegistrationRepository
+import io.tolgee.ee.service.OAuthService
+import org.mockito.kotlin.any
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.whenever
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.web.client.RestTemplate
+
+class OAuthMultiTenantsMocks(
+  private var authMvc: MockMvc? = null,
+  private val restTemplate: RestTemplate? = null,
+  private val dynamicOAuth2ClientRegistrationRepository: DynamicOAuth2ClientRegistrationRepository,
+) {
+  companion object {
+    val defaultUserResponse =
+      OAuthService.GenericUserResponse().apply {
+        sub = "fakeId"
+        given_name = "fakeGiveName"
+        family_name = "fakeGivenFamilyName"
+        email = "email@domain.com"
+      }
+
+    val defaultToken =
+      OAuthService.OAuth2TokenResponse(id_token = "id_token", scope = "scope")
+
+    val defaultTokenResponse =
+      ResponseEntity(
+        defaultToken,
+        HttpStatus.OK,
+      )
+  }
+
+  fun authorize(registrationId: String) {
+    val receivedCode = "fake_access_token"
+    val registration = dynamicOAuth2ClientRegistrationRepository.findByRegistrationId(registrationId)
+
+    whenever(
+      restTemplate?.exchange(
+        eq(registration.providerDetails.tokenUri),
+        eq(HttpMethod.POST),
+        any(),
+        eq(OAuthService.OAuth2TokenResponse::class.java),
+      ),
+    ).thenReturn(defaultTokenResponse)
+  }
+}
