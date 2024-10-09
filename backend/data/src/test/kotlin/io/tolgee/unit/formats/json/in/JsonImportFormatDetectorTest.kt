1064508eaecb39119fc1279b522bda04656e0b75
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2024-10-09 16:14:31 +0200

    fix: lint

--- backend/data/src/test/kotlin/io/tolgee/unit/formats/json/in/JsonImportFormatDetectorTest.kt
+++ backend/data/src/test/kotlin/io/tolgee/unit/formats/json/in/JsonImportFormatDetectorTest.kt
@@ -1,7 +1,5 @@
 package io.tolgee.unit.formats.json.`in`
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import io.tolgee.formats.importCommon.ImportFormat
