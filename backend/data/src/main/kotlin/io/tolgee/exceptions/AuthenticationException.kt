7ff0425c5f7e5c248f9c38d3ca0ff4f400f1a6ef
Ivan Manzhosov <manzhiva@fit.cvut.cz>
2024-11-08 10:55:13 +0100

    feat: add params to auth exception

--- backend/data/src/main/kotlin/io/tolgee/exceptions/AuthenticationException.kt
+++ backend/data/src/main/kotlin/io/tolgee/exceptions/AuthenticationException.kt
@@ -3,9 +3,14 @@ package io.tolgee.exceptions
 import io.tolgee.constants.Message
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
+import java.io.Serializable
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
 open class AuthenticationException(message: Message) : ErrorException(message) {
+class AuthenticationException(
+  message: io.tolgee.constants.Message,
+  params: List<Serializable?>? = null,
+) : ErrorException(message, params) {
   override val httpStatus: HttpStatus
     get() = HttpStatus.UNAUTHORIZED
 }
