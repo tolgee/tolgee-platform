e6de95466c9daf25b08e424cd72257d5a560c3a4
Ivan Manzhosov <manzhiva@fit.cvut.cz>
2024-11-08 10:58:23 +0100

    feat: add CHANGE_AUTH_PROVIDER route

--- webapp/src/component/security/Login/LoginRouter.tsx
+++ webapp/src/component/security/Login/LoginRouter.tsx
@@ -1,13 +1,14 @@
-import { default as React, FunctionComponent } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import {default as React, FunctionComponent} from 'react';
+import {Route, Switch} from 'react-router-dom';
 
-import { LINKS } from 'tg.constants/links';
+import {LINKS} from 'tg.constants/links';
 
-import { LoginView } from './LoginView';
-import { EmailVerificationHandler } from './EmailVerificationHandler';
-import { OAuthRedirectionHandler } from './OAuthRedirectionHandler';
-import { PublicOnlyRoute } from 'tg.component/common/PublicOnlyRoute';
-import { SsoRedirectionHandler } from 'tg.component/security/Sso/SsoRedirectionHandler';
+import {LoginView} from './LoginView';
+import {EmailVerificationHandler} from './EmailVerificationHandler';
+import {OAuthRedirectionHandler} from './OAuthRedirectionHandler';
+import {PublicOnlyRoute} from 'tg.component/common/PublicOnlyRoute';
+import {SsoRedirectionHandler} from 'tg.component/security/Sso/SsoRedirectionHandler';
+import {ChangeAuthProviderView} from "tg.component/security/ChangeAuthProviderView";
 
 interface LoginRouterProps {}
 
@@ -19,6 +20,9 @@ const LoginRouter: FunctionComponent<LoginRouterProps> = (props) => {
       </PublicOnlyRoute>
       <PublicOnlyRoute path={LINKS.OAUTH_RESPONSE.template}>
         <OAuthRedirectionHandler />
+      </PublicOnlyRoute>
+      <PublicOnlyRoute path={LINKS.CHANGE_AUTH_PROVIDER.template}>
+        <ChangeAuthProviderView />
       </PublicOnlyRoute>
       <PublicOnlyRoute path={LINKS.OPENID_RESPONSE.template}>
         <SsoRedirectionHandler />
