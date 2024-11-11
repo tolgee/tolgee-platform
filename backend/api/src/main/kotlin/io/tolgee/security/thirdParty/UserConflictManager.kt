7e35d3c10749a35b3661736a0d46cecf3265489d
Ivan Manzhosov <manzhiva@fit.cvut.cz>
2024-11-11 20:30:42 +0100

    fix: change logic of changing provider

---
+++ backend/api/src/main/kotlin/io/tolgee/security/thirdParty/UserConflictManager.kt
@@ -0,0 +1,48 @@
+package io.tolgee.security.thirdParty
+
+import io.tolgee.constants.Message
+import io.tolgee.dtos.AuthProviderChangeRequestData
+import io.tolgee.exceptions.AuthenticationException
+import io.tolgee.model.UserAccount
+import io.tolgee.model.enums.ThirdPartyAuthType
+import io.tolgee.service.AuthProviderChangeRequestService
+import org.springframework.context.annotation.Lazy
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class UserConflictManager(
+  @Lazy
+  private val authProviderChangeRequestService: AuthProviderChangeRequestService,
+) {
+  fun manageUserNameConflict(
+    user: UserAccount,
+    newAuthProvider: ThirdPartyAuthType,
+    newAccountType: UserAccount.AccountType,
+    ssoDomain: String? = null,
+    sub: String? = null,
+    refreshToken: String? = null,
+    calculateExpirationDate: Date? = null
+  ) {
+
+    val requestData = AuthProviderChangeRequestData(
+      userAccount = user,
+      newAuthProvider = newAuthProvider,
+      oldAuthProvider = user.thirdPartyAuthType,
+      newAccountType = newAccountType,
+      oldAccountType = user.accountType,
+      ssoDomain = ssoDomain,
+      sub = sub,
+      refreshToken = refreshToken,
+      calculateExpirationDate = calculateExpirationDate
+    )
+    val request = authProviderChangeRequestService.create(requestData)
+    throw AuthenticationException(Message.USERNAME_ALREADY_EXISTS, listOf(request?.id))
+  }
+
+  fun resolveRequestIfExist(user: Optional<UserAccount>) {
+    if(user.isPresent) {
+      authProviderChangeRequestService.resolveChangeRequestIfExist(user.get())
+    }
+  }
+}
---
+++ backend/data/src/main/kotlin/io/tolgee/dtos/AuthProviderChangeRequestData.kt
@@ -0,0 +1,17 @@
+package io.tolgee.dtos
+
+import io.tolgee.model.UserAccount
+import io.tolgee.model.enums.ThirdPartyAuthType
+import java.util.*
+
+data class AuthProviderChangeRequestData(
+  val userAccount: UserAccount,
+  val newAuthProvider: ThirdPartyAuthType?,
+  val oldAuthProvider: ThirdPartyAuthType?,
+  val newAccountType: UserAccount.AccountType,
+  val oldAccountType: UserAccount.AccountType?,
+  val ssoDomain: String? = null,
+  val sub: String? = null,
+  val refreshToken: String? = null,
+  val calculateExpirationDate: Date? = null
+)
