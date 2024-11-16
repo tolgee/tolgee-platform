a397c43f467154636b70e6e454a3b7dfba907cc8
Cynthia <cynthia@cynthia.dev>
2024-11-16 13:01:02 +0100

    chore: upgrade email subproject

--- email/components/LocalizedText.ts
+++ email/components/LocalizedText.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2023 Tolgee s.r.o. and contributors
+ * Copyright (C) 2024 Tolgee s.r.o. and contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
