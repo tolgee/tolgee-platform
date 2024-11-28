8e6dbe04830a612a68721da34fd55939a55d21bc
Jan Cizmar <cizmar@chlupac.com>
2024-11-28 11:58:27 +0100

    chore: Make EE plugin on frontend > fix build

---
+++ webapp/scripts/prepareEE.js
@@ -0,0 +1,25 @@
+// if src/ee exists copy the webapp/src/eePlugin.ee.tsx to webapp/src/eePlugin.local.tsx
+// else copy the webapp/src/eePlugin.oss.tsx to webapp/src/eePlugin.local.tsx
+
+import { existsSync, symlinkSync, unlinkSync } from 'fs';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const srcDir = path.join(__dirname, '../src/ee');
+const eePluginEe = path.join(__dirname, '../src/eePlugin.ee.tsx');
+const eePluginOss = path.join(__dirname, '../src/eePlugin.oss.tsx');
+const eePluginLocal = path.join(__dirname, '../src/eePlugin.local.tsx');
+
+if (existsSync(eePluginLocal)) {
+  unlinkSync(eePluginLocal);
+}
+
+if (existsSync(srcDir)) {
+  symlinkSync(eePluginEe, eePluginLocal);
+} else {
+  symlinkSync(eePluginOss, eePluginLocal);
+}
