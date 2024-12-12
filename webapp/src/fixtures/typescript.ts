4f8235d0c2faa1f5fd2cb98021758e46f851f4da
Štěpán Granát <granat.stepan@gmail.com>
2024-12-12 11:43:29 +0100

    chore: add order_translations to enterprise plan

--- webapp/src/fixtures/typescript.ts
+++ webapp/src/fixtures/typescript.ts
@@ -1,3 +1,13 @@
 export type LiteralUnion<LiteralType extends BaseType, BaseType> =
   | LiteralType
   | (BaseType & { _?: never });
+
+// Utility to check the array is exhaustive
+export type CheckExhaustiveness<
+  T extends readonly any[],
+  U
+> = T[number] extends U
+  ? Exclude<U, T[number]> extends never
+    ? true
+    : false
+  : false;
