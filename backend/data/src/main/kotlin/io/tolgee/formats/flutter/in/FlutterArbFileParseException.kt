efac14cc401172866d5dadcab656a460996fb9ee
Jiri Kuchynka <jiri.kuchynka@codetopic.eu>
2025-01-03 13:30:45 +0100

    fix: use proper base type for flutter parse exceptions (#2655)

--- backend/data/src/main/kotlin/io/tolgee/formats/flutter/in/FlutterArbFileParseException.kt
+++ backend/data/src/main/kotlin/io/tolgee/formats/flutter/in/FlutterArbFileParseException.kt
@@ -1,3 +1,6 @@
 package io.tolgee.formats.flutter.`in`
 
-class FlutterArbFileParseException(cause: Exception) : RuntimeException("Cannot parse arb file", cause)
+import io.tolgee.exceptions.ImportCannotParseFileException
+
+class FlutterArbFileParseException(filename: String, cause: Exception) :
+  ImportCannotParseFileException(filename, "Cannot parse arb file", cause)
