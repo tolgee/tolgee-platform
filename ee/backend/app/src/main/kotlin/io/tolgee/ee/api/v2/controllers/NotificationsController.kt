39c4e7b2b15dc9e21f5074c62d4f345030603d04
StaNov <stanovec@gmail.com>
2025-01-06 14:49:47 +0100

    BE: Fetching notifications from database

--- ee/backend/app/src/main/kotlin/io/tolgee/ee/api/v2/controllers/NotificationsController.kt
+++ ee/backend/app/src/main/kotlin/io/tolgee/ee/api/v2/controllers/NotificationsController.kt
@@ -3,7 +3,9 @@ package io.tolgee.ee.api.v2.controllers
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import io.tolgee.security.authentication.AllowApiAccess
-import org.springframework.http.ResponseEntity
+import io.tolgee.security.authentication.AuthenticationFacade
+import io.tolgee.service.NotificationModel
+import io.tolgee.service.NotificationService
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,31 +19,21 @@ import org.springframework.web.bind.annotation.RestController
   ],
 )
 @Tag(name = "Notifications", description = "Manipulates notifications")
-class NotificationsController {
+class NotificationsController(
+  private val notificationService: NotificationService,
+  private val authenticationFacade: AuthenticationFacade,
+) {
 
   @GetMapping
   @Operation(summary = "Get notifications")
   @AllowApiAccess
-  fun getNotifications(): ResponseEntity<NotificationsResponse> {
-    return ResponseEntity.ok(
-      NotificationsResponse(
-        listOf(
-          Notification(1, 1000000001, "Task One"),
-          Notification(2, 1000000002, "Task Two"),
-          Notification(3, 1000000003, "Task Three"),
-          Notification(4, 1000000004, "Task Four"),
-        )
-      )
+  fun getNotifications(): NotificationsResponse {
+    return NotificationsResponse(
+      notificationService.getNotifications(authenticationFacade.authenticatedUser.id)
     )
   }
 }
 
 data class NotificationsResponse(
-  val notifications: List<Notification>,
-)
-
-data class Notification(
-  val id: Long,
-  val linkedEntityId: Long,
-  val linkedEntityName: String,
+  val notifications: List<NotificationModel>,
 )
