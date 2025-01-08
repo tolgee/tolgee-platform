4c597f54add27aa04a3200993bb1e36b59bb4436
StaNov <stanovec@gmail.com>
2025-01-08 15:12:27 +0100

    chore: Extracted PagedModelWithNextCursor

--- backend/api/src/main/kotlin/io/tolgee/hateoas/translations/KeysWithTranslationsPageModel.kt
+++ backend/api/src/main/kotlin/io/tolgee/hateoas/translations/KeysWithTranslationsPageModel.kt
@@ -1,22 +1,18 @@
 package io.tolgee.hateoas.translations
 
 import io.swagger.v3.oas.annotations.media.Schema
+import io.tolgee.hateoas.PagedModelWithNextCursor
 import io.tolgee.hateoas.language.LanguageModel
 import org.springframework.hateoas.Link
-import org.springframework.hateoas.PagedModel
 
-@Suppress("unused")
 class KeysWithTranslationsPageModel(
   content: Collection<KeyWithTranslationsModel>,
   metadata: PageMetadata?,
   vararg links: Link,
+  @Suppress("unused")
   @field:Schema(
     description = "Provided languages data",
   )
   val selectedLanguages: Collection<LanguageModel>,
-  @field:Schema(
-    description = "Cursor to get next data",
-    example = "eyJrZXlJZCI6eyJkaXJlY3Rpb24iOiJBU0MiLCJ2YWx1ZSI6IjEwMDAwMDAxMjAifX0=",
-  )
-  val nextCursor: String?,
-) : PagedModel<KeyWithTranslationsModel>(content, metadata, links.toList())
+  nextCursor: String?,
+) : PagedModelWithNextCursor<KeyWithTranslationsModel>(content, metadata, links.toList(), nextCursor)
