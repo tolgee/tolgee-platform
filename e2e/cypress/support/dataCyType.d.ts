1feaaa4009075975db1a4c330cb96bbcc622f390
Standa Novák <stanovec@gmail.com>
2025-01-14 15:38:00 +0100

    feat: Notifications MVP (#2813)
    Partially implements #2577.
    
    (cherry picked from commit da3ccc756ea0792f013be4bd6423bfde9e83405b)

--- e2e/cypress/support/dataCyType.d.ts
+++ e2e/cypress/support/dataCyType.d.ts
@@ -347,6 +347,8 @@ declare namespace DataCy {
         "namespaces-select-text-field" |
         "namespaces-selector" |
         "navigation-item" |
+        "notifications-button" |
+        "notifications-list" |
         "order-translation-confirmation" |
         "order-translation-confirmation-ok" |
         "order-translation-invitation-checkbox" |
