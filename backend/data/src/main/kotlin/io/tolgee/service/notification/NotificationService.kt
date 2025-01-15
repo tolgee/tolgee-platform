7da86719c7be229629c7a9e20023420e4cf1b038
Standa Novák <stanovec@gmail.com>
2025-01-15 21:54:07 +0100

    feat: Notifications real-time update with websockets (#2843)
    (cherry picked from commit be62d9291c3d4ac45187a475404e7032d932d042)

--- backend/data/src/main/kotlin/io/tolgee/service/notification/NotificationService.kt
+++ backend/data/src/main/kotlin/io/tolgee/service/notification/NotificationService.kt
@@ -1,14 +1,22 @@
 package io.tolgee.service.notification
 
+import io.tolgee.component.CurrentDateProvider
+import io.tolgee.events.OnNotificationsChangedForUser
 import io.tolgee.model.Notification
 import io.tolgee.repository.NotificationRepository
+import io.tolgee.websocket.*
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
+import org.springframework.transaction.event.TransactionalEventListener
 
 @Service
 class NotificationService(
   private val notificationRepository: NotificationRepository,
+  private val websocketEventPublisher: WebsocketEventPublisher,
+  private val currentDateProvider: CurrentDateProvider,
+  private val applicationEventPublisher: ApplicationEventPublisher,
 ) {
   fun getNotifications(
     userId: Long,
@@ -19,5 +27,16 @@ class NotificationService(
 
   fun save(notification: Notification) {
     notificationRepository.save(notification)
+    applicationEventPublisher.publishEvent(OnNotificationsChangedForUser(notification.user.id))
+  }
+
+  @TransactionalEventListener
+  fun onNotificationSaved(event: OnNotificationsChangedForUser) {
+    websocketEventPublisher(
+      "/users/${event.userId}/${WebsocketEventType.NOTIFICATIONS_CHANGED.typeName}",
+      WebsocketEvent(
+        timestamp = currentDateProvider.date.time,
+      ),
+    )
   }
 }
