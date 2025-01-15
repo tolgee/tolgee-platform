2bc6e12f00937368798bd44fad30b9fe8356e8f6
StaNov <stanovec@gmail.com>
2025-01-15 16:25:11 +0100

    BE tests: Extract generating notifications

--- backend/data/src/main/kotlin/io/tolgee/development/testDataBuilder/data/NotificationsTestData.kt
+++ backend/data/src/main/kotlin/io/tolgee/development/testDataBuilder/data/NotificationsTestData.kt
@@ -1,6 +1,10 @@
 package io.tolgee.development.testDataBuilder.data
 
+import io.tolgee.model.Notification
+
 class NotificationsTestData : BaseTestData() {
+  private var nextTaskId = 100000L
+
   val originatingUser =
     root.addUserAccount {
       name = "originating user"
@@ -23,4 +27,24 @@ class NotificationsTestData : BaseTestData() {
     }
 
   val notification = notificationBuilder.self
+
+  fun generateNotificationWithTask(taskNumber: Long = nextTaskId++): Notification {
+    val task =
+      projectBuilder.addTask {
+        this.name = "Notification task $taskNumber"
+        this.language = englishLanguage
+        this.author = originatingUser.self
+        this.number = taskNumber
+      }
+
+    val notification =
+      userAccountBuilder.addNotification {
+        this.user = this@NotificationsTestData.user
+        this.project = this@NotificationsTestData.project
+        this.linkedTask = task.self
+        this.originatingUser = this@NotificationsTestData.originatingUser.self
+      }
+
+    return notification.self
+  }
 }
