e98b00d29b8468fbfadc52ca530739e8b0e1cc43
Jan Cizmar <cizmar@chlupac.com>
2025-01-16 17:24:36 +0100

    fix: Test trial limit reaching message & fixes

--- webapp/src/component/layout/TopBar/announcements/TrialAnnouncement.tsx
+++ webapp/src/component/layout/TopBar/announcements/TrialAnnouncement.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
 import { TopBarAnnouncementWithIcon } from './TopBarAnnouncementWithIcon';
 import { ClockStopwatch } from '@untitled-ui/icons-react';
-import { Box, Link } from '@mui/material';
+import { Link } from '@mui/material';
 import { T } from '@tolgee/react';
 import { Link as RouterLink } from 'react-router-dom';
 import { useTrialInfo } from './useTrialInfo';
 
 export const TrialAnnouncement: React.FC = () => {
-  const { daysLeft, subscriptionsLink, shouldShowAnnouncement } =
+  const { subscriptionsLink, shouldShowAnnouncement, daysLeft } =
     useTrialInfo();
 
   if (!shouldShowAnnouncement) {
     return null;
   }
 
-  const message =
-    daysLeft > 8 ? (
-      <T
-        keyName="trial-end-topbar-announcement-message"
-        params={{ daysLeft: daysLeft }}
-      />
-    ) : (
+  return (
+    <TopBarAnnouncementWithIcon icon={<ClockStopwatch />}>
       <T
         keyName="trial-end-topbar-announcement-message-urgent"
         params={{
@@ -28,11 +23,6 @@ export const TrialAnnouncement: React.FC = () => {
           link: <Link component={RouterLink} to={subscriptionsLink} />,
         }}
       />
-    );
-
-  return (
-    <TopBarAnnouncementWithIcon icon={<ClockStopwatch />}>
-      <Box>{message}</Box>
     </TopBarAnnouncementWithIcon>
   );
 };
