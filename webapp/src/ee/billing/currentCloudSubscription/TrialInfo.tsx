b9cae0e2102e753b6ed5a9400a0be81c51ce5ccd
Jan Cizmar <cizmar@chlupac.com>
2025-01-17 12:07:26 +0100

    fix: Demo feedback issues

---
+++ webapp/src/ee/billing/currentCloudSubscription/TrialInfo.tsx
@@ -0,0 +1,43 @@
+import { FC } from 'react';
+import { useTranslate } from '@tolgee/react';
+import { useDateFormatter } from 'tg.hooks/useLocale';
+import { Box, styled } from '@mui/material';
+import { components } from 'tg.service/billingApiSchema.generated';
+
+const StyledPositive = styled('span')`
+  color: ${({ theme }) => theme.palette.secondary.main};
+`;
+
+const StyledNegative = styled('span')`
+  color: ${({ theme }) => theme.palette.error.main};
+`;
+
+export const TrialInfo: FC<{
+  subscription: components['schemas']['CloudSubscriptionModel'];
+}> = ({ subscription }) => {
+  const { t } = useTranslate();
+  const formatDate = useDateFormatter();
+
+  if (subscription.status != 'TRIALING') {
+    return null;
+  }
+
+  return (
+    <>
+      <Box gridColumn="1">{t('billing_actual_trial_end')}</Box>
+      <Box gridColumn="2 / -1" data-cy="billing-actual-period-end">
+        {formatDate(subscription.trialEnd, {
+          dateStyle: 'long',
+          timeStyle: 'short',
+        })}{' '}
+        (
+        {!subscription.cancelAtPeriodEnd && subscription.hasPaymentMethod ? (
+          <StyledPositive>{t('billing_trial_renews')}</StyledPositive>
+        ) : (
+          <StyledNegative>{t('billing_trial_will_cancel')}</StyledNegative>
+        )}
+        )
+      </Box>
+    </>
+  );
+};
