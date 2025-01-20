a48ba277f99260e814a62b58ffba2ed0b02ffa40
Štěpán Granát <granat.stepan@gmail.com>
2025-01-20 17:16:18 +0100

    fix: split screenshots big component

--- webapp/src/views/projects/translations/Screenshots/Screenshots.tsx
+++ webapp/src/views/projects/translations/Screenshots/Screenshots.tsx
@@ -1,26 +1,18 @@
 import { useRef, useState } from 'react';
 import { Box, styled, SxProps } from '@mui/material';
 import { components } from 'tg.service/apiSchema.generated';
+import clsx from 'clsx';
 
-import { ScreenshotThumbnail } from './ScreenshotThumbnail';
 import { ScreenshotDetail } from './ScreenshotDetail';
 import { stopAndPrevent } from 'tg.fixtures/eventHandler';
-import { useApiMutation } from 'tg.service/http/useQueryApi';
-import { useProject } from 'tg.hooks/useProject';
-import { useTranslationsActions } from '../context/TranslationsContext';
 import { useScrollStatus } from '../TranslationsTable/useScrollStatus';
-import clsx from 'clsx';
 import { ChevronLeft, ChevronRight } from '@untitled-ui/icons-react';
+import { ScreenshotsList } from './ScreenshotsList';
 
 export const MAX_FILE_COUNT = 20;
 export const ALLOWED_UPLOAD_TYPES = ['image/png', 'image/jpeg', 'image/gif'];
 
 type ScreenshotModel = components['schemas']['ScreenshotModel'];
-
-const MAX_SIZE = 350;
-const MIN_SIZE = 100;
-
-const MAX_HEIGHT = 350;
 
 const StyledContainer = styled(Box)`
   display: grid;
@@ -139,48 +131,8 @@ export const Screenshots = ({
   sx,
 }: Props) => {
   const containerRef = useRef<HTMLDivElement>(null);
-  const size = width && Math.floor(width / 50) * 50;
 
-  const project = useProject();
-  let boundedSize: number | undefined = undefined;
-  if (size) {
-    boundedSize = Math.max(Math.min(MAX_SIZE, size), MIN_SIZE) - 24;
-    if (boundedSize < MIN_SIZE) {
-      boundedSize = undefined;
-    }
-  }
-  const { updateScreenshots } = useTranslationsActions();
-
-  const oneOnly = screenshots.length === 1 && boundedSize && oneBig;
   const [detailData, setDetailData] = useState<number>();
-
-  const deleteLoadable = useApiMutation({
-    url: '/v2/projects/{projectId}/keys/{keyId}/screenshots/{ids}',
-    method: 'delete',
-  });
-
-  const handleDelete = (id: number) => {
-    deleteLoadable.mutate(
-      {
-        path: { projectId: project.id, ids: [id], keyId },
-      },
-      {
-        onSuccess() {
-          updateScreenshots({
-            keyId,
-            screenshots(data) {
-              return data.filter((sc) => sc.id !== id);
-            },
-          });
-        },
-      }
-    );
-  };
-
-  const calculatedHeight = Math.min(
-    Math.max(...screenshots.map((sc) => 100 / (sc.width! / sc.height!))),
-    100
-  );
 
   const screenshotsMapped = screenshots.map((sc) => {
     return {
@@ -221,50 +173,13 @@ export const Screenshots = ({
       onClick={stopAndPrevent()}
     >
       <StyledScrollWrapper ref={containerRef}>
-        {screenshots.map((sc, index) => {
-          let width =
-            oneOnly && boundedSize ? Math.min(boundedSize, sc.width!) : 100;
-          let height =
-            oneOnly && boundedSize
-              ? width / (sc.width! / sc.height!)
-              : calculatedHeight;
-
-          if (height > MAX_HEIGHT && oneOnly) {
-            height = MAX_HEIGHT;
-            width = height * (sc.width! / sc.height!);
-          }
-
-          const isLarge = height > 100 || width > 100;
-
-          const screenshot = {
-            id: sc.id,
-            src: isLarge ? sc.fileUrl : sc.thumbnailUrl,
-            width: sc.width,
-            height: sc.height,
-            highlightedKeyId: keyId,
-            keyReferences: sc.keyReferences,
-          };
-
-          return (
-            <ScreenshotThumbnail
-              key={sc.id}
-              screenshot={screenshot}
-              objectFit={oneOnly ? 'cover' : 'contain'}
-              scaleHighlight={(sc.width! / width - 1) * 0.5 + 1}
-              onClick={() => {
-                setDetailData(index);
-              }}
-              onDelete={() => {
-                handleDelete(sc.id);
-              }}
-              sx={{
-                width,
-                height,
-                scrollSnapAlign: 'center',
-              }}
-            />
-          );
-        })}
+        <ScreenshotsList
+          width={width}
+          keyId={keyId}
+          screenshots={screenshots}
+          oneBig={oneBig}
+          setDetail={setDetailData}
+        />
       </StyledScrollWrapper>
       {scrollLeft && (
         <Box className="arrowLeft" onClick={handleScrollLeft} role="button">
