d84923650024029b0b3b3276614d96ee362b49a7
StaNov <stanovec@gmail.com>
2025-01-21 09:40:57 +0100

    Tests refactoring

--- backend/data/src/main/kotlin/io/tolgee/development/testDataBuilder/data/NotificationsTestData.kt
+++ backend/data/src/main/kotlin/io/tolgee/development/testDataBuilder/data/NotificationsTestData.kt
@@ -11,22 +11,8 @@ class NotificationsTestData : BaseTestData() {
       username = "originatingUser"
     }
 
-  val task =
-    projectBuilder.addTask {
-      this.name = "Notification task"
-      this.language = englishLanguage
-      this.author = originatingUser.self
-    }
-
-  val notificationBuilder =
-    userAccountBuilder.addNotification {
-      this.user = this@NotificationsTestData.user
-      this.project = this@NotificationsTestData.project
-      this.linkedTask = task.self
-      this.originatingUser = this@NotificationsTestData.originatingUser.self
-    }
-
-  val notification = notificationBuilder.self
+  val notification = generateNotificationWithTask()
+  val task = notification.linkedTask
 
   fun generateNotificationWithTask(taskNumber: Long = nextTaskId++): Notification {
     val task =
