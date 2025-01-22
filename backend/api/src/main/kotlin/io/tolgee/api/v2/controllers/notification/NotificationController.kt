ce81f5d58e290f2c69e5851fdb33e3681177efcc
Standa Novák <stanovec@gmail.com>
2025-01-22 17:27:10 +0100

    feat: Websockets optimizations for notifications (#2860)
    * Sending new notification in websocket instead of just ping.
    * Not loading notifications before first opening of the notification
    list.

--- backend/api/src/main/kotlin/io/tolgee/api/v2/controllers/notification/NotificationController.kt
+++ backend/api/src/main/kotlin/io/tolgee/api/v2/controllers/notification/NotificationController.kt
@@ -2,20 +2,28 @@ package io.tolgee.api.v2.controllers.notification
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import io.tolgee.component.CurrentDateProvider
 import io.tolgee.dtos.request.notification.NotificationFilters
+import io.tolgee.events.OnNotificationsChangedForUser
 import io.tolgee.hateoas.notification.NotificationEnhancer
 import io.tolgee.hateoas.notification.NotificationModel
 import io.tolgee.hateoas.notification.NotificationModelAssembler
+import io.tolgee.hateoas.notification.NotificationWebsocketModel
 import io.tolgee.model.Notification
 import io.tolgee.security.authentication.AllowApiAccess
 import io.tolgee.security.authentication.AuthenticationFacade
 import io.tolgee.service.notification.NotificationService
+import io.tolgee.websocket.WebsocketEvent
+import io.tolgee.websocket.WebsocketEventPublisher
+import io.tolgee.websocket.WebsocketEventType
 import org.springdoc.core.annotations.ParameterObject
+import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.web.PagedResourcesAssembler
 import org.springframework.data.web.SortDefault
 import org.springframework.hateoas.PagedModel
+import org.springframework.transaction.event.TransactionalEventListener
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -34,6 +42,8 @@ class NotificationController(
   private val authenticationFacade: AuthenticationFacade,
   private val enhancers: List<NotificationEnhancer>,
   private val pagedResourcesAssembler: PagedResourcesAssembler<Notification>,
+  private val websocketEventPublisher: WebsocketEventPublisher,
+  private val currentDateProvider: CurrentDateProvider,
 ) {
   @GetMapping
   @Operation(summary = "Gets notifications of the currently logged in user, newest is first.")
@@ -54,6 +64,28 @@ class NotificationController(
     return pagedResourcesAssembler.toModel(
       notifications,
       NotificationModelAssembler(enhancers, notifications),
+    )
+  }
+
+  @TransactionalEventListener
+  fun onNotificationsChanged(event: OnNotificationsChangedForUser) {
+    val websocketModel =
+      NotificationWebsocketModel(
+        notificationService.getCountOfUnseenNotifications(event.userId),
+        event.newNotification?.let {
+          NotificationModelAssembler(enhancers, PageImpl(listOf(it))).toModel(it)
+        },
+      )
+
+    val websocketEvent =
+      WebsocketEvent(
+        data = websocketModel,
+        timestamp = currentDateProvider.date.time,
+      )
+
+    websocketEventPublisher(
+      "/users/${event.userId}/${WebsocketEventType.NOTIFICATIONS_CHANGED.typeName}",
+      websocketEvent,
     )
   }
 }
--- backend/data/src/main/kotlin/io/tolgee/service/notification/NotificationService.kt
+++ backend/data/src/main/kotlin/io/tolgee/service/notification/NotificationService.kt
@@ -1,24 +1,19 @@
 package io.tolgee.service.notification
 
-import io.tolgee.component.CurrentDateProvider
 import io.tolgee.dtos.request.notification.NotificationFilters
 import io.tolgee.events.OnNotificationsChangedForUser
 import io.tolgee.model.Notification
 import io.tolgee.repository.NotificationRepository
-import io.tolgee.websocket.WebsocketEvent
-import io.tolgee.websocket.WebsocketEventPublisher
-import io.tolgee.websocket.WebsocketEventType
+import jakarta.transaction.Transactional
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
-import org.springframework.transaction.event.TransactionalEventListener
 
 @Service
 class NotificationService(
   private val notificationRepository: NotificationRepository,
-  private val websocketEventPublisher: WebsocketEventPublisher,
-  private val currentDateProvider: CurrentDateProvider,
   private val applicationEventPublisher: ApplicationEventPublisher,
 ) {
   fun getNotifications(
@@ -27,11 +22,27 @@ class NotificationService(
     filters: NotificationFilters,
   ): Page<Notification> = notificationRepository.fetchNotificationsByUserId(userId, pageable, filters)
 
+  fun getCountOfUnseenNotifications(userId: Long): Int =
+    notificationRepository
+      .fetchNotificationsByUserId(
+        userId,
+        PageRequest.of(0, 1),
+        NotificationFilters(false),
+      ).totalElements
+      .toInt()
+
+  @Transactional
   fun save(notification: Notification) {
     notificationRepository.save(notification)
-    applicationEventPublisher.publishEvent(OnNotificationsChangedForUser(notification.user.id))
+    applicationEventPublisher.publishEvent(
+      OnNotificationsChangedForUser(
+        notification.user.id,
+        notification,
+      ),
+    )
   }
 
+  @Transactional
   fun markNotificationsAsSeen(
     notificationIds: List<Long>,
     userId: Long,
@@ -39,21 +50,11 @@ class NotificationService(
     val modifiedCount = notificationRepository.markNotificationsAsSeen(notificationIds, userId)
 
     if (modifiedCount > 0) {
-      sendWebsocketEvent(userId)
+      applicationEventPublisher.publishEvent(
+        OnNotificationsChangedForUser(
+          userId,
+        ),
+      )
     }
-  }
-
-  @TransactionalEventListener
-  fun onNotificationSaved(event: OnNotificationsChangedForUser) {
-    sendWebsocketEvent(event.userId)
-  }
-
-  private fun sendWebsocketEvent(userId: Long) {
-    websocketEventPublisher(
-      "/users/$userId/${WebsocketEventType.NOTIFICATIONS_CHANGED.typeName}",
-      WebsocketEvent(
-        timestamp = currentDateProvider.date.time,
-      ),
-    )
   }
 }
