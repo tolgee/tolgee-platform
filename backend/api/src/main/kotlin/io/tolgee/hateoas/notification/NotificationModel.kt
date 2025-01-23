432377f7f9206c7251cbc3da3410962b1de5f28b
Standa Novák <stanovec@gmail.com>
2025-01-23 11:57:08 +0100

    feat: Display notification creation time + originating user + avatar + project name + flag (#2861)

--- backend/api/src/main/kotlin/io/tolgee/hateoas/notification/NotificationModel.kt
+++ backend/api/src/main/kotlin/io/tolgee/hateoas/notification/NotificationModel.kt
@@ -2,11 +2,15 @@ package io.tolgee.hateoas.notification
 
 import io.tolgee.hateoas.project.SimpleProjectModel
 import io.tolgee.hateoas.task.TaskModel
+import io.tolgee.hateoas.userAccount.SimpleUserAccountModel
 import org.springframework.hateoas.RepresentationModel
 import java.io.Serializable
+import java.util.*
 
 data class NotificationModel(
   val id: Long,
   var project: SimpleProjectModel? = null,
+  var originatingUser: SimpleUserAccountModel? = null,
   var linkedTask: TaskModel? = null,
+  var createdAt: Date?,
 ) : RepresentationModel<NotificationModel>(), Serializable
