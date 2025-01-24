af5036aef62935a5bc0b259fda36ec851a75045c
Standa Novák <stanovec@gmail.com>
2025-01-24 07:54:58 +0100

    feat: Send notification on task completed, MFA enabled+disabled, password chaged (#2865)

--- backend/api/src/main/kotlin/io/tolgee/hateoas/notification/NotificationModel.kt
+++ backend/api/src/main/kotlin/io/tolgee/hateoas/notification/NotificationModel.kt
@@ -1,5 +1,6 @@
 package io.tolgee.hateoas.notification
 
+import io.tolgee.constants.NotificationType
 import io.tolgee.hateoas.project.SimpleProjectModel
 import io.tolgee.hateoas.task.TaskModel
 import io.tolgee.hateoas.userAccount.SimpleUserAccountModel
@@ -10,6 +11,7 @@ import java.util.*
 data class NotificationModel(
   val id: Long,
   var project: SimpleProjectModel? = null,
+  var type: NotificationType,
   var originatingUser: SimpleUserAccountModel? = null,
   var linkedTask: TaskModel? = null,
   var createdAt: Date?,
--- ee/backend/app/src/main/kotlin/io/tolgee/ee/service/TaskService.kt
+++ ee/backend/app/src/main/kotlin/io/tolgee/ee/service/TaskService.kt
@@ -2,19 +2,14 @@ package io.tolgee.ee.service
 
 import io.tolgee.component.CurrentDateProvider
 import io.tolgee.constants.Message
+import io.tolgee.constants.NotificationType
 import io.tolgee.ee.component.TaskReportHelper
-import io.tolgee.ee.data.task.CalculateScopeRequest
-import io.tolgee.ee.data.task.CreateTaskRequest
-import io.tolgee.ee.data.task.TaskFilters
-import io.tolgee.ee.data.task.TranslationScopeFilters
-import io.tolgee.ee.data.task.UpdateTaskKeyRequest
-import io.tolgee.ee.data.task.UpdateTaskKeyResponse
-import io.tolgee.ee.data.task.UpdateTaskKeysRequest
-import io.tolgee.ee.data.task.UpdateTaskRequest
+import io.tolgee.ee.data.task.*
 import io.tolgee.ee.repository.TaskRepository
 import io.tolgee.exceptions.BadRequestException
 import io.tolgee.exceptions.NotFoundException
 import io.tolgee.model.Language
+import io.tolgee.model.Notification
 import io.tolgee.model.Project
 import io.tolgee.model.UserAccount
 import io.tolgee.model.enums.TaskState
@@ -30,6 +25,7 @@ import io.tolgee.repository.TaskKeyRepository
 import io.tolgee.security.authentication.AuthenticationFacade
 import io.tolgee.service.key.KeyService
 import io.tolgee.service.language.LanguageService
+import io.tolgee.service.notification.NotificationService
 import io.tolgee.service.project.ProjectService
 import io.tolgee.service.security.PermissionService
 import io.tolgee.service.security.SecurityService
@@ -69,6 +65,7 @@ class TaskService(
   private val permissionService: PermissionService,
   @Lazy
   private val taskService: TaskService,
+  private val notificationService: NotificationService,
 ) : ITaskService {
   fun getAllPaged(
     projectId: Long,
@@ -247,6 +244,7 @@ class TaskService(
       task.state = state
     }
     taskRepository.saveAndFlush(task)
+    createNotificationIfApplicable(task)
     return getTask(projectId, taskNumber)
   }
 
@@ -557,6 +555,21 @@ class TaskService(
       )
     }
     return null
+  }
+
+  private fun createNotificationIfApplicable(task: Task) {
+    if (task.state != TaskState.DONE) return
+    val author = task.author ?: return
+
+    notificationService.save(
+      Notification().apply {
+        type = NotificationType.TASK_COMPLETED
+        user = author
+        project = task.project
+        originatingUser = authenticationFacade.authenticatedUserEntity
+        linkedTask = task
+      },
+    )
   }
 
   override fun getAgencyTasks(agencyId: Long): List<Task> {
