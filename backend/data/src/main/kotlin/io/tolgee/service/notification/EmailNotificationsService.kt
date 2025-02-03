eba687613d6a60e0b481436348d968ba595bce79
StaNov <stanovec@gmail.com>
2025-02-03 10:52:19 +0100

    Extracted compose method

--- backend/data/src/main/kotlin/io/tolgee/service/notification/EmailNotificationsService.kt
+++ backend/data/src/main/kotlin/io/tolgee/service/notification/EmailNotificationsService.kt
@@ -17,38 +17,7 @@ class EmailNotificationsService(
   private val frontendUrlProvider: FrontendUrlProvider,
 ) : Logging {
   fun sendEmailNotification(notification: Notification) {
-    val params =
-      when (notification.type) {
-        NotificationType.TASK_ASSIGNED -> {
-          val task =
-            notification.linkedTask
-              ?: throw IllegalStateException("Notification of type ${notification.type} must contain linkedTask.")
-          val user = notification.user
-          val taskUrl = getTaskUrl(task.project.id, task.number)
-          val myTasksUrl = getMyTasksUrl()
-
-          EmailParams(
-            to = user.username,
-            subject = "New task assignment",
-            text =
-              """
-              Hello! 👋<br/><br/>          
-              You've been assigned to a task:<br/>
-              <a href="$taskUrl">${task.name} #${task.number} (${task.language.name}) - ${
-                getTaskTypeName(task.type)
-              }</a><br/><br/>
-              
-              
-              Check all your tasks <a href="$myTasksUrl">here</a>.<br/><br/>
-              
-              Regards,<br/>
-              Tolgee
-              """.trimIndent(),
-          )
-        }
-
-        else -> null // TODO add branches for all types, remove else
-      }
+    val params = composeEmailParams(notification)
 
     try {
       params?.let { tolgeeEmailSender.sendEmail(it) } // TODO remove nullable
@@ -58,22 +27,50 @@ class EmailNotificationsService(
     }
   }
 
-  private fun getTaskTypeName(type: TaskType): String {
-    return if (type === TaskType.TRANSLATE) {
+  private fun composeEmailParams(notification: Notification) =
+    when (notification.type) {
+      NotificationType.TASK_ASSIGNED -> {
+        val task =
+          notification.linkedTask
+            ?: throw IllegalStateException("Notification of type ${notification.type} must contain linkedTask.")
+        val user = notification.user
+        val taskUrl = getTaskUrl(task.project.id, task.number)
+        val myTasksUrl = getMyTasksUrl()
+
+        EmailParams(
+          to = user.username,
+          subject = "New task assignment",
+          text =
+            """
+                Hello! 👋<br/><br/>          
+                You've been assigned to a task:<br/>
+                <a href="$taskUrl">${task.name} #${task.number} (${task.language.name}) - ${
+              getTaskTypeName(task.type)
+            }</a><br/><br/>
+                
+                
+                Check all your tasks <a href="$myTasksUrl">here</a>.<br/><br/>
+                
+                Regards,<br/>
+                Tolgee
+            """.trimIndent(),
+        )
+      }
+
+      else -> null // TODO add branches for all types, remove else
+    }
+
+  private fun getTaskTypeName(type: TaskType): String =
+    if (type === TaskType.TRANSLATE) {
       "translate"
     } else {
       "review"
     }
-  }
 
   fun getTaskUrl(
     projectId: Long,
     taskId: Long,
-  ): String {
-    return "${frontendUrlProvider.url}/projects/$projectId/task?number=$taskId&detail=true"
-  }
+  ): String = "${frontendUrlProvider.url}/projects/$projectId/task?number=$taskId&detail=true"
 
-  fun getMyTasksUrl(): String {
-    return "${frontendUrlProvider.url}/my-tasks"
-  }
+  fun getMyTasksUrl(): String = "${frontendUrlProvider.url}/my-tasks"
 }
