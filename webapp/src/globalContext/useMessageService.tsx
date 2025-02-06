61db3eb7048ce29cb9c58608546db1f063d1a0ee
Štěpán Granát <granat.stepan@gmail.com>
2025-02-06 12:08:40 +0100

    fix: closable notifications

--- webapp/src/globalContext/useMessageService.tsx
+++ webapp/src/globalContext/useMessageService.tsx
@@ -1,17 +1,43 @@
-import { VariantType, useSnackbar } from 'notistack';
 import { ReactNode } from 'react';
+import { IconButton, styled, touchRippleClasses } from '@mui/material';
+import { VariantType, useSnackbar, closeSnackbar } from 'notistack';
+import { X } from '@untitled-ui/icons-react';
+
+const StyledButton = styled(IconButton)`
+  color: white;
+  &:hover {
+    background-color: #ffffff1b;
+  }
+  .${touchRippleClasses.child} {
+    background-color: white;
+  }
+`;
 
 export type Message = {
   text: ReactNode | string;
   variant: VariantType;
 };
 
+const action = (snackbarId) => (
+  <>
+    <StyledButton
+      className="notistack-CloseButton"
+      size="small"
+      onClick={() => {
+        closeSnackbar(snackbarId);
+      }}
+    >
+      <X />
+    </StyledButton>
+  </>
+);
+
 export const useMessageService = () => {
   const { enqueueSnackbar } = useSnackbar();
 
   const actions = {
     showMessage(m: Message) {
-      enqueueSnackbar(m.text, { variant: m.variant });
+      enqueueSnackbar(m.text, { variant: m.variant, action });
     },
   };
 
