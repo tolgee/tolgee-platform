a5a1e3c63d4e6f403fa33bbe3f84017eac788416
Jan Cizmar <cizmar@chlupac.com>
2025-02-14 17:11:28 +0100

    chore: fix correct test classes are run, remove the need to recreate context

--- ee/backend/tests/src/test/kotlin/io/tolgee/ee/slack/SlackWithAutoTranslationTest.kt
+++ ee/backend/tests/src/test/kotlin/io/tolgee/ee/slack/SlackWithAutoTranslationTest.kt
@@ -8,7 +8,6 @@ import io.tolgee.dtos.request.translation.SetTranslationsWithKeyDto
 import io.tolgee.fixtures.MachineTranslationTest
 import io.tolgee.fixtures.andIsCreated
 import io.tolgee.fixtures.waitForNotThrowing
-import io.tolgee.testing.ContextRecreatingTest
 import io.tolgee.testing.annotations.ProjectJWTAuthTestMethod
 import io.tolgee.testing.assert
 import org.assertj.core.api.Assertions.assertThat
@@ -18,12 +17,9 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.test.annotation.DirtiesContext
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
-@ContextRecreatingTest
 class SlackWithAutoTranslationTest : MachineTranslationTest() {
   @Autowired
   @MockBean
--- ee/backend/tests/src/test/kotlin/io/tolgee/ee/slack/SlackWithBatchOperationTest.kt
+++ ee/backend/tests/src/test/kotlin/io/tolgee/ee/slack/SlackWithBatchOperationTest.kt
@@ -10,7 +10,6 @@ import io.tolgee.fixtures.andIsOk
 import io.tolgee.fixtures.waitFor
 import io.tolgee.fixtures.waitForNotThrowing
 import io.tolgee.model.slackIntegration.SavedSlackMessage
-import io.tolgee.testing.ContextRecreatingTest
 import io.tolgee.testing.annotations.ProjectJWTAuthTestMethod
 import io.tolgee.testing.assert
 import org.junit.jupiter.api.BeforeEach
@@ -19,12 +18,9 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.test.annotation.DirtiesContext
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
-@ContextRecreatingTest
 class SlackWithBatchOperationTest : MachineTranslationTest() {
   @Autowired
   @MockBean
