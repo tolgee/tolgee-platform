079943fe965b81c1adb497e8e7eadaacce591464
Jan Cizmar <cizmar@chlupac.com>
2025-02-19 17:23:00 +0100

    fix: Prevent assigning non-active plans

--- backend/data/src/main/kotlin/io/tolgee/api/SubscriptionStatus.kt
+++ backend/data/src/main/kotlin/io/tolgee/api/SubscriptionStatus.kt
@@ -10,6 +10,23 @@ enum class SubscriptionStatus(
   ERROR(false),
   TRIALING(true),
 
-  // might be stored on the EE side, but not license server (billing) side
+  /** might be stored on the EE side, but not license server (billing) side */
   KEY_USED_BY_ANOTHER_INSTANCE(false),
+
+  /** when we cannot map from stripe status */
+  UNKNOWN(false),
+  ;
+
+  companion object {
+    fun fromStripeStatus(stripeStatus: String?): SubscriptionStatus {
+      return when (stripeStatus) {
+        "active" -> ACTIVE
+        "canceled" -> CANCELED
+        "past_due" -> PAST_DUE
+        "unpaid" -> UNPAID
+        "trialing" -> TRIALING
+        else -> UNKNOWN
+      }
+    }
+  }
 }
