170ca0179a5957f75de084fb795639278e2fbda9
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2025-02-24 13:27:51 +0100

    fix: propely mock properties

--- backend/security/src/test/kotlin/io/tolgee/security/authentication/SsoAuthenticationInterceptorTest.kt
+++ backend/security/src/test/kotlin/io/tolgee/security/authentication/SsoAuthenticationInterceptorTest.kt
@@ -1,5 +1,6 @@
 package io.tolgee.security.authentication
 
+import io.tolgee.configuration.tolgee.AuthenticationProperties
 import io.tolgee.configuration.tolgee.TolgeeProperties
 import io.tolgee.dtos.cacheable.UserAccountDto
 import io.tolgee.fixtures.andIsForbidden
@@ -23,6 +24,8 @@ class SsoAuthenticationInterceptorTest {
 
   private val tolgeeProperties = Mockito.mock(TolgeeProperties::class.java)
 
+  private val authenticationProperties = Mockito.mock(AuthenticationProperties::class.java)
+
   private val tenantService = Mockito.mock(TenantService::class.java)
 
   private val ssoAuthenticationInterceptor =
@@ -35,7 +38,8 @@ class SsoAuthenticationInterceptorTest {
 
   @BeforeEach
   fun setupMocks() {
-    Mockito.`when`(tolgeeProperties.authentication.enabled).thenReturn(true)
+    Mockito.`when`(tolgeeProperties.authentication).thenReturn(authenticationProperties)
+    Mockito.`when`(authenticationProperties.enabled).thenReturn(true)
     Mockito.`when`(authenticationFacade.authenticatedUser).thenReturn(userAccount)
     Mockito.`when`(authenticationFacade.isAuthenticated).thenReturn(true)
     Mockito.`when`(userAccount.username).thenReturn("user@domain.com")
