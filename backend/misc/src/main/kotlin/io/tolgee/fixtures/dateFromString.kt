346affaa24eeeba466faf87c574b6a9c3c652e47
Jan Cizmar <cizmar@chlupac.com>
2025-02-25 16:05:15 +0100

    chore: KtLint

---
+++ backend/misc/src/main/kotlin/io/tolgee/fixtures/dateFromString.kt
@@ -0,0 +1,15 @@
+package io.tolgee.fixtures
+
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+fun dateFromString(
+  dateString: String,
+  pattern: String = "yyyy-MM-dd",
+): Date {
+  val formatter = DateTimeFormatter.ofPattern(pattern)
+  val localDate = LocalDate.parse(dateString, formatter)
+  return Date.from(localDate.atStartOfDay(ZoneId.of("UTC")).toInstant())
+}
