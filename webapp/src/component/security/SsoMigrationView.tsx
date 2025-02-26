ef80a27523de77c36230e8615a33eebb87bd0235
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2025-02-26 16:47:32 +0100

    feat: implement sso migration ui prototype

---
+++ webapp/src/component/security/SsoMigrationView.tsx
@@ -0,0 +1,136 @@
+import { useHistory } from 'react-router-dom';
+import { T, useTranslate } from '@tolgee/react';
+import { Box, Paper, styled, Typography } from '@mui/material';
+
+import { LINKS } from 'tg.constants/links';
+import { messageService } from 'tg.service/MessageService';
+import { useApiQuery } from 'tg.service/http/useQueryApi';
+import { DashboardPage } from 'tg.component/layout/DashboardPage';
+import { useWindowTitle } from 'tg.hooks/useWindowTitle';
+import LoadingButton from 'tg.component/common/form/LoadingButton';
+import { FullPageLoading } from 'tg.component/common/FullPageLoading';
+import { TranslatedError } from 'tg.translationTools/TranslatedError';
+import React from 'react';
+import { Key02 } from '@untitled-ui/icons-react';
+import {
+  useGlobalActions,
+  useGlobalContext,
+} from 'tg.globalContext/GlobalContext';
+import { useUser } from 'tg.globalContext/helpers';
+
+export const FULL_PAGE_BREAK_POINT = '(max-width: 700px)';
+
+const StyledContainer = styled(Box)`
+  display: grid;
+  justify-content: center;
+  align-content: center;
+`;
+
+const StyledContent = styled(Box)`
+  display: grid;
+  width: min(100vw, 660px);
+  gap: 32px;
+`;
+
+const StyledPaper = styled(Paper)`
+  padding: 60px;
+  display: grid;
+  gap: 32px;
+  background: ${({ theme }) => theme.palette.tokens.background['paper-1']};
+  @media ${FULL_PAGE_BREAK_POINT} {
+    padding: 10px;
+    box-shadow: none;
+    background: transparent;
+  }
+`;
+
+const SsoMigrationView: React.FC = () => {
+  const { useSsoAuthLinkByDomain } = useGlobalActions();
+  const user = useUser();
+
+  const migrationRequired = useGlobalContext(
+    (c) =>
+      c.initialData.ssoInfo?.force &&
+      c.initialData.userInfo?.accountType !== 'MANAGED'
+  );
+
+  const history = useHistory();
+  const { t } = useTranslate();
+
+  useWindowTitle(t('sso_migration_title'));
+
+  const authProviderChangeInfo = useApiQuery({
+    url: '/v2/auth-provider/change',
+    method: 'get',
+    options: {
+      onError(e) {
+        if (e.code && e.code != 'resource_not_found') {
+          messageService.error(<TranslatedError code={e.code} />);
+        }
+      },
+    },
+  });
+
+  const ssoUrl = useSsoAuthLinkByDomain(user?.domain || '');
+  const ssoUrlUnavailable = !ssoUrl.isLoading && !ssoUrl.data?.redirectUrl;
+  const ssoUrlValue = ssoUrl.data?.redirectUrl || '';
+
+  if (!migrationRequired) {
+    history.replace(LINKS.ROOT.build());
+  }
+
+  if (authProviderChangeInfo.isLoading) {
+    return <FullPageLoading />;
+  }
+
+  if (authProviderChangeInfo.data) {
+    history.replace(LINKS.ACCEPT_AUTH_PROVIDER_CHANGE.build());
+  }
+
+  return (
+    <DashboardPage hideQuickStart>
+      <StyledContainer>
+        <StyledContent>
+          <StyledPaper>
+            <Typography variant="h3" sx={{ textAlign: 'center' }}>
+              <T keyName="sso_migration_title" />
+            </Typography>
+
+            <Box display="grid" gap="24px" justifyItems="center">
+              <Box
+                textAlign="center"
+                data-cy="accept-auth-provider-change-info-text"
+              >
+                <T keyName="sso_migration_description" />
+                {ssoUrlUnavailable && (
+                  // TODO: use error color
+                  <T keyName="sso_migration_redirect_url_failed" />
+                )}
+              </Box>
+              <Box
+                display="flex"
+                gap={3}
+                flexWrap="wrap"
+                justifyContent="center"
+              >
+                <LoadingButton
+                  loading={ssoUrl.isLoading}
+                  href={ssoUrlValue}
+                  disabled={ssoUrlUnavailable}
+                  size="medium"
+                  endIcon={<Key02 />}
+                  variant="outlined"
+                  style={{ marginBottom: '0.5rem' }}
+                  color="inherit"
+                >
+                  <T keyName="connect_sso" />
+                </LoadingButton>
+              </Box>
+            </Box>
+          </StyledPaper>
+        </StyledContent>
+      </StyledContainer>
+    </DashboardPage>
+  );
+};
+export default SsoMigrationView;
