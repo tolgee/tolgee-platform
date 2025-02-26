423cbbba86bdb0eb6331f70dd48994559fcf3ead
Štěpán Granát <granat.stepan@gmail.com>
2025-02-26 14:34:41 +0100

    fix: validate tasks correctly

--- ee/backend/app/src/main/kotlin/io/tolgee/ee/data/task/CreateMultipleTasksRequest.kt
+++ ee/backend/app/src/main/kotlin/io/tolgee/ee/data/task/CreateMultipleTasksRequest.kt
@@ -1,5 +1,8 @@
 package io.tolgee.ee.data.task
 
+import jakarta.validation.Valid
+
 class CreateMultipleTasksRequest(
+  @field:Valid
   var tasks: MutableSet<CreateTaskRequest> = mutableSetOf(),
 )
