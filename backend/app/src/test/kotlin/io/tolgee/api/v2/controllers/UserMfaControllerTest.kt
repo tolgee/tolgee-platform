Tolgee/server

Tests for MFA and password change

backend/app/src/test/kotlin/io/tolgee/api/v2/controllers/UserMfaControllerTest.kt
@@ -7,11 +7,16 @@ import io.tolgee.fixtures.andIsBadRequest
 import io.tolgee.fixtures.andIsForbidden
 import io.tolgee.fixtures.andIsOk
 import io.tolgee.fixtures.retry
+import io.tolgee.model.notifications.NotificationType
+import io.tolgee.model.notifications.NotificationType.MFA_DISABLED
+import io.tolgee.model.notifications.NotificationType.MFA_ENABLED
 import io.tolgee.testing.AuthorizedControllerTest
+import io.tolgee.testing.NotificationUtil
 import io.tolgee.testing.assertions.Assertions.assertThat
 import org.apache.commons.codec.binary.Base32
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 class UserMfaControllerTest : AuthorizedControllerTest() {
@@ -20,6 +25,9 @@ class UserMfaControllerTest : AuthorizedControllerTest() {
   }
 
   private val encodedKey: ByteArray = Base32().decode(TOTP_KEY)
+
+  @Autowired
+  private lateinit var notificationUtil: NotificationUtil
 
   fun enableMfa() {
     userAccount?.let {
@@ -40,6 +48,12 @@ class UserMfaControllerTest : AuthorizedControllerTest() {
       performAuthPut("/v2/user/mfa/totp", requestDto).andIsOk
       val fromDb = userAccountService.findActive(initialUsername)
       Assertions.assertThat(fromDb!!.totpKey).isEqualTo(encodedKey)
+
+      notificationUtil.newestNotification().also {
+        assertThat(it.type).isEqualTo(MFA_ENABLED)
+        assertThat(it.user.id).isEqualTo(userAccount?.id)
+        assertThat(it.originatingUser?.id).isEqualTo(userAccount?.id)
+      }
     }
   }
 
@@ -53,6 +67,12 @@ class UserMfaControllerTest : AuthorizedControllerTest() {
     performAuthDelete("/v2/user/mfa/totp", requestDto).andIsOk
     val fromDb = userAccountService.findActive(initialUsername)
     Assertions.assertThat(fromDb!!.totpKey).isNull()
+
+    notificationUtil.newestNotification().also {
+      assertThat(it.type).isEqualTo(MFA_DISABLED)
+      assertThat(it.user.id).isEqualTo(userAccount?.id)
+      assertThat(it.originatingUser?.id).isEqualTo(userAccount?.id)
+    }
   }
 
   @Test

backend/app/src/test/kotlin/io/tolgee/api/v2/controllers/V2UserControllerTest.kt
@@ -7,8 +7,10 @@ import io.tolgee.dtos.request.UserUpdatePasswordRequestDto
 import io.tolgee.dtos.request.UserUpdateRequestDto
 import io.tolgee.fixtures.*
 import io.tolgee.model.UserAccount
+import io.tolgee.model.notifications.NotificationType.PASSWORD_CHANGED
 import io.tolgee.testing.AuthorizedControllerTest
 import io.tolgee.testing.ContextRecreatingTest
+import io.tolgee.testing.NotificationUtil
 import io.tolgee.testing.assert
 import io.tolgee.testing.assertions.Assertions.assertThat
 import org.assertj.core.api.Assertions
@@ -31,6 +33,9 @@ class V2UserControllerTest : AuthorizedControllerTest() {
 
   @Autowired
   private lateinit var emailTestUtil: EmailTestUtil
+
+  @Autowired
+  private lateinit var notificationUtil: NotificationUtil
 
   @BeforeEach
   fun init() {
@@ -61,6 +66,12 @@ class V2UserControllerTest : AuthorizedControllerTest() {
     val fromDb = userAccountService.findActive(initialUsername)
     Assertions.assertThat(passwordEncoder.matches(requestDTO.password, fromDb!!.password))
       .describedAs("Password is changed").isTrue
+
+    notificationUtil.newestNotification().also {
+      assertThat(it.type).isEqualTo(PASSWORD_CHANGED)
+      assertThat(it.user.id).isEqualTo(userAccount?.id)
+      assertThat(it.originatingUser?.id).isEqualTo(userAccount?.id)
+    }
   }
 
   @Test

tolgee/tolgee-platform

backend/app/src/test/kotlin/io/tolgee/api/v2/controllers/UserMfaControllerTest.kt
@@ -7,11 +7,15 @@ import io.tolgee.fixtures.andIsBadRequest
 import io.tolgee.fixtures.andIsForbidden
 import io.tolgee.fixtures.andIsOk
 import io.tolgee.fixtures.retry
+import io.tolgee.model.notifications.NotificationType.MFA_DISABLED
+import io.tolgee.model.notifications.NotificationType.MFA_ENABLED
 import io.tolgee.testing.AuthorizedControllerTest
+import io.tolgee.testing.NotificationTestUtil
 import io.tolgee.testing.assertions.Assertions.assertThat
 import org.apache.commons.codec.binary.Base32
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 class UserMfaControllerTest : AuthorizedControllerTest() {
@@ -20,6 +24,9 @@ class UserMfaControllerTest : AuthorizedControllerTest() {
   }
 
   private val encodedKey: ByteArray = Base32().decode(TOTP_KEY)
+
+  @Autowired
+  private lateinit var notificationUtil: NotificationTestUtil
 
   fun enableMfa() {
     userAccount?.let {
@@ -40,6 +47,12 @@ class UserMfaControllerTest : AuthorizedControllerTest() {
       performAuthPut("/v2/user/mfa/totp", requestDto).andIsOk
       val fromDb = userAccountService.findActive(initialUsername)
       Assertions.assertThat(fromDb!!.totpKey).isEqualTo(encodedKey)
+
+      notificationUtil.newestInAppNotification().also {
+        assertThat(it.type).isEqualTo(MFA_ENABLED)
+        assertThat(it.user.id).isEqualTo(userAccount?.id)
+        assertThat(it.originatingUser?.id).isEqualTo(userAccount?.id)
+      }
     }
   }
 
@@ -53,6 +66,12 @@ class UserMfaControllerTest : AuthorizedControllerTest() {
     performAuthDelete("/v2/user/mfa/totp", requestDto).andIsOk
     val fromDb = userAccountService.findActive(initialUsername)
     Assertions.assertThat(fromDb!!.totpKey).isNull()
+
+    notificationUtil.newestInAppNotification().also {
+      assertThat(it.type).isEqualTo(MFA_DISABLED)
+      assertThat(it.user.id).isEqualTo(userAccount?.id)
+      assertThat(it.originatingUser?.id).isEqualTo(userAccount?.id)
+    }
   }
 
   @Test

backend/app/src/test/kotlin/io/tolgee/api/v2/controllers/V2UserControllerTest.kt
@@ -7,8 +7,10 @@ import io.tolgee.dtos.request.UserUpdatePasswordRequestDto
 import io.tolgee.dtos.request.UserUpdateRequestDto
 import io.tolgee.fixtures.*
 import io.tolgee.model.UserAccount
+import io.tolgee.model.notifications.NotificationType.PASSWORD_CHANGED
 import io.tolgee.testing.AuthorizedControllerTest
 import io.tolgee.testing.ContextRecreatingTest
+import io.tolgee.testing.NotificationTestUtil
 import io.tolgee.testing.assert
 import io.tolgee.testing.assertions.Assertions.assertThat
 import org.assertj.core.api.Assertions
@@ -31,6 +33,9 @@ class V2UserControllerTest : AuthorizedControllerTest() {
 
   @Autowired
   private lateinit var emailTestUtil: EmailTestUtil
+
+  @Autowired
+  private lateinit var notificationUtil: NotificationTestUtil
 
   @BeforeEach
   fun init() {
@@ -61,6 +66,12 @@ class V2UserControllerTest : AuthorizedControllerTest() {
     val fromDb = userAccountService.findActive(initialUsername)
     Assertions.assertThat(passwordEncoder.matches(requestDTO.password, fromDb!!.password))
       .describedAs("Password is changed").isTrue
+
+    notificationUtil.newestInAppNotification().also {
+      assertThat(it.type).isEqualTo(PASSWORD_CHANGED)
+      assertThat(it.user.id).isEqualTo(userAccount?.id)
+      assertThat(it.originatingUser?.id).isEqualTo(userAccount?.id)
+    }
   }
 
   @Test