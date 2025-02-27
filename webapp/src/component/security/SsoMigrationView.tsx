bcf88f414c474f143829bcf0ac13b91f3b67900d
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2025-02-27 18:16:43 +0100

    fix: remove url load error text for now

--- webapp/src/component/security/SsoMigrationView.tsx
+++ webapp/src/component/security/SsoMigrationView.tsx
@@ -111,10 +111,10 @@ const SsoMigrationView: React.FC = () => {
                   keyName="sso_migration_description"
                   params={{ domain: user?.domain || '', br: <br /> }}
                 />
-                {ssoUrlUnavailable && (
-                  // TODO: use error color
-                  <T keyName="sso_migration_redirect_url_failed" />
-                )}
+                {/*{ssoUrlUnavailable && (*/}
+                {/*  // TODO: should we do this? ; use error color*/}
+                {/*  <T keyName="sso_migration_redirect_url_failed" />*/}
+                {/*)}*/}
               </Box>
               <Box
                 display="flex"
