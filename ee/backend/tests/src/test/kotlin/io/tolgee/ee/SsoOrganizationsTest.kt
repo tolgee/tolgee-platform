32186f2ac1dee20f0ec6582d956020f65f61b596
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2025-03-04 17:46:10 +0100

    chore: native signup tests

--- ee/backend/tests/src/test/kotlin/io/tolgee/ee/SsoOrganizationsTest.kt
+++ ee/backend/tests/src/test/kotlin/io/tolgee/ee/SsoOrganizationsTest.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.tolgee.constants.Feature
 import io.tolgee.constants.Message
 import io.tolgee.development.testDataBuilder.data.SsoTestData
+import io.tolgee.dtos.request.auth.SignUpDto
 import io.tolgee.dtos.request.organization.OrganizationDto
 import io.tolgee.ee.component.PublicEnabledFeaturesProvider
 import io.tolgee.ee.data.OAuth2TokenResponse
@@ -11,6 +12,7 @@ import io.tolgee.ee.security.thirdParty.SsoDelegateEe
 import io.tolgee.ee.utils.SsoMultiTenantsMocks
 import io.tolgee.exceptions.NotFoundException
 import io.tolgee.fixtures.andIsForbidden
+import io.tolgee.fixtures.andIsUnauthorized
 import io.tolgee.model.enums.OrganizationRoleType
 import io.tolgee.service.TenantService
 import io.tolgee.testing.AuthorizedControllerTest
@@ -133,6 +135,18 @@ class SsoOrganizationsTest : AuthorizedControllerTest() {
     assertThat(response.response.contentAsString).contains(Message.SSO_TOKEN_EXCHANGE_FAILED.code)
     val userName = SsoMultiTenantsMocks.jwtClaimsSet.get("email") as String
     assertThrows<NotFoundException> { userAccountService.get(userName) }
+  }
+
+  @Test
+  fun `doesn't allow sign up when enabled for domain`() {
+    val dto =
+      SignUpDto(
+        name = "Pavel Novak",
+        password = "aaaaaaaaa",
+        email = "aaaa@domain.com",
+        organizationName = "Jejda",
+      )
+    performPost("/api/public/sign_up", dto).andIsUnauthorized
   }
 
   @Transactional
