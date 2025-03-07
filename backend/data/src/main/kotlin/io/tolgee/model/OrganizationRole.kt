34597447763124fcd36e6bdcae37753bccc18a81
Jan Cizmar <cizmar@chlupac.com>
2025-03-07 12:03:09 +0100

    feat: Count seats and store usage

--- backend/data/src/main/kotlin/io/tolgee/model/OrganizationRole.kt
+++ backend/data/src/main/kotlin/io/tolgee/model/OrganizationRole.kt
@@ -1,13 +1,7 @@
 package io.tolgee.model
 
 import io.tolgee.model.enums.OrganizationRoleType
-import jakarta.persistence.Entity
-import jakarta.persistence.EnumType
-import jakarta.persistence.Enumerated
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.OneToOne
-import jakarta.persistence.Table
-import jakarta.persistence.UniqueConstraint
+import jakarta.persistence.*
 import jakarta.validation.constraints.NotNull
 import org.hibernate.annotations.ColumnDefault
 
