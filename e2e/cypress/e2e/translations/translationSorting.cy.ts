89ee80cbd952d5b1012996325c3676464b307210
Štěpán Granát <granat.stepan@gmail.com>
2025-03-10 16:29:30 +0100

    chore: improve e2e reliability

--- e2e/cypress/e2e/translations/translationSorting.cy.ts
+++ e2e/cypress/e2e/translations/translationSorting.cy.ts
@@ -1,5 +1,6 @@
 import { ProjectDTO } from '../../../../webapp/src/service/response.types';
 import { createKey } from '../../common/apiCalls/common';
+import { waitForGlobalLoading } from '../../common/loading';
 import {
   create4Translations,
   translationsBeforeEach,
@@ -18,22 +19,24 @@ describe('Translations sorting', () => {
       .then(() => visitTranslations(project.id));
   });
 
-  it('sort by key name', () => {
+  it('sort by key name a to z', () => {
     cy.gcy('translation-controls-order').click();
     cy.waitForDom();
     cy.gcy('translation-controls-order-item')
       .contains('Key name A to Z')
       .click();
+    waitForGlobalLoading();
     cy.gcy('translations-key-name').eq(0).should('contain', 'aaa.last.created');
     cy.gcy('translations-key-name').eq(1).should('contain', 'Cool key 01');
   });
 
-  it('sort by key name', () => {
+  it('sort by key name z to a', () => {
     cy.gcy('translation-controls-order').click();
     cy.waitForDom();
     cy.gcy('translation-controls-order-item')
       .contains('Key name Z to A')
       .click();
+
     cy.gcy('translations-key-name')
       .eq(0)
       .should('contain', 'zzz.first.created');
@@ -46,6 +49,7 @@ describe('Translations sorting', () => {
     cy.gcy('translation-controls-order-item')
       .contains('First key added')
       .click();
+    waitForGlobalLoading();
     cy.gcy('translations-key-name')
       .eq(0)
       .should('contain', 'zzz.first.created');
@@ -58,6 +62,7 @@ describe('Translations sorting', () => {
     cy.gcy('translation-controls-order-item')
       .contains('Last key added')
       .click();
+    waitForGlobalLoading();
     cy.gcy('translations-key-name').eq(0).should('contain', 'aaa.last.created');
     cy.gcy('translations-key-name').eq(1).should('contain', 'Cool key 04');
   });
