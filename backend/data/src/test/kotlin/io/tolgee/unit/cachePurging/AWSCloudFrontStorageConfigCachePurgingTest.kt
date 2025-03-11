095eeb4d5ab2d282007ef5f02e12386181cd177d
David King <david.king@globalsign.com>
2025-03-11 22:16:43 +0000

    Updated tests to catch exception

--- backend/data/src/test/kotlin/io/tolgee/unit/cachePurging/AWSCloudFrontStorageConfigCachePurgingTest.kt
+++ backend/data/src/test/kotlin/io/tolgee/unit/cachePurging/AWSCloudFrontStorageConfigCachePurgingTest.kt
@@ -1,10 +1,13 @@
 package io.tolgee.unit.cachePurging
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.services.cloudfront.model.CloudFrontException
 import io.tolgee.component.contentDelivery.cachePurging.awsCloudFront.AWSCloudFrontContentDeliveryCachePurging
 import io.tolgee.component.contentDelivery.cachePurging.awsCloudFront.AWSCredentialProvider
 import io.tolgee.model.contentDelivery.AWSCloudFrontConfig
 import io.tolgee.model.contentDelivery.ContentDeliveryConfig
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.Test
+import io.tolgee.testing.assert
 import org.mockito.Mockito
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
@@ -34,6 +37,12 @@ class AWSCloudflareContentStorageConfigCachePurgingTest() {
     val contentDeliveryConfig = mock<ContentDeliveryConfig>()
     whenever(contentDeliveryConfig.slug).thenReturn("fake-slug")
 
-    purging.purgeForPaths(contentDeliveryConfig, setOf("fake-path"))
+    val exception = assertThrows<CloudFrontException> {
+            purging.purgeForPaths(contentDeliveryConfig, setOf("fake-path"))
+    }
+    
+    exception.message?.startsWith("Missing Authentication Token (Service: CloudFront, Status Code: 403, Request ID:").assert.isTrue
+
+    
   }
 }
