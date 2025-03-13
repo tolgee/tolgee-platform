050c082b0329eeac6e103568c677b854f659ee50
Štěpán Granát <granat.stepan@gmail.com>
2025-03-13 18:55:29 +0100

    fix: sorting labels

--- webapp/src/component/ListComponents.tsx
+++ webapp/src/component/ListComponents.tsx
@@ -9,6 +9,7 @@ const StyledListSubheader = styled(ListSubheader)`
   line-height: unset;
   padding-top: ${({ theme }) => theme.spacing(2)};
   padding-bottom: ${({ theme }) => theme.spacing(0.5)};
+  background: transparent;
 `;
 
 export const CompactMenuItem: React.FC<React.ComponentProps<typeof MenuItem>> =
