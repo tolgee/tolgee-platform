ea945dfa382587e8400657ff3d19dc82a4c7326c
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2025-03-27 16:17:23 +0100

    feat: implemented glossary create form

---
+++ webapp/src/component/searchSelect/MultiselectItem.tsx
@@ -0,0 +1,51 @@
+import { Checkbox, ListItemText, MenuItemProps, styled } from '@mui/material';
+import React from 'react';
+import { CompactMenuItem } from 'tg.component/ListComponents';
+
+const StyledMenuItem = styled(CompactMenuItem)`
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  align-items: center;
+  padding-left: 4px !important;
+
+  & .hidden {
+    opacity: 0;
+    transition: opacity ease-in 0.1s;
+  }
+
+  &:hover .hidden {
+    opacity: 1;
+  }
+
+  gap: 8px;
+`;
+
+const StyledListItemText = styled(ListItemText)`
+  overflow: hidden;
+`;
+
+const StyledCheckbox = styled(Checkbox)`
+  margin: -8px -8px -8px 0px;
+`;
+
+type Props = MenuItemProps & {
+  label: React.ReactNode;
+  selected: boolean;
+};
+
+export const MultiselectItem = React.forwardRef(function MultiselectItem(
+  { label, selected, ...other }: Props,
+  ref
+) {
+  return (
+    <StyledMenuItem
+      ref={ref as any}
+      data-cy="filter-item"
+      selected={selected}
+      {...other}
+    >
+      <StyledCheckbox checked={Boolean(selected)} size="small" />
+      <StyledListItemText primary={label} />
+    </StyledMenuItem>
+  );
+});
