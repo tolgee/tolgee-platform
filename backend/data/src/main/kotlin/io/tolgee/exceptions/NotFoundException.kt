77a821bb99a344f422db5d89651e8ca37fc3721b
Jan Cizmar <cizmar@chlupac.com>
2025-04-10 17:27:15 +0200

    fix: Store usage when project is deleted

--- backend/data/src/main/kotlin/io/tolgee/exceptions/NotFoundException.kt
+++ backend/data/src/main/kotlin/io/tolgee/exceptions/NotFoundException.kt
@@ -5,4 +5,5 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-data class NotFoundException(val msg: Message = Message.RESOURCE_NOT_FOUND) : RuntimeException(), ExpectedException
+open class NotFoundException(val msg: Message = Message.RESOURCE_NOT_FOUND, val resourceId: Any? = null) :
+  RuntimeException(), ExpectedException
