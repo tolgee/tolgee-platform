e6f321de73db1df3545b4213df48031a03574899
Jan Cizmar <cizmar@chlupac.com>
2025-05-07 14:54:26 +0200

    refactor: remove unused S3 content storage and delivery config test data

--- backend/data/src/main/kotlin/io/tolgee/development/testDataBuilder/data/ContentDeliveryConfigTestData.kt
+++ backend/data/src/main/kotlin/io/tolgee/development/testDataBuilder/data/ContentDeliveryConfigTestData.kt
@@ -29,20 +29,6 @@ class ContentDeliveryConfigTestData : BaseTestData() {
         }
     }
 
-  val s3ContentStorageWithCustomPath =
-    projectBuilder.addContentStorage {
-      name = "S3"
-      this.s3ContentStorageConfig =
-        S3ContentStorageConfig(this).apply {
-          bucketName = "fake"
-          accessKey = "fake"
-          secretKey = "fake"
-          endpoint = "fake"
-          signingRegion = "fake"
-          path = "fake"
-        }
-    }
-
   val defaultServerContentDeliveryConfig =
     projectBuilder.addContentDeliveryConfig {
       name = "Default server"
@@ -63,14 +49,6 @@ class ContentDeliveryConfigTestData : BaseTestData() {
   val s3ContentDeliveryConfigWithCustomSlug =
     projectBuilder.addContentDeliveryConfig {
       contentStorage = s3ContentStorage.self
-      name = "Custom Slug"
-      slug = "my-slug"
-      customSlug = true
-    }
-
-  val s3ContentDeliveryConfigWithCustomSlugAndPath =
-    projectBuilder.addContentDeliveryConfig {
-      contentStorage = s3ContentStorageWithCustomPath.self
       name = "Custom Slug"
       slug = "my-slug"
       customSlug = true
