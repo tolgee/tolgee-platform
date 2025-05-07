a4c265b070a0dae637f2544d6d2be7ca80ac0268
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2025-05-07 13:09:46 +0200

    fix: replace usages of checkFeatureEnabled in controllers with annotations

--- ee/backend/app/src/main/kotlin/io/tolgee/ee/api/v2/controllers/glossary/GlossaryTermTranslationController.kt
+++ ee/backend/app/src/main/kotlin/io/tolgee/ee/api/v2/controllers/glossary/GlossaryTermTranslationController.kt
@@ -2,7 +2,6 @@ package io.tolgee.ee.api.v2.controllers.glossary
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import io.tolgee.component.enabledFeaturesProvider.EnabledFeaturesProvider
 import io.tolgee.constants.Feature
 import io.tolgee.ee.api.v2.hateoas.assemblers.glossary.GlossaryTermTranslationModelAssembler
 import io.tolgee.ee.api.v2.hateoas.model.glossary.GlossaryTermTranslationModel
@@ -10,15 +9,14 @@ import io.tolgee.ee.data.glossary.UpdateGlossaryTermTranslationRequest
 import io.tolgee.ee.service.glossary.GlossaryTermService
 import io.tolgee.ee.service.glossary.GlossaryTermTranslationService
 import io.tolgee.model.enums.OrganizationRoleType
-import io.tolgee.security.OrganizationHolder
 import io.tolgee.security.authentication.AllowApiAccess
 import io.tolgee.security.authentication.AuthTokenType
+import io.tolgee.security.authorization.RequiresFeatures
 import io.tolgee.security.authorization.RequiresOrganizationRole
 import io.tolgee.security.authorization.UseDefaultPermissions
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.*
 
-@Suppress("SpringJavaInjectionPointsAutowiringInspection")
 @RestController
 @RequestMapping(
   "/v2/organizations/{organizationId:[0-9]+}/glossaries/{glossaryId:[0-9]+}/terms/{termId:[0-9]+}/translations",
@@ -28,13 +26,12 @@ class GlossaryTermTranslationController(
   private val glossaryTermService: GlossaryTermService,
   private val glossaryTermTranslationService: GlossaryTermTranslationService,
   private val modelAssembler: GlossaryTermTranslationModelAssembler,
-  private val organizationHolder: OrganizationHolder,
-  private val enabledFeaturesProvider: EnabledFeaturesProvider,
 ) {
   @PostMapping()
   @Operation(summary = "Set a new glossary term translation for language")
   @AllowApiAccess(AuthTokenType.ONLY_PAT)
   @RequiresOrganizationRole(OrganizationRoleType.MAINTAINER)
+  @RequiresFeatures(Feature.GLOSSARY)
   @Transactional
   fun update(
     @PathVariable
@@ -46,11 +43,6 @@ class GlossaryTermTranslationController(
     @RequestBody
     dto: UpdateGlossaryTermTranslationRequest,
   ): GlossaryTermTranslationModel {
-    enabledFeaturesProvider.checkFeatureEnabled(
-      organizationHolder.organization.id,
-      Feature.GLOSSARY,
-    )
-
     val glossaryTerm = glossaryTermService.get(organizationId, glossaryId, termId)
     val translation = glossaryTermTranslationService.updateOrCreate(glossaryTerm, dto)
     return translation?.let { modelAssembler.toModel(translation) } ?: GlossaryTermTranslationModel.defaultValue(
@@ -62,6 +54,7 @@ class GlossaryTermTranslationController(
   @Operation(summary = "Get glossary term translation for language")
   @AllowApiAccess(AuthTokenType.ONLY_PAT)
   @UseDefaultPermissions
+  @RequiresFeatures(Feature.GLOSSARY)
   fun get(
     @PathVariable
     organizationId: Long,
@@ -72,11 +65,6 @@ class GlossaryTermTranslationController(
     @PathVariable
     languageTag: String,
   ): GlossaryTermTranslationModel {
-    enabledFeaturesProvider.checkFeatureEnabled(
-      organizationHolder.organization.id,
-      Feature.GLOSSARY,
-    )
-
     val glossaryTerm = glossaryTermService.get(organizationId, glossaryId, termId)
     val translation = glossaryTermTranslationService.find(glossaryTerm, languageTag)
     return translation?.let { modelAssembler.toModel(translation) } ?: GlossaryTermTranslationModel.defaultValue(
