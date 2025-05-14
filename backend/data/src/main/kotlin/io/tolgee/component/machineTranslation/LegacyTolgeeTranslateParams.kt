cb9d2dc718d41fa1cf6266c306e0ed02dcb0cfb6
Štěpán Granát <granat.stepan@gmail.com>
2025-05-14 18:02:35 +0200

    feat: legacy translate endpoint

---
+++ backend/data/src/main/kotlin/io/tolgee/component/machineTranslation/LegacyTolgeeTranslateParams.kt
@@ -0,0 +1,30 @@
+package io.tolgee.component.machineTranslation
+
+import io.tolgee.model.mtServiceConfig.Formality
+
+data class LegacyTolgeeTranslateParams(
+  val text: String,
+  val keyName: String?,
+  val sourceTag: String,
+  val targetTag: String,
+  val metadata: Metadata?,
+  val formality: Formality?,
+  val isBatch: Boolean,
+  val pluralForms: Map<String, String>? = null,
+  val pluralFormExamples: Map<String, String>? = null,
+) {
+  data class Metadata(
+    val examples: List<ExampleItem> = emptyList(),
+    val closeItems: List<ExampleItem> = emptyList(),
+    val keyDescription: String?,
+    val projectDescription: String?,
+    val languageDescription: String?,
+  )
+
+  data class ExampleItem(
+    val source: String,
+    val target: String,
+    val key: String,
+    val keyNamespace: String?,
+  )
+}
