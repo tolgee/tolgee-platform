0a227fe76d4f321de52ca078e27543d327eb3581
Štěpán Granát <granat.stepan@gmail.com>
2025-05-21 16:11:32 +0200

    chore: fix some e2e tests

--- e2e/cypress/e2e/projects/permissions/permissionsServerAdmin.2.cy.ts
+++ e2e/cypress/e2e/projects/permissions/permissionsServerAdmin.2.cy.ts
@@ -23,6 +23,7 @@ describe('Server admin 2', () => {
           'project-menu-item-export': SKIP,
           'project-menu-item-developer': SKIP,
           'project-menu-item-integrate': SKIP,
+          'project-menu-item-ai': SKIP,
         });
       }
     );
