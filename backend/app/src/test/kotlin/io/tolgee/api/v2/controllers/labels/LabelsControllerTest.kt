05df55684e9926339e72b8f37b6ecf1b9f3b727b
dkrizan <krizan@survio.cz>
2025-05-26 09:29:20 +0200

    chore: Ktlint check fix

--- backend/app/src/test/kotlin/io/tolgee/api/v2/controllers/labels/LabelsControllerTest.kt
+++ backend/app/src/test/kotlin/io/tolgee/api/v2/controllers/labels/LabelsControllerTest.kt
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.data.domain.Pageable
 
-
 class LabelsControllerTest : ProjectAuthControllerTest("/v2/projects/") {
 
   lateinit var testData: LabelsTestData
