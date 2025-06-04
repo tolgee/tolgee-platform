bd788d10acda69afed56066f17da466ef793e4f3
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2025-06-04 15:45:01 +0200

    fix: correct controller for OrganizationLanguageModelAssembler

--- backend/api/src/main/kotlin/io/tolgee/hateoas/language/OrganizationLanguageModelAssembler.kt
+++ backend/api/src/main/kotlin/io/tolgee/hateoas/language/OrganizationLanguageModelAssembler.kt
@@ -1,6 +1,6 @@
 package io.tolgee.hateoas.language
 
-import io.tolgee.api.v2.controllers.organization.OrganizationProjectController
+import io.tolgee.api.v2.controllers.organization.OrganizationLanguageController
 import io.tolgee.dtos.cacheable.OrganizationLanguageDto
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport
 import org.springframework.stereotype.Component
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component
 @Component
 class OrganizationLanguageModelAssembler :
   RepresentationModelAssemblerSupport<OrganizationLanguageDto, OrganizationLanguageModel>(
-    OrganizationProjectController::class.java,
+    OrganizationLanguageController::class.java,
     OrganizationLanguageModel::class.java,
   ) {
   override fun toModel(languageDto: OrganizationLanguageDto): OrganizationLanguageModel {
