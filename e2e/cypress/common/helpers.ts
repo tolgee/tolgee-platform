2d1c3d48b95d07265631a08d278fbac00a938740
dkrizan <krizan@survio.cz>
2025-06-12 16:15:20 +0200

    activity log test

--- e2e/cypress/common/helpers.ts
+++ e2e/cypress/common/helpers.ts
@@ -1,15 +1,3 @@
-export const rgbToHex = (rgb: string): string => {
-  const result = rgb.match(/\d+/g);
-  if (!result) return '';
-  return (
-    '#' +
-    result
-      .slice(0, 3)
-      .map((x) => (+x).toString(16).padStart(2, '0').toUpperCase())
-      .join('')
-  );
-};
-
 export const isDarkMode = window.matchMedia(
   '(prefers-color-scheme: dark)'
 ).matches;
---
+++ e2e/cypress/common/label.ts
@@ -0,0 +1,55 @@
+import { getTranslationCell } from './translations';
+import { gcy } from './shared';
+import { E2ProjectLabelsSection } from '../compounds/projectSettings/labels/E2ProjectLabelsSection';
+
+export function assignLabelToTranslation(
+  key: string,
+  languageTag: string,
+  label: string,
+  expectLabelCount?: number | undefined
+) {
+  getTranslationCell(key, languageTag).within(() => {
+    gcy('translation-label-control')
+      .should('not.be.visible')
+      .click()
+      .should('be.visible');
+    gcy('autocomplete-label-input').should('be.visible').click();
+    gcy('label-selector-autocomplete').should('be.visible');
+  });
+  if (expectLabelCount !== undefined) {
+    gcy('label-autocomplete-option').should('have.length', expectLabelCount);
+  }
+  gcy('label-autocomplete-option').contains(label).should('be.visible').click();
+}
+
+export const verifyLabelInTranslationCell = (
+  key: string,
+  lang: string,
+  label: string,
+  expectedColor: string
+) => {
+  getTranslationCell(key, lang).within(() => {
+    gcy('translation-label')
+      .contains(label)
+      .parent('[data-cy="translation-label"]')
+      .should('be.visible')
+      .should('have.css', 'background-color', expectedColor);
+  });
+};
+
+export const verifyLabelsCountInTranslationCell = (
+  key: string,
+  lang: string,
+  expectedCount: number
+) => {
+  getTranslationCell(key, lang).within(() => {
+    gcy('translation-label').should('have.length', expectedCount);
+  });
+};
+
+export const createLabel = (labelName: string, color?: string) => {
+  const projectLabels = new E2ProjectLabelsSection();
+  projectLabels.visitFromProjectSettings();
+  const labelModal = projectLabels.openCreateLabelModal();
+  labelModal.fillAndSave(labelName, color);
+};
