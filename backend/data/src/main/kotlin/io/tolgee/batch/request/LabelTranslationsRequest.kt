e484a7d9f674000eb41fa420eab2093d49fe5a32
dkrizan <krizan@survio.cz>
2025-06-12 11:18:47 +0200

    ktlintCheck

--- backend/data/src/main/kotlin/io/tolgee/batch/request/LabelTranslationsRequest.kt
+++ backend/data/src/main/kotlin/io/tolgee/batch/request/LabelTranslationsRequest.kt
@@ -6,8 +6,10 @@ import jakarta.validation.constraints.Size
 class LabelTranslationsRequest {
   @NotEmpty
   var keyIds: List<Long> = listOf()
+
   @Size(min = 1)
   var languageIds: List<Long> = listOf()
+
   @NotEmpty
   var labelIds: List<Long> = listOf()
 }
