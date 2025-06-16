6e674433498a72b8106bd6ae0103fef839cbce6e
Štěpán Granát <granat.stepan@gmail.com>
2025-06-16 11:11:47 +0200

    chore: fix ktlint

--- backend/data/src/main/kotlin/io/tolgee/exceptions/LlmRateLimitedException.kt
+++ backend/data/src/main/kotlin/io/tolgee/exceptions/LlmRateLimitedException.kt
@@ -3,7 +3,11 @@ package io.tolgee.exceptions
 import io.tolgee.constants.Message
 import java.io.Serializable
 
-class LlmRateLimitedException(val retryAt: Long? = null, params: List<Serializable?>? = null, cause: Exception? = null) : FailedDependencyException(
+class LlmRateLimitedException(
+  val retryAt: Long? = null,
+  params: List<Serializable?>? = null,
+  cause: Exception? = null
+) : FailedDependencyException(
   Message.LLM_RATE_LIMITED,
   params,
   cause,
