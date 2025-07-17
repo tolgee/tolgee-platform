ea1ccfeb3893abddb6db3b07df0f8e3ab9c61f22
Daniel Krizan <danyelkrizan@gmail.com>
2025-07-17 13:06:18 +0200

    feat: cloud plans migration controller + UI

---
+++ webapp/src/ee/billing/administration/subscriptionPlans/migration/AdministrationPlanMigrationEdit.tsx
@@ -0,0 +1,93 @@
+import { Box, Typography } from '@mui/material';
+import { T, useTranslate } from '@tolgee/react';
+
+import { DashboardPage } from 'tg.component/layout/DashboardPage';
+import { BaseAdministrationView } from 'tg.views/administration/components/BaseAdministrationView';
+import { LINKS, PARAMS } from 'tg.constants/links';
+import {
+  PlanMigrationForm,
+  PlanMigrationFormData,
+} from 'tg.ee.module/billing/administration/subscriptionPlans/components/migrationForm/PlanMigrationForm';
+import {
+  useBillingApiMutation,
+  useBillingApiQuery,
+} from 'tg.service/http/useQueryApi';
+import React from 'react';
+import { useMessage } from 'tg.hooks/useSuccessMessage';
+import { useHistory, useRouteMatch } from 'react-router-dom';
+import { SpinnerProgress } from 'tg.component/SpinnerProgress';
+
+export const AdministrationPlanMigrationEdit = () => {
+  const { t } = useTranslate();
+  const match = useRouteMatch();
+  const messaging = useMessage();
+  const history = useHistory();
+  const migrationId = match.params[PARAMS.PLAN_MIGRATION_ID] as number;
+
+  const migrationLoadable = useBillingApiQuery({
+    url: '/v2/administration/billing/cloud-plans/migration/{migrationId}',
+    method: 'get',
+    path: { migrationId },
+  });
+
+  const updatePlanMigrationLoadable = useBillingApiMutation({
+    url: '/v2/administration/billing/cloud-plans/migration/{migrationId}',
+    method: 'put',
+  });
+
+  if (migrationLoadable.isLoading) {
+    return <SpinnerProgress />;
+  }
+
+  const migration = migrationLoadable.data!;
+
+  const submit = (values: PlanMigrationFormData) => {
+    updatePlanMigrationLoadable.mutate(
+      {
+        path: { migrationId },
+        content: { 'application/json': values },
+      },
+      {
+        onSuccess: () => {
+          messaging.success(
+            <T keyName="administration_cloud_plan_migration_updated_success" />
+          );
+          history.push(LINKS.ADMINISTRATION_BILLING_CLOUD_PLANS.build());
+        },
+      }
+    );
+  };
+
+  return (
+    <DashboardPage>
+      <BaseAdministrationView
+        windowTitle={t('administration_plan_migration_configure_existing')}
+        allCentered
+        hideChildrenOnLoading={false}
+        navigation={[
+          [
+            t('administration_cloud_plans'),
+            LINKS.ADMINISTRATION_BILLING_CLOUD_PLANS.build(),
+          ],
+          [
+            t('administration_plan_migration_configure_existing'),
+            LINKS.ADMINISTRATION_BILLING_PLAN_MIGRATION_EDIT.build({
+              [PARAMS.PLAN_MIGRATION_ID]: migrationId,
+            }),
+          ],
+        ]}
+      >
+        <Box>
+          <Typography variant="h5">
+            {t('administration_plan_migration_configure_existing')}
+          </Typography>
+        </Box>
+        <PlanMigrationForm
+          migration={migration}
+          onSubmit={submit}
+          loading={updatePlanMigrationLoadable.isLoading}
+        />
+      </BaseAdministrationView>
+    </DashboardPage>
+  );
+};
