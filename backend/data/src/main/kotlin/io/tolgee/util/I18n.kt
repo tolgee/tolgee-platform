0c570b1a4b41050f365feb226d44be5115198d3f
Daniel Krizan <danyelkrizan@gmail.com>
2025-07-30 12:41:15 +0200

    chore: use i18n to translate checkout VAT notice

--- backend/data/src/main/kotlin/io/tolgee/util/I18n.kt
+++ backend/data/src/main/kotlin/io/tolgee/util/I18n.kt
@@ -1,29 +1,66 @@
 package io.tolgee.util
 
+import io.tolgee.constants.SupportedLocale
 import org.springframework.stereotype.Component
 import java.text.MessageFormat
 import java.util.*
 
 @Component
 class I18n : Logging {
-  private val bundle by lazy { ResourceBundle.getBundle("I18n", Locale.ENGLISH) }
+  private val bundles = mutableMapOf<SupportedLocale, ResourceBundle>()
+  private val defaultLocale: SupportedLocale = SupportedLocale.DEFAULT
+
+  internal fun getBundle(locale: SupportedLocale): ResourceBundle {
+    return bundles.getOrPut(locale) {
+      ResourceBundle.getBundle(
+        "I18n",
+        Locale.forLanguageTag(locale.code),
+      )
+    }
+  }
+
+  private fun getBundleString(key: String, locale: SupportedLocale): String {
+    fun tryGetBundleString(locale: SupportedLocale): String? {
+      return try {
+        getBundle(locale).getString(key)
+      } catch (_: MissingResourceException) {
+        null
+      } catch (_: Exception) {
+        null
+      }
+    }
+
+    tryGetBundleString(locale)?.let { return it }
+
+    if (locale != defaultLocale) {
+      tryGetBundleString(defaultLocale)?.let { return it }
+    }
+
+    logMissing(key, locale.code)
+    return key
+  }
+
+  private fun logMissing(key: String, locale: String) {
+    if (locale != defaultLocale.code) {
+      logger.warn(
+        "Key '$key' was not found in the resource bundle for locales $locale and ${defaultLocale.code} (default)"
+      )
+    } else {
+      logger.warn("Key '$key' was not found in the resource bundle for locale $locale")
+    }
+  }
 
   fun translate(
     key: String,
     vararg parameters: String,
+    locale: SupportedLocale = defaultLocale
   ): String {
-    val bundleString =
-      try {
-        bundle.getString(key)
-      } catch (e: MissingResourceException) {
-        logger.warn("Key '$key' was not found in the resource bundle", e)
-        key
-      }
+    val bundleString = getBundleString(key, locale)
 
     return if (parameters.isEmpty()) {
       bundleString
     } else {
-      MessageFormat(bundleString, Locale.ENGLISH).format(parameters)
+      MessageFormat(bundleString, Locale.forLanguageTag(locale.code)).format(parameters)
     }
   }
 }
