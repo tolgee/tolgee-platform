4f8800bbd505a7e59bf08e6ff61160a05ccf17db
Štěpán Granát <granat.stepan@gmail.com>
2025-08-13 15:58:13 +0200

    fix: correct package.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.ts"),
-      name: "@tolgee/client",
+      name: "client",
       // the proper extensions will be added
       fileName: "client",
     },
