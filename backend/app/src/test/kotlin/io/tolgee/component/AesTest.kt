389e758577ea10270a96d992a5f183033aba0adc
dmitrii.bocharov <bdshadow@gmail.com>
2025-08-19 00:07:28 +0200

    cleanup: AesTest can be just unit test, not SpringBootTest
    Signed-off-by: dmitrii.bocharov <bdshadow@gmail.com>

--- backend/app/src/test/kotlin/io/tolgee/component/AesTest.kt
+++ backend/app/src/test/kotlin/io/tolgee/component/AesTest.kt
@@ -4,24 +4,14 @@
 
 package io.tolgee.component
 
-import io.tolgee.testing.ContextRecreatingTest
 import io.tolgee.testing.assertions.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
 
-@ContextRecreatingTest
-@SpringBootTest(
-  properties = [
-    "tolgee.authentication.jwtSecret=this_is_dummy_jwt_secret_azeazezaezaezaezaezzaezaezaeazeazezaeazezeaeazea",
-  ],
-)
 class AesTest {
-  @set:Autowired
-  lateinit var aes: Aes
 
   @Test
   fun `it works`() {
+    val aes = Aes("this_is_dummy_jwt_secret_azeazezaezaezaezaezzaezaezaeazeazezaeazezeaeazea".toByteArray())
     val encrypted1 = aes.encrypt("hello".toByteArray(charset("UTF-8")))
     val encrypted2 = aes.encrypt("hello hello".toByteArray(charset("UTF-8")))
     val decrypted2 = aes.decrypt(encrypted2).toString(charset("UTF-8"))
