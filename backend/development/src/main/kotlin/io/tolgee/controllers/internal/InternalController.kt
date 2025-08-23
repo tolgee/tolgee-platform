db9e63db8a1c94c19059154d4b4d0d2e58ea732b
dmitrii.bocharov <bdshadow@gmail.com>
2025-08-23 06:40:13 +0200

    cleanup: do NOT create internal Controllers when not intentionally allowed
    Signed-off-by: dmitrii.bocharov <bdshadow@gmail.com>

---
+++ backend/development/src/main/kotlin/io/tolgee/controllers/internal/InternalController.kt
@@ -0,0 +1,24 @@
+package io.tolgee.controllers.internal
+
+import io.swagger.v3.oas.annotations.Hidden
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.core.annotation.AliasFor
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@CrossOrigin(origins = ["*"])
+@Hidden
+@RequestMapping
+@Transactional
+@ConditionalOnProperty(
+    value = ["tolgee.internal.controller-enabled"],
+    havingValue = "true",
+    matchIfMissing = false
+)
+annotation class InternalController(
+    @get:AliasFor(annotation = RequestMapping::class)
+    val value: Array<String> = []
+)
--- backend/development/src/main/kotlin/io/tolgee/controllers/internal/e2eData/TranslationSingleE2eDataController.kt
+++ backend/development/src/main/kotlin/io/tolgee/controllers/internal/e2eData/TranslationSingleE2eDataController.kt
@@ -1,21 +1,14 @@
 package io.tolgee.controllers.internal.e2eData
 
-import io.swagger.v3.oas.annotations.Hidden
+import io.tolgee.controllers.internal.InternalController
 import io.tolgee.development.testDataBuilder.TestDataService
 import io.tolgee.development.testDataBuilder.data.TranslationSingleTestData
 import io.tolgee.service.project.ProjectService
 import io.tolgee.service.security.UserAccountService
 import org.springframework.transaction.annotation.Transactional
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
-@CrossOrigin(origins = ["*"])
-@Hidden
-@RequestMapping(value = ["internal/e2e-data/translation-single"])
-@Transactional
+@InternalController(["internal/e2e-data/translation-single"])
 class TranslationSingleE2eDataController(
   private val testDataService: TestDataService,
   private val projectService: ProjectService,
