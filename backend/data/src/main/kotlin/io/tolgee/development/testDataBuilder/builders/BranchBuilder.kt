3a695fc8622000202a85c0f4149d112c2ebe250e
Daniel Krizan <danyelkrizan@gmail.com>
2025-09-17 15:16:20 +0200

    feat: Branch entity and controller

---
+++ backend/data/src/main/kotlin/io/tolgee/development/testDataBuilder/builders/BranchBuilder.kt
@@ -0,0 +1,10 @@
+package io.tolgee.development.testDataBuilder.builders
+
+import io.tolgee.model.branching.Branch
+
+class BranchBuilder(
+  val projectBuilder: ProjectBuilder,
+) : BaseEntityDataBuilder<Branch, BranchBuilder>() {
+  override val self: Branch =
+    Branch().apply { this.project = projectBuilder.self }
+}
