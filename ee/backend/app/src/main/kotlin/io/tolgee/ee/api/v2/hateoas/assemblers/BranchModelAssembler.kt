0f8edaecd57559f5c1a57d2aca2ddc3c8bb27869
Daniel Krizan <danyelkrizan@gmail.com>
2025-09-17 15:16:20 +0200

    feat: Branch entity and controller

---
+++ ee/backend/app/src/main/kotlin/io/tolgee/ee/api/v2/hateoas/assemblers/BranchModelAssembler.kt
@@ -0,0 +1,17 @@
+package io.tolgee.ee.api.v2.hateoas.assemblers
+
+import io.tolgee.ee.api.v2.hateoas.model.branching.BranchModel
+import io.tolgee.model.branching.Branch
+import org.springframework.hateoas.server.RepresentationModelAssembler
+import org.springframework.stereotype.Component
+
+@Component
+class BranchModelAssembler : RepresentationModelAssembler<Branch, BranchModel> {
+  override fun toModel(entity: Branch): BranchModel {
+    return BranchModel(
+      id = entity.id,
+      name = entity.name,
+      active = entity.archivedAt == null,
+    )
+  }
+}
