d5cf14ebd5e965fa5222a0a7ea2d8e9d98e88b95
Daniel Krizan <danyelkrizan@gmail.com>
2025-09-23 16:25:54 +0200

    chore: Branch copy test improved - checks data consistency

--- ee/backend/tests/src/test/kotlin/io/tolgee/ee/api/v2/controllers/branching/BranchCopyIntegrationTest.kt
+++ ee/backend/tests/src/test/kotlin/io/tolgee/ee/api/v2/controllers/branching/BranchCopyIntegrationTest.kt
@@ -5,6 +5,7 @@ import io.tolgee.development.testDataBuilder.data.BranchTranslationsTestData
 import io.tolgee.ee.repository.BranchRepository
 import io.tolgee.fixtures.andAssertThatJson
 import io.tolgee.fixtures.andIsOk
+import io.tolgee.model.key.Key
 import io.tolgee.testing.annotations.ProjectJWTAuthTestMethod
 import io.tolgee.testing.assert
 import org.junit.jupiter.api.BeforeEach
@@ -45,6 +46,12 @@ class BranchCopyIntegrationTest : ProjectAuthControllerTest("/v2/projects/") {
 
     newBranchKeyCount.assert.isEqualTo(500)
 
+    val firstMainKey = keyRepository.findPrefetchedByNameAndBranch(projectId, "branched key 1", "main")
+    val firstBranchKey = keyRepository.findPrefetchedByNameAndBranch(projectId, "branched key 1", "feature-x")
+
+    firstBranchKey!!.branch!!.id.assert.isEqualTo(newBranchId)
+    firstBranchKey.assertIsCopyOf(firstMainKey!!)
+
     // branch should be ready
     val branch = branchRepository.findByIdOrNull(newBranchId)!!
     branch.pending.assert.isFalse()
@@ -71,5 +78,37 @@ class BranchCopyIntegrationTest : ProjectAuthControllerTest("/v2/projects/") {
         "originBranchId" to testData.mainBranch.id,
       )
     )
+  }
+
+  private fun Key.assertIsCopyOf(other: Key) {
+    val mainTranslations = this.translations
+    val branchTranslations = other.translations
+    val mainTranslation = mainTranslations.find { t -> t.language.tag == "en" }!!
+    val branchTranslation = branchTranslations.find { t -> t.language.tag == "en" }!!
+    val mainTranslationLabels = mainTranslation.labels
+    val branchTranslationLabels = branchTranslation.labels
+
+    // only key.id is different
+    this.id.assert.isNotEqualTo(other.id)
+    this.namespace.assert.isEqualTo(other.namespace)
+    this.name.assert.isEqualTo(other.name)
+    this.isPlural.assert.isEqualTo(other.isPlural)
+    this.pluralArgName.assert.isEqualTo(other.pluralArgName)
+
+    // translations are the same
+    mainTranslations.assert.hasSize(branchTranslations.size)
+    mainTranslation.assert.isNotNull()
+    branchTranslation.assert.isNotNull()
+    mainTranslation.text.assert.isEqualTo(branchTranslation.text)
+    mainTranslation.state.assert.isEqualTo(branchTranslation.state)
+    mainTranslation.auto.assert.isEqualTo(branchTranslation.auto)
+    mainTranslation.mtProvider.assert.isEqualTo(branchTranslation.mtProvider)
+    mainTranslation.outdated.assert.isEqualTo(branchTranslation.outdated)
+    mainTranslation.wordCount.assert.isEqualTo(branchTranslation.wordCount)
+    mainTranslation.characterCount.assert.isEqualTo(branchTranslation.characterCount)
+
+    // labels are the same
+    mainTranslationLabels.assert.hasSize(branchTranslationLabels.size)
+    mainTranslationLabels.assert.containsAll(branchTranslationLabels)
   }
 }
