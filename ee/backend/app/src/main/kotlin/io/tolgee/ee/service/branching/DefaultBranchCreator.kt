8f0b04071a6f2dc256f7735a4559bc0a16b41323
Daniel Krizan <danyelkrizan@gmail.com>
2025-10-07 21:08:18 +0200

    feat: create a default branch on the first load of branches

---
+++ ee/backend/app/src/main/kotlin/io/tolgee/ee/service/branching/DefaultBranchCreator.kt
@@ -0,0 +1,30 @@
+package io.tolgee.ee.service.branching
+
+import io.tolgee.model.Project
+import io.tolgee.model.branching.Branch
+import io.tolgee.service.key.KeyService
+import io.tolgee.service.project.ProjectService
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Service
+
+@Service
+class DefaultBranchCreator(
+  private val keyService: KeyService,
+  private val projectService: ProjectService,
+) {
+
+  @Transactional
+  fun create(projectId: Long): Branch {
+    val project = projectService.get(projectId)
+    return create(project)
+  }
+
+  @Transactional
+  fun create(project: Project): Branch {
+    val branch = Branch.createMainBranch(project)
+    keyService.getAll(project.id).forEach { it ->
+      it.branch = branch
+    }
+    return branch
+  }
+}
