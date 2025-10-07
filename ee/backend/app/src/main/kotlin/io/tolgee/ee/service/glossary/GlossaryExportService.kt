b3456aaefa06dc3bc393d74a7373eb4414fe08b3
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2025-10-07 15:18:37 +0200

    feat: glossary export endoint

---
+++ ee/backend/app/src/main/kotlin/io/tolgee/ee/service/glossary/GlossaryExportService.kt
@@ -0,0 +1,18 @@
+package io.tolgee.ee.service.glossary
+
+import io.tolgee.ee.service.glossary.formats.csv.out.GlossaryCSVExporter
+import io.tolgee.model.glossary.Glossary
+import org.springframework.stereotype.Service
+import java.io.InputStream
+
+@Service
+class GlossaryExportService(
+  private val glossaryTermService: GlossaryTermService,
+  private val glossaryTermTranslationService: GlossaryTermTranslationService,
+) {
+  fun exportCsv(glossary: Glossary, delimiter: Char = ','): InputStream {
+    val terms = glossaryTermService.findAllWithTranslations(glossary)
+    val languageTags = glossaryTermTranslationService.getDistinctLanguageTags(glossary)
+    return GlossaryCSVExporter(glossary, terms, languageTags, delimiter).export()
+  }
+}
---
+++ ee/backend/app/src/main/kotlin/io/tolgee/ee/service/glossary/formats/csv/out/GlossaryCSVExporter.kt
@@ -0,0 +1,48 @@
+package io.tolgee.ee.service.glossary.formats.csv.out
+
+import com.opencsv.CSVWriterBuilder
+import io.tolgee.model.glossary.Glossary
+import io.tolgee.model.glossary.GlossaryTerm
+import java.io.InputStream
+import java.io.StringWriter
+
+class GlossaryCSVExporter(
+    val glossary: Glossary,
+    val terms: List<GlossaryTerm>,
+    val languageTags: Set<String>,
+    val delimiter: Char,
+) {
+    val languageTagsWithoutBaseLanguage by lazy { languageTags - glossary.baseLanguageTag }
+
+    val headers by lazy {
+        arrayOf(
+            "Term",
+            "Description",
+            "Flagged as non-translatable",
+            "Flagged as case-sensitive",
+            "Flagged as abbreviation",
+            "Flagged as forbidden term",
+        ) + languageTagsWithoutBaseLanguage
+    }
+
+    fun GlossaryTerm.asColumns(): Array<String> {
+        return arrayOf(
+            translations.find { it.languageTag == glossary.baseLanguageTag }?.text ?: "",
+            description,
+            flagNonTranslatable.toString(),
+            flagCaseSensitive.toString(),
+            flagAbbreviation.toString(),
+            flagForbiddenTerm.toString(),
+        ) + languageTagsWithoutBaseLanguage.map { languageTag ->
+            translations.find { it.languageTag == languageTag }?.text ?: ""
+        }
+    }
+
+    fun export(): InputStream {
+        val output = StringWriter()
+        val writer = CSVWriterBuilder(output).withSeparator(delimiter).build()
+        writer.writeNext(headers)
+        terms.forEach { writer.writeNext(it.asColumns()) }
+        return output.toString().byteInputStream()
+    }
+}
