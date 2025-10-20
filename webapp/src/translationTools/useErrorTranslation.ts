ed8a3025c6a96fc5d9de7b9d1af0d55e5094f12a
Daniel Krizan <danyelkrizan@gmail.com>
2025-10-20 08:49:04 +0200

    chore: error message for missing plan migration

--- webapp/src/translationTools/useErrorTranslation.ts
+++ webapp/src/translationTools/useErrorTranslation.ts
@@ -111,6 +111,8 @@ export function useErrorTranslation() {
         return t('plan_has_subscribers');
       case 'plan_has_migrations':
         return t('plan_has_migrations');
+      case 'plan_migration_not_found':
+        return t('plan_migration_not_found');
       case 'cannot_store_file_to_content_storage':
         return t('cannot_store_file_to_content_storage');
       case 'unexpected_error_while_publishing_to_content_storage':
