ff5f0a5281aee55a7bfb05c14a3fd7e9ead0d9dc
Jan Molnár <jan.molnar@showmax.com>
2025-10-24 00:02:47 +0200

    fix: search only within list items (i.e. not in menu)

--- e2e/cypress/common/administration.ts
+++ e2e/cypress/common/administration.ts
@@ -1,9 +1,12 @@
 import { HOST } from './constants';
+import { gcy } from './shared';
 
 export const visitAdministration = () => {
   cy.visit(`${HOST}/administration/organizations`);
 };
 
 export function getUserListItem(userName = 'John User') {
-  return cy.contains(userName).closestDcy('administration-users-list-item');
+  return gcy('administration-users-list-item')
+    .contains(userName)
+    .closestDcy('administration-users-list-item');
 }
