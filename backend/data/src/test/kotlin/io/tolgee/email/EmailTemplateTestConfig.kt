ff4f16e1354f30b835445a76c225b43a4f2816f7
Daniel Krizan <danyelkrizan@gmail.com>
2025-10-29 10:58:50 +0100

    chore: coderabbit changes

---
+++ backend/data/src/test/kotlin/io/tolgee/email/EmailTemplateTestConfig.kt
@@ -0,0 +1,24 @@
+package io.tolgee.email
+
+import com.transferwise.icu.ICUReloadableResourceBundleMessageSource
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.MessageSource
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
+import java.util.*
+
+@TestConfiguration
+class EmailTemplateTestConfig {
+  @Bean("emailIcuMessageSource")
+  @Primary
+  fun testMessageSource(): MessageSource {
+    val messageSource = ICUReloadableResourceBundleMessageSource()
+    messageSource.setBasenames(
+      "classpath:email-i18n/messages",
+      "classpath:email-i18n-test/messages",
+    )
+    messageSource.setDefaultEncoding("UTF-8")
+    messageSource.setDefaultLocale(Locale.ENGLISH)
+    return messageSource
+  }
+}
