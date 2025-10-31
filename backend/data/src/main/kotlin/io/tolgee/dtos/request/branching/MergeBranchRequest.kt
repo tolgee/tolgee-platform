382f82fc3ff2113ce87fbc98c7cb2153a1618728
Daniel Krizan <danyelkrizan@gmail.com>
2025-10-31 14:45:19 +0100

    feat: branch conflict resolving endpoint

--- backend/data/src/main/kotlin/io/tolgee/dtos/request/branching/MergeBranchRequest.kt
+++
@@ -1,9 +0,0 @@
-package io.tolgee.dtos.request.branching
-
-import io.swagger.v3.oas.annotations.media.Schema
-
-@Suppress("unused")
-open class MergeBranchRequest(
-  @Schema(description = "Target branch id")
-  val mergeSessionId: Long,
-)
