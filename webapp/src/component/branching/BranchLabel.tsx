54902c1db6e1dd339b7a09a2501dab70bbd533d9
Daniel Krizan <danyelkrizan@gmail.com>
2025-11-07 14:36:03 +0100

    feat: branch merge detail and list refactored

--- webapp/src/component/branching/BranchLabel.tsx
+++ webapp/src/component/branching/BranchLabel.tsx
@@ -1,27 +1,19 @@
-import { InputBaseComponentProps, styled } from '@mui/material';
+import { InputBaseComponentProps } from '@mui/material';
 import React from 'react';
-import { GitBranch02 } from '@untitled-ui/icons-react';
-
-const StyledFakeInput = styled('div')`
-  box-sizing: content-box;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  white-space: nowrap;
-`;
+import { BranchLabelInput } from 'tg.component/branching/BranchLabelInput';
 
 export const BranchLabel = React.forwardRef(function BranchLabel(
   { value, placeholder, ...rest }: InputBaseComponentProps,
   ref
 ) {
   return (
-    <StyledFakeInput
+    <BranchLabelInput
+      value={value}
+      placeholder={placeholder}
       tabIndex={0}
       ref={ref}
       style={{ display: 'flex' }}
       {...(rest as any)}
-    >
-      <GitBranch02 height={20} width={20} style={{ marginRight: 10 }} />
-      <div>{value || placeholder}</div>
-    </StyledFakeInput>
+    />
   );
 });
