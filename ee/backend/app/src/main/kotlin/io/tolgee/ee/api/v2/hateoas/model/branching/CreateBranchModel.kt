0bd560a3de34440a1da263e6a38d12e2ec476625
Daniel Krizan <danyelkrizan@gmail.com>
2025-11-14 13:06:07 +0100

    feat: branch name rules and router for names containing slash character

--- ee/backend/app/src/main/kotlin/io/tolgee/ee/api/v2/hateoas/model/branching/CreateBranchModel.kt
+++ ee/backend/app/src/main/kotlin/io/tolgee/ee/api/v2/hateoas/model/branching/CreateBranchModel.kt
@@ -2,13 +2,15 @@ package io.tolgee.ee.api.v2.hateoas.model.branching
 
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.Pattern
+import org.hibernate.validator.constraints.Length
 import org.springframework.hateoas.RepresentationModel
 
 @Suppress("unused")
 open class CreateBranchModel(
   @Schema(description = "Branch name, example = feature/new-feature")
-  @field:Pattern(regexp = "^[a-z0-9]([a-z0-9-_]*[a-z0-9])?$", message = "invalid_pattern")
+  @field:Pattern(regexp = "^[a-z0-9]([a-z0-9-_/]*[a-z0-9])?$", message = "invalid_pattern")
+  @field:Length(min = 2, max = 100, message = "max_length")
   val name: String,
   @Schema(description = "Origin branch id")
   val originBranchId: Long,
-  ) : RepresentationModel<CreateBranchModel>()
+) : RepresentationModel<CreateBranchModel>()
