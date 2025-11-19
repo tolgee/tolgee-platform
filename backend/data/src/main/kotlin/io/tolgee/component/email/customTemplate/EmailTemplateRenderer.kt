07235a6b12700da821ca7ed8cad9ea5d9adf1071
Daniel Krizan <danyelkrizan@gmail.com>
2025-11-19 12:27:40 +0100

    feat: custom email in Plan migration form

---
+++ backend/data/src/main/kotlin/io/tolgee/component/email/customTemplate/EmailTemplateRenderer.kt
@@ -0,0 +1,32 @@
+package io.tolgee.component.email.customTemplate
+
+import io.tolgee.component.email.customTemplate.placeholder.EmailPlaceholdersExtractor
+import org.springframework.stereotype.Component
+import java.text.MessageFormat
+import java.util.Locale
+import kotlin.reflect.KClass
+
+@Component
+class EmailTemplateRenderer(
+  private val placeholderExtractor: EmailPlaceholdersExtractor,
+) {
+
+  fun render(
+    template: String,
+    variables: EmailTemplateVariables,
+  ): String {
+    @Suppress("UNCHECKED_CAST")
+    val entries =
+      placeholderExtractor.getEntries(
+        variables::class as KClass<EmailTemplateVariables>,
+      )
+
+    val parameters =
+      entries.map { entry ->
+        entry.accessor(variables) ?: ""
+      }.toTypedArray()
+
+    return MessageFormat(template, Locale.ENGLISH).format(parameters)
+  }
+}
+
--- webapp/src/ee/billing/administration/subscriptionPlans/components/migration/PlanMigrationEmailSection.tsx
+++ webapp/src/ee/billing/administration/subscriptionPlans/components/migration/PlanMigrationEmailSection.tsx
@@ -7,7 +7,6 @@ import {
 } from 'tg.ee.module/billing/administration/subscriptionPlans/components/migration/PlanMigrationForm';
 import { Box, Typography } from '@mui/material';
 import { HtmlTemplateEditor } from 'tg.component/common/form/HtmlTemplateEditor';
-import { useEffect } from 'react';
 
 type EmailSectionProps = {
   template?: EmailTemplateData;
@@ -19,12 +18,6 @@ export const PlanMigrationEmailSection = ({ template }: EmailSectionProps) => {
     CreatePlanMigrationFormData | PlanMigrationFormData
   >();
 
-  useEffect(() => {
-    if (values.customEmailBody == null && template?.body) {
-      setFieldValue('customEmailBody', null);
-    }
-  }, [setFieldValue, template?.body, values.customEmailBody]);
-
   return (
     <Box mt={1} display="grid" gap={1}>
       <Typography>
@@ -33,8 +26,6 @@ export const PlanMigrationEmailSection = ({ template }: EmailSectionProps) => {
       <HtmlTemplateEditor
         value={values.customEmailBody ?? template?.body ?? ''}
         onChange={(val) => setFieldValue('customEmailBody', val)}
-        label={t('administration_plan_migration_custom_email_label')}
-        readOnly={false}
         disabled={!template}
         placeholders={template?.placeholders ?? []}
       />
