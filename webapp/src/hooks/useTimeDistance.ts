1e0e21c983c4a105f57789da8b49b1c9272245fc
Daniel Krizan <danyelkrizan@gmail.com>
2025-11-24 20:56:25 +0100

    feat: to be transferred subscription list

--- webapp/src/hooks/useTimeDistance.ts
+++ webapp/src/hooks/useTimeDistance.ts
@@ -6,29 +6,38 @@ export function useTimeDistance() {
   const formatDate = useDateFormatter();
 
   return (timeInPast: number | string | Date) => {
-    const differenceInSeconds =
+    const diffSeconds =
       (Date.now().valueOf() - new Date(timeInPast).valueOf()) / 1000;
+    const isFuture = diffSeconds < 0;
+    const differenceInSeconds = Math.abs(diffSeconds);
 
     if (differenceInSeconds < 60) {
-      return t('time_difference_right_now');
+      return isFuture
+        ? t('time_difference_in_seconds', {
+            value: Math.trunc(differenceInSeconds),
+          })
+        : t('time_difference_right_now');
     }
 
     if (differenceInSeconds < 60 * 60) {
-      return t('time_difference_minutes', {
-        value: Math.trunc(differenceInSeconds / 60),
-      });
+      const value = Math.trunc(differenceInSeconds / 60);
+      return isFuture
+        ? t('time_difference_in_minutes', { value })
+        : t('time_difference_minutes', { value });
     }
 
     if (differenceInSeconds < 60 * 60 * 24) {
-      return t('time_difference_hours', {
-        value: Math.trunc(differenceInSeconds / (60 * 60)),
-      });
+      const value = Math.trunc(differenceInSeconds / (60 * 60));
+      return isFuture
+        ? t('time_difference_in_hours', { value })
+        : t('time_difference_hours', { value });
     }
 
     if (differenceInSeconds < 60 * 60 * 24 * 30) {
-      return t('time_difference_days', {
-        value: Math.trunc(differenceInSeconds / (60 * 60 * 24)),
-      });
+      const value = Math.trunc(differenceInSeconds / (60 * 60 * 24));
+      return isFuture
+        ? t('time_difference_in_days', { value })
+        : t('time_difference_days', { value });
     }
 
     return formatDate(new Date(timeInPast), { dateStyle: 'medium' });
