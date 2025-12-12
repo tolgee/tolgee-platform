fbcecb05366777ec4931a6947f0ce14bb1abf162
Daniel Krizan <danyelkrizan@gmail.com>
2025-12-12 15:02:28 +0100

    feat: merge enhancement - merge all non-conflict values, conflicted values are merged based on resolution

--- backend/data/src/main/kotlin/io/tolgee/model/branching/snapshot/TranslationSnapshot.kt
+++ backend/data/src/main/kotlin/io/tolgee/model/branching/snapshot/TranslationSnapshot.kt
@@ -1,5 +1,6 @@
 package io.tolgee.model.branching.snapshot
 
+import io.hypersistence.utils.hibernate.type.json.JsonBinaryType
 import io.tolgee.activity.annotation.ActivityLoggedProp
 import io.tolgee.model.StandardAuditModel
 import io.tolgee.model.enums.TranslationState
@@ -13,6 +14,7 @@ import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import jakarta.validation.constraints.NotBlank
 import org.hibernate.annotations.ColumnDefault
+import org.hibernate.annotations.Type
 
 @Entity
 @Table(
@@ -33,6 +35,10 @@ class TranslationSnapshot(
   @ActivityLoggedProp
   var state: TranslationState = TranslationState.TRANSLATED,
 ) : StandardAuditModel() {
+  @Column(columnDefinition = "jsonb")
+  @Type(JsonBinaryType::class)
+  var labels: MutableSet<String> = mutableSetOf()
+
   @ManyToOne(optional = false, fetch = FetchType.LAZY)
   @JoinColumn(name = "key_snapshot_id")
   lateinit var keySnapshot: KeySnapshot
