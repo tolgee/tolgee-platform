b071dd7521708c4eabc7222bf58a8d2a398d774d
Daniel Krizan <danyelkrizan@gmail.com>
2025-12-12 16:56:23 +0100

    chore: snapshot stores translation labels test

--- ee/backend/tests/src/test/kotlin/io/tolgee/ee/service/branching/BranchSnapshotServiceTest.kt
+++ ee/backend/tests/src/test/kotlin/io/tolgee/ee/service/branching/BranchSnapshotServiceTest.kt
@@ -1,7 +1,7 @@
 package io.tolgee.ee.service.branching
 
 import io.tolgee.AbstractSpringTest
-import io.tolgee.development.testDataBuilder.data.BranchMergeTestData
+import io.tolgee.development.testDataBuilder.data.BranchSnapshotTestData
 import io.tolgee.testing.assert
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -14,30 +14,40 @@ class BranchSnapshotServiceTest : AbstractSpringTest() {
   @Autowired
   lateinit var branchSnapshotService: BranchSnapshotService
 
-  private lateinit var testData: BranchMergeTestData
+  private lateinit var testData: BranchSnapshotTestData
 
   @BeforeEach
   fun setup() {
-    testData = BranchMergeTestData()
+    testData = BranchSnapshotTestData()
     testDataService.saveTestData(testData.root)
-  }
-
-  @Test
-  @Transactional
-  fun `snapshot stores key meta tags`() {
     branchSnapshotService.createInitialSnapshot(
       testData.project.id,
       testData.mainBranch,
       testData.featureBranch,
     )
+  }
 
-    val snapshot =
-      branchSnapshotService
-        .getSnapshotKeys(testData.featureBranch.id)
-        .first { it.name == BranchMergeTestData.UPDATE_KEY_NAME }
-
-    val snapshotTags = snapshot.keyMetaSnapshot!!.tags
+  @Test
+  @Transactional
+  fun `snapshot stores key meta tags`() {
+    val snapshotTags = getSnapshotKey().keyMetaSnapshot!!.tags
     snapshotTags.assert.hasSize(3)
     snapshotTags.map { it.name }.assert.containsExactlyInAnyOrder("abc", "def", "ghi")
   }
+
+  @Test
+  @Transactional
+  fun `snapshot stores translation labels`() {
+    val snapshotTranslation =
+      getSnapshotKey()
+        .translations
+        .first { it.language == "en" }
+
+    snapshotTranslation.labels.assert.containsExactlyInAnyOrder("prod")
+  }
+
+  private fun getSnapshotKey() =
+    branchSnapshotService
+      .getSnapshotKeys(testData.featureBranch.id)
+      .first { it.name == testData.keyToSnapshot.name }
 }
