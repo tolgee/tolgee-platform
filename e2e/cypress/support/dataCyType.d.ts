33dc7badb895ca31f2a71573734359311a5b76b9
Daniel Krizan <danyelkrizan@gmail.com>
2025-12-16 15:58:15 +0100

    chore: coderabbitai changes

--- e2e/cypress/support/dataCyType.d.ts
+++ e2e/cypress/support/dataCyType.d.ts
@@ -576,10 +576,12 @@ declare namespace DataCy {
         "plan-limit-dialog-close" |
         "plan-limit-exceeded-popover" |
         "plan-migration-email-section" |
+        "plan-migration-subscription-skip" |
         "plan-migration-tab-migrated" |
         "plan-migration-tab-upcoming" |
         "plan-migration-tooltip-detail" |
         "plan-migration-upcoming-list" |
+        "plan-migration-upcoming-list-item" |
         "plan_seat_limit_exceeded_while_accepting_invitation_message" |
         "project-ai-prompt-dialog-description-input" |
         "project-ai-prompt-dialog-save" |
