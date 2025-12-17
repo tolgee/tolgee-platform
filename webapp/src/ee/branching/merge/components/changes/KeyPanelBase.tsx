782e78b3beeb341f7cf0b56e5080b7c7eb567774
Daniel Krizan <danyelkrizan@gmail.com>
2025-12-17 20:33:45 +0100

    feat: merge detail refactor (wip)

---
+++ webapp/src/ee/branching/merge/components/changes/KeyPanelBase.tsx
@@ -0,0 +1,42 @@
+import { Box, styled } from '@mui/material';
+
+export const KeyPanel = styled(Box)`
+  flex: 1;
+  border: 1px solid ${({ theme }) => theme.palette.divider1};
+  border-radius: ${({ theme }) => theme.spacing(1)};
+  display: grid;
+
+  &.accepted {
+    border-color: ${({ theme }) => theme.palette.tokens.success.main};
+  }
+`;
+
+export const KeyHeader = styled(Box)`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  background: ${({ theme }) => theme.palette.tokens.background.hover};
+  padding: ${({ theme }) => theme.spacing(1.5, 2)};
+
+  &.accepted {
+    background: ${({ theme }) => theme.palette.tokens.success._states.selected};
+  }
+`;
+
+export const AcceptButton = styled(Box)`
+  display: flex;
+  align-items: center;
+`;
+
+export const TranslationList = styled(Box)`
+  display: grid;
+`;
+
+export const StyledLanguageField = styled('div')`
+  border-color: ${({ theme }) => theme.palette.divider1};
+  border-width: 1px 1px 1px 0;
+  border-style: solid;
+
+  & + & {
+    border-top: 0;
+  }
+`;
