7080bf55c5c398fe434a1e601de9854862f6e959
Daniel Krizan <danyelkrizan@gmail.com>
2025-12-17 20:33:45 +0100

    feat: merge detail refactor (wip)

---
+++ webapp/src/ee/branching/merge/components/changes/PlaceholderKeyPanel.tsx
@@ -0,0 +1,24 @@
+import { Box, styled, Typography } from '@mui/material';
+import { FC } from 'react';
+import { KeyPanel } from './KeyPanelBase';
+
+const PlaceholderContent = styled(Box)`
+  min-height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: ${({ theme }) => theme.spacing(2)};
+  color: ${({ theme }) => theme.palette.text.secondary};
+  background: ${({ theme }) => theme.palette.action.hover};
+  text-align: center;
+`;
+
+export const PlaceholderKeyPanel: FC<{ text: string }> = ({ text }) => (
+  <KeyPanel>
+    <PlaceholderContent>
+      <Typography variant="body2" sx={{ fontStyle: 'italic' }}>
+        {text}
+      </Typography>
+    </PlaceholderContent>
+  </KeyPanel>
+);
