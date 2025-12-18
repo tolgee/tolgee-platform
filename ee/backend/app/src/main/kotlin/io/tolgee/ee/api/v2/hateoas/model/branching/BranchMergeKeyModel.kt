4aa431e37466ae081a97eb4380ad7d569ccd2f38
Daniel Krizan <danyelkrizan@gmail.com>
2025-12-18 13:28:39 +0100

    feat: merge detail refactor - modifies and conflicts model

---
+++ ee/backend/app/src/main/kotlin/io/tolgee/ee/api/v2/hateoas/model/branching/BranchMergeKeyModel.kt
@@ -0,0 +1,16 @@
+package io.tolgee.ee.api.v2.hateoas.model.branching
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+data class BranchMergeKeyModel(
+  @Schema(description = "Key id")
+  val keyId: Long,
+  @Schema(description = "Key name")
+  val keyName: String,
+  @Schema(description = "Whether key uses plural forms")
+  val keyIsPlural: Boolean,
+  @Schema(description = "Key description")
+  val keyDescription: String?,
+  @Schema(description = "Translations indexed by language tag")
+  val translations: Map<String, BranchMergeTranslationModel>,
+)
