883cfe7ebbf74383ce54c06bacd51945d1fc72e3
Daniel Krizan <danyelkrizan@gmail.com>
2026-01-13 20:02:02 +0100

    feat: enhance task handling with origin branch tracking during branch merges

--- webapp/src/service/apiSchema.generated.ts
+++ webapp/src/service/apiSchema.generated.ts
@@ -5962,6 +5962,7 @@ export interface components {
       name?: string;
       /** Format: int64 */
       number: number;
+      originBranchName?: string;
       /** @enum {string} */
       state: "NEW" | "IN_PROGRESS" | "FINISHED" | "CANCELED";
       /** Format: int64 */
@@ -5999,6 +6000,7 @@ export interface components {
       name?: string;
       /** Format: int64 */
       number: number;
+      originBranchName?: string;
       project: components["schemas"]["SimpleProjectModel"];
       /** @enum {string} */
       state: "NEW" | "IN_PROGRESS" | "FINISHED" | "CANCELED";
