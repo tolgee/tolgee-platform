d0764fd5ae23f254325e6b10d647e8baa06e31d3
Daniel Krizan <danyelkrizan@gmail.com>
2026-01-13 15:20:10 +0100

    feat: replace direct links with `TaskTranslationsLink` - allows redirect only if task is not merged

---
+++ webapp/src/component/task/TaskTranslationsLink.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+
+import { getTaskUrl } from 'tg.constants/links';
+import { components } from 'tg.service/apiSchema.generated';
+import { TASK_ACTIVE_STATES } from 'tg.component/task/taskActiveStates';
+import { useBranchesService } from 'tg.views/projects/translations/context/services/useBranchesService';
+
+type TaskModel = components['schemas']['TaskModel'];
+
+type Props<T extends React.ElementType> = {
+  task: TaskModel;
+  projectId?: number;
+  component?: T;
+} & Omit<React.ComponentPropsWithoutRef<T>, 'component' | 'to'>;
+
+export const TaskTranslationsLink = <
+  T extends React.ElementType = typeof RouterLink
+>({
+  task,
+  projectId,
+  component,
+  ...rest
+}: Props<T>) => {
+  const { selectedName } = useBranchesService({ projectId });
+  const allowLink = !(
+    task.branchName &&
+    task.branchName != selectedName &&
+    !TASK_ACTIVE_STATES.includes(task.state)
+  );
+  const to = projectId && allowLink ? getTaskUrl(projectId, task.number) : '';
+  const Component = (component ?? RouterLink) as React.ElementType;
+
+  if (!to) {
+    const Fallback = Component === RouterLink ? 'span' : Component;
+    const style = { cursor: 'default', ...(rest as any).style };
+    return <Fallback {...rest} style={style} />;
+  }
+
+  if (Component === RouterLink) {
+    return <Component to={to} {...rest} />;
+  }
+
+  return <Component component={RouterLink} to={to} {...rest} />;
+};
--- webapp/src/ee/task/components/TaskTooltip.tsx
+++ webapp/src/ee/task/components/TaskTooltip.tsx
@@ -1,5 +1,4 @@
 import { useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { Dialog, IconButton, Tooltip } from '@mui/material';
 import { InfoCircle, Translate01 } from '@untitled-ui/icons-react';
 import { useTranslate } from '@tolgee/react';
@@ -8,7 +7,7 @@ import { components } from 'tg.service/apiSchema.generated';
 import { stopAndPrevent } from 'tg.fixtures/eventHandler';
 import { TaskTooltipContent } from './TaskTooltipContent';
 import { TaskDetail } from './TaskDetail';
-import { getTaskUrl } from 'tg.constants/links';
+import { TaskTranslationsLink } from 'tg.component/task/TaskTranslationsLink';
 
 type TaskModel = components['schemas']['TaskModel'];
 type SimpleProjectModel = components['schemas']['SimpleProjectModel'];
@@ -42,14 +41,15 @@ export const TaskTooltip = ({
               disableInteractive
               title={t('task_link_translations_tooltip')}
             >
-              <IconButton
+              <TaskTranslationsLink
+                component={IconButton}
+                task={task}
+                projectId={project.id}
                 data-cy="task-tooltip-action-translations"
-                component={Link}
-                to={getTaskUrl(project.id, task.number)}
                 size="small"
               >
                 <Translate01 width={20} height={20} />
-              </IconButton>
+              </TaskTranslationsLink>
             </Tooltip>
           )}
           {actions.includes('detail') && (
