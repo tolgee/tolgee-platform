ed437344e751460118559be84435c02f7ade6972
Jan Molnár <molec@centrum.cz>
2026-01-22 16:40:05 +0100

    fix: e2e & co. fixes (#3402)

--- webapp/src/component/branching/useIsBranchingEnabled.ts
+++ webapp/src/component/branching/useIsBranchingEnabled.ts
@@ -1,9 +1,9 @@
-import { useProject } from 'tg.hooks/useProject';
+import { useProjectContextOptional } from 'tg.hooks/useProject';
 import { useEnabledFeatures } from 'tg.globalContext/helpers';
 
 export const useIsBranchingEnabled = () => {
-  const project = useProject();
+  const project = useProjectContextOptional()?.project; // useProject() throws error here, because project may not be loaded
   const { isEnabled } = useEnabledFeatures();
 
-  return isEnabled('BRANCHING') && project.useBranching;
+  return isEnabled('BRANCHING') && !!project?.useBranching;
 };
