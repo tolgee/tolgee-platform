467c0828051d681d2f2fbf5b31eb52ef155144f1
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2026-01-27 12:04:10 +0100

    fixup! chore: add e2e tests

--- e2e/cypress/compounds/glossaries/E2GlossaryPanel.ts
+++ e2e/cypress/compounds/glossaries/E2GlossaryPanel.ts
@@ -1,5 +1,5 @@
 import { gcy } from '../../common/shared';
-
+import { waitForGlobalLoading } from '../../common/loading';
 
 export class E2GlossaryPanel {
   hoverOnTermPreview(name: string) {
@@ -8,7 +8,30 @@ export class E2GlossaryPanel {
       .first()
       .trigger('mouseover');
 
-    // Wait for edit button to appear in the tooltip
+    // Wait for the edit button to appear in the tooltip
     gcy('glossary-term-preview-edit-button').should('be.visible');
-  };
+  }
+
+  inlineEditTermPreview(
+    name: string,
+    newTranslation: string,
+    currentTranslation: string | undefined = undefined
+  ) {
+    // Hover over the term preview to show the edit button
+    this.hoverOnTermPreview(name);
+
+    gcy('glossary-term-preview-edit-button').click();
+    gcy('glossary-term-preview-edit-input').should('be.visible');
+    if (currentTranslation !== undefined) {
+      gcy('glossary-term-preview-edit-input')
+        .find('input')
+        .should('have.value', currentTranslation);
+    }
+    gcy('glossary-term-preview-edit-input')
+      .find('input')
+      .clear()
+      .type(newTranslation);
+    gcy('glossary-term-preview-save-button').click();
+    waitForGlobalLoading();
+  }
 }
--- e2e/cypress/e2e/glossary/panel.cy.ts
+++ e2e/cypress/e2e/glossary/panel.cy.ts
@@ -52,21 +52,10 @@ describe('Glossary panel', () => {
 
       view.getTranslationCell('key_with_term', 'fr').click();
       const panel = view.openGlossaryPanel();
-      panel.hoverOnTermPreview('Term');
 
-      // Click edit button to start editing
-      gcy('glossary-term-preview-edit-button').click();
-      gcy('glossary-term-preview-edit-input').should('be.visible');
-
-      // Type the new French translation
-      gcy('glossary-term-preview-edit-input').find('input').type('Terme');
-
-      // Save the translation
-      gcy('glossary-term-preview-save-button').click();
-      waitForGlobalLoading();
+      panel.inlineEditTermPreview('Term', 'Terme');
 
       // Verify the translation was saved
-      panel.hoverOnTermPreview('Term');
       gcy('glossary-term-preview-target-text').should('contain.text', 'Terme');
     });
 
@@ -77,33 +66,15 @@ describe('Glossary panel', () => {
 
       view.getTranslationCell('key_with_term', 'fr').click();
       const panel = view.openGlossaryPanel();
-      panel.hoverOnTermPreview('Term');
 
-      // First add a translation so we can edit it
-      gcy('glossary-term-preview-edit-button').click();
-      gcy('glossary-term-preview-edit-input')
-        .find('input')
-        .type('Terme initial');
-      gcy('glossary-term-preview-save-button').click();
-      waitForGlobalLoading();
+      panel.inlineEditTermPreview('Term', 'Terme initial');
+      gcy('glossary-term-preview-target-text').should(
+        'contain.text',
+        'Terme initial'
+      );
 
-      // Now hover again and edit the existing translation
-      panel.hoverOnTermPreview('Term');
-      gcy('glossary-term-preview-edit-button').click();
-      gcy('glossary-term-preview-edit-input')
-        .find('input')
-        .should('have.value', 'Terme initial');
+      panel.inlineEditTermPreview('Term', 'Terme modifié', 'Terme initial');
 
-      // Clear and type new value
-      gcy('glossary-term-preview-edit-input')
-        .find('input')
-        .clear()
-        .type('Terme modifié');
-      gcy('glossary-term-preview-save-button').click();
-      waitForGlobalLoading();
-
-      // Verify the updated translation
-      panel.hoverOnTermPreview('Term');
       gcy('glossary-term-preview-target-text').should(
         'contain.text',
         'Terme modifié'
@@ -125,7 +96,6 @@ describe('Glossary panel', () => {
         .type('Terme avec Enter{enter}');
       waitForGlobalLoading();
 
-      panel.hoverOnTermPreview('Term');
       gcy('glossary-term-preview-target-text').should(
         'contain.text',
         'Terme avec Enter'
@@ -139,8 +109,10 @@ describe('Glossary panel', () => {
 
       view.getTranslationCell('key_with_term', 'fr').click();
       const panel = view.openGlossaryPanel();
-      panel.hoverOnTermPreview('Term');
 
+      panel.inlineEditTermPreview('Term', 'This should be left untouched');
+
+      panel.hoverOnTermPreview('Term');
       gcy('glossary-term-preview-edit-button').click();
       gcy('glossary-term-preview-edit-input')
         .find('input')
@@ -148,11 +120,12 @@ describe('Glossary panel', () => {
 
       gcy('glossary-term-preview-cancel-button').click();
 
-      // Edit mode should close
       gcy('glossary-term-preview-edit-input').should('not.exist');
-      // Should show empty state since no translation was saved
-      panel.hoverOnTermPreview('Term');
-      gcy('glossary-term-preview-target-text').should('exist');
+
+      gcy('glossary-term-preview-target-text').should(
+        'contain.text',
+        'This should be left untouched'
+      );
     });
 
     it('Cancels editing via Escape key', () => {
@@ -162,15 +135,21 @@ describe('Glossary panel', () => {
 
       view.getTranslationCell('key_with_term', 'fr').click();
       const panel = view.openGlossaryPanel();
-      panel.hoverOnTermPreview('Term');
 
+      panel.inlineEditTermPreview('Term', 'This should be left untouched');
+
+      panel.hoverOnTermPreview('Term');
       gcy('glossary-term-preview-edit-button').click();
       gcy('glossary-term-preview-edit-input')
         .find('input')
         .type('This should be cancelled{esc}');
 
-      // Edit mode should close
       gcy('glossary-term-preview-edit-input').should('not.exist');
+
+      gcy('glossary-term-preview-target-text').should(
+        'contain.text',
+        'This should be left untouched'
+      );
     });
   });
 });
