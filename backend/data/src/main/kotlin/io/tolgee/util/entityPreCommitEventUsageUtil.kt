b85770ac5e98dda8569e4e6afb2f7a97c41f7e63
Daniel Krizan <danyelkrizan@gmail.com>
2026-02-24 16:40:02 +0100

    feat: add usage increase logic for translation updates, adjusted logic to track usage when empty text becomes non-empty.

--- backend/data/src/main/kotlin/io/tolgee/util/entityPreCommitEventUsageUtil.kt
+++ backend/data/src/main/kotlin/io/tolgee/util/entityPreCommitEventUsageUtil.kt
@@ -3,11 +3,36 @@ package io.tolgee.util
 import io.tolgee.events.EntityPreCommitEvent
 import io.tolgee.events.OnEntityPreDelete
 import io.tolgee.events.OnEntityPrePersist
+import io.tolgee.events.OnEntityPreUpdate
+import io.tolgee.model.translation.Translation
 
 fun EntityPreCommitEvent.getUsageIncreaseAmount(): Long {
   return when (this) {
-    is OnEntityPrePersist -> 1
-    is OnEntityPreDelete -> -1
-    else -> 0
+    is OnEntityPrePersist -> {
+      1
+    }
+
+    is OnEntityPreDelete -> {
+      -1
+    }
+
+    is OnEntityPreUpdate -> {
+      val entity = this.entity
+      if (entity is Translation) {
+        val textIndex = this.propertyNames?.indexOf("text") ?: -1
+        if (textIndex != -1) {
+          val previousText = this.previousState?.get(textIndex) as? String
+          val currentText = entity.text
+          if (previousText.isNullOrEmpty() && !currentText.isNullOrEmpty()) {
+            return 1
+          }
+        }
+      }
+      0
+    }
+
+    else -> {
+      0
+    }
   }
 }
