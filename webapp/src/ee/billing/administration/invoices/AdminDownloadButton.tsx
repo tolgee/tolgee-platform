fda3db8bf03e34a08864bf024e172d32b0fa75b9
Daniel Krizan <danyelkrizan@gmail.com>
2026-03-14 19:59:43 +0100

    refactor: extract invoice PDF download logic to reusable hook and component

--- webapp/src/ee/billing/administration/invoices/AdminDownloadButton.tsx
+++ webapp/src/ee/billing/administration/invoices/AdminDownloadButton.tsx
@@ -1,9 +1,9 @@
 import { FC } from 'react';
 
-import LoadingButton from 'tg.component/common/form/LoadingButton';
 import { components } from 'tg.service/billingApiSchema.generated';
-import { useConfig } from 'tg.globalContext/helpers';
 import { useBillingApiMutation } from 'tg.service/http/useQueryApi';
+import { useInvoicePdfDownload } from '../../Invoices/useInvoicePdfDownload';
+import { PdfDownloadButton } from '../../Invoices/PdfDownloadButton';
 
 type AdminDownloadButtonProps = {
   invoice: components['schemas']['InvoiceModel'];
@@ -12,55 +12,23 @@ type AdminDownloadButtonProps = {
 export const AdminDownloadButton: FC<AdminDownloadButtonProps> = ({
   invoice,
 }) => {
-  const config = useConfig();
+  const { onSuccess } = useInvoicePdfDownload(invoice);
 
   const pdfMutation = useBillingApiMutation({
     url: '/v2/administration/billing/invoices/{invoiceId}/pdf',
     method: 'get',
-    fetchOptions: {
-      rawResponse: true,
-    },
+    fetchOptions: { rawResponse: true },
   });
 
   const onDownload = () => {
-    pdfMutation.mutate(
-      {
-        path: {
-          invoiceId: invoice.id,
-        },
-      },
-      {
-        async onSuccess(response) {
-          const res = response as unknown as Response;
-          const data = await res.blob();
-          const url = URL.createObjectURL(data as unknown as Blob);
-          try {
-            const a = document.createElement('a');
-            try {
-              a.href = url;
-              a.download = `${config.appName.toLowerCase()}-${
-                invoice.number
-              }.pdf`;
-              a.click();
-            } finally {
-              a.remove();
-            }
-          } finally {
-            setTimeout(() => URL.revokeObjectURL(url), 7000);
-          }
-        },
-      }
-    );
+    pdfMutation.mutate({ path: { invoiceId: invoice.id } }, { onSuccess });
   };
 
   return (
-    <LoadingButton
-      disabled={!invoice.pdfReady}
-      loading={pdfMutation.isLoading}
-      onClick={onDownload}
-      size="small"
-    >
-      PDF
-    </LoadingButton>
+    <PdfDownloadButton
+      invoice={invoice}
+      onDownload={onDownload}
+      isLoading={pdfMutation.isLoading}
+    />
   );
 };
