ea96388cdf5cb7bafd871a6ac26990ad74e947fc
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2026-03-16 15:02:38 +0100

    fix: comment out not yet implemented checks for now

--- backend/data/src/main/kotlin/io/tolgee/model/enums/qa/QaCheckType.kt
+++ backend/data/src/main/kotlin/io/tolgee/model/enums/qa/QaCheckType.kt
@@ -5,19 +5,20 @@ enum class QaCheckType(
 ) {
   EMPTY_TRANSLATION(QaCheckSeverity.OFF),
   SPACES_MISMATCH(QaCheckSeverity.WARNING),
-  UNMATCHED_NEWLINES(QaCheckSeverity.WARNING),
+
+//  UNMATCHED_NEWLINES(QaCheckSeverity.WARNING),
   CHARACTER_CASE_MISMATCH(QaCheckSeverity.WARNING),
   MISSING_NUMBERS(QaCheckSeverity.WARNING),
   PUNCTUATION_MISMATCH(QaCheckSeverity.WARNING),
-  BRACKETS_MISMATCH(QaCheckSeverity.WARNING),
-  SPECIAL_CHARACTER_MISMATCH(QaCheckSeverity.OFF),
-  DIFFERENT_URLS(QaCheckSeverity.WARNING),
-  INCONSISTENT_PLACEHOLDERS(QaCheckSeverity.WARNING),
-  INCONSISTENT_HTML(QaCheckSeverity.WARNING),
-  ICU_SYNTAX(QaCheckSeverity.WARNING),
-  REPEATED_WORDS(QaCheckSeverity.WARNING),
-  SPELLING(QaCheckSeverity.WARNING),
-  UNRESOLVED_COMMENTS(QaCheckSeverity.WARNING),
+//  BRACKETS_MISMATCH(QaCheckSeverity.WARNING),
+//  SPECIAL_CHARACTER_MISMATCH(QaCheckSeverity.OFF),
+//  DIFFERENT_URLS(QaCheckSeverity.WARNING),
+//  INCONSISTENT_PLACEHOLDERS(QaCheckSeverity.WARNING),
+//  INCONSISTENT_HTML(QaCheckSeverity.WARNING),
+//  ICU_SYNTAX(QaCheckSeverity.WARNING),
+//  REPEATED_WORDS(QaCheckSeverity.WARNING),
+//  SPELLING(QaCheckSeverity.WARNING),
+//  UNRESOLVED_COMMENTS(QaCheckSeverity.WARNING),
   ;
 
   companion object {
@@ -27,23 +28,23 @@ enum class QaCheckType(
           listOf(
             EMPTY_TRANSLATION,
             SPACES_MISMATCH,
-            UNMATCHED_NEWLINES,
+//            UNMATCHED_NEWLINES,
             CHARACTER_CASE_MISMATCH,
             MISSING_NUMBERS,
-            SPELLING,
-            REPEATED_WORDS,
+//            SPELLING,
+//            REPEATED_WORDS,
             PUNCTUATION_MISMATCH,
-            BRACKETS_MISMATCH,
-            SPECIAL_CHARACTER_MISMATCH,
-            DIFFERENT_URLS,
+//            BRACKETS_MISMATCH,
+//            SPECIAL_CHARACTER_MISMATCH,
+//            DIFFERENT_URLS,
           ),
-        QaCheckCategory.SYNTAX to
-          listOf(
-            INCONSISTENT_PLACEHOLDERS,
-            INCONSISTENT_HTML,
-            ICU_SYNTAX,
-            UNRESOLVED_COMMENTS,
-          ),
+//        QaCheckCategory.SYNTAX to
+//          listOf(
+//            INCONSISTENT_PLACEHOLDERS,
+//            INCONSISTENT_HTML,
+//            ICU_SYNTAX,
+//            UNRESOLVED_COMMENTS,
+//          ),
       )
   }
 }
