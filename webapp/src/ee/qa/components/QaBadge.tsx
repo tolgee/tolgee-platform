267cc2680b93980f52e1c5ff7a075027242cfd39
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2026-05-20 18:39:31 +0200

    feat: replace QA badge stale spinner with static dots icon

--- webapp/src/ee/qa/components/QaBadge.tsx
+++ webapp/src/ee/qa/components/QaBadge.tsx
@@ -1,10 +1,15 @@
-import { Badge, CircularProgress, styled } from '@mui/material';
+import { Badge, styled } from '@mui/material';
 
 import { QaBadgeProps } from '../../../eeSetup/EeModuleType';
 import { QaCheck } from 'tg.component/CustomIcons';
-import { Check } from '@untitled-ui/icons-react';
+import { Check, DotsHorizontal } from '@untitled-ui/icons-react';
+import clsx from 'clsx';
 
 const StyledBadge = styled(Badge)`
+  &.darker {
+    opacity: 0.5;
+  }
+
   display: flex;
 
   & .unresolved {
@@ -22,24 +27,16 @@ const StyledBadge = styled(Badge)`
     align-items: center;
     justify-content: center;
   }
-  & .stale {
-    overflow: hidden;
-  }
-`;
-
-const StyledSpinner = styled(CircularProgress)`
-  color: ${({ theme }) => theme.palette.emphasis[100]};
-  width: 10px !important;
-  height: 10px !important;
-  margin: -5px;
-
-  & svg {
-    width: 10px;
-    height: 10px;
-  }
 `;
 
 const StyledCheckIcon = styled(Check)`
+  color: ${({ theme }) => theme.palette.emphasis[100]};
+  width: 14px !important;
+  height: 14px !important;
+  margin: -5px;
+`;
+
+const StyledDotsIcon = styled(DotsHorizontal)`
   color: ${({ theme }) => theme.palette.emphasis[100]};
   width: 14px !important;
   height: 14px !important;
@@ -52,18 +49,14 @@ export const QaBadge = ({
   darkWhenNoIssues = false,
 }: QaBadgeProps) => {
   if (stale) {
-    const hasIssues = count !== undefined && count > 0;
     return (
       <span data-cy="qa-badge">
         <StyledBadge
-          badgeContent={<StyledSpinner size={10} thickness={5} />}
+          badgeContent={<StyledDotsIcon />}
           classes={{ badge: 'stale' }}
+          className={clsx({ darker: darkWhenNoIssues })}
         >
-          <QaCheck
-            style={
-              hasIssues || !darkWhenNoIssues ? undefined : { opacity: 0.5 }
-            }
-          />
+          <QaCheck />
         </StyledBadge>
       </span>
     );
@@ -75,8 +68,9 @@ export const QaBadge = ({
         <StyledBadge
           badgeContent={<StyledCheckIcon />}
           classes={{ badge: 'resolved' }}
+          className={clsx({ darker: darkWhenNoIssues })}
         >
-          <QaCheck style={!darkWhenNoIssues ? undefined : { opacity: 0.5 }} />
+          <QaCheck />
         </StyledBadge>
       </span>
     );
