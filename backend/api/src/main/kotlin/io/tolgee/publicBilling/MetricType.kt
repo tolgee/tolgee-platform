b0e7b2040ddc5d31831c0334f20387864aa530d3
Daniel Krizan <danyelkrizan@gmail.com>
2026-06-24 21:34:37 +0200

    chore: add HOSTED_WORDS metric type

--- backend/api/src/main/kotlin/io/tolgee/publicBilling/MetricType.kt
+++ backend/api/src/main/kotlin/io/tolgee/publicBilling/MetricType.kt
@@ -4,10 +4,12 @@ enum class MetricType(
   val useSeats: Boolean,
   val useKeys: Boolean,
   val useStrings: Boolean,
+  val useWords: Boolean,
 ) {
   /**
    * Non-free plans, where user cay be charged by their usage
    */
-  KEYS_SEATS(useSeats = true, useKeys = true, useStrings = false),
-  STRINGS(useSeats = false, useKeys = false, useStrings = true),
+  KEYS_SEATS(useSeats = true, useKeys = true, useStrings = false, useWords = false),
+  STRINGS(useSeats = false, useKeys = false, useStrings = true, useWords = false),
+  HOSTED_WORDS(useSeats = false, useKeys = false, useStrings = false, useWords = true),
 }
