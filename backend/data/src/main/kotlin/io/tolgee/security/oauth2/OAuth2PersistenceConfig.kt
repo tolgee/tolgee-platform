e9939a48ee7cca35434bba34975eee9c2120ebaa
Dmitrii Bocharov <bdshadow@gmail.com>
2026-08-06 11:22:58 +0200

    feat: add OAuth2 signing keys, JWKS and JDBC grant storage
    Persisted RSA keypair via FileStorage (separate from the HMAC session secret) feeding a JWKSource, JWT encoder and RS256 decoder. Adds the Spring Authorization Server JDBC schema (registered clients, authorizations, consents) as a Liquibase changeset, included in schema.xml and excluded from diffChangeLog so it is not dropped.

---
+++ backend/data/src/main/kotlin/io/tolgee/security/oauth2/OAuth2PersistenceConfig.kt
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) 2026 Tolgee s.r.o. and contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.tolgee.security.oauth2
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService
+import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
+
+/** JDBC persistence for OAuth2 grants/consents/clients (SAS built-in schema, see oauth2-server.xml). */
+@Configuration
+class OAuth2PersistenceConfig {
+  @Bean("jdbcRegisteredClientRepository")
+  fun jdbcRegisteredClientRepository(jdbcTemplate: JdbcTemplate): JdbcRegisteredClientRepository {
+    return JdbcRegisteredClientRepository(jdbcTemplate)
+  }
+
+  @Bean
+  fun oAuth2AuthorizationService(
+    jdbcTemplate: JdbcTemplate,
+    registeredClientRepository: RegisteredClientRepository,
+  ): OAuth2AuthorizationService {
+    return JdbcOAuth2AuthorizationService(jdbcTemplate, registeredClientRepository)
+  }
+
+  @Bean
+  fun oAuth2AuthorizationConsentService(
+    jdbcTemplate: JdbcTemplate,
+    registeredClientRepository: RegisteredClientRepository,
+  ): OAuth2AuthorizationConsentService {
+    return JdbcOAuth2AuthorizationConsentService(jdbcTemplate, registeredClientRepository)
+  }
+}
